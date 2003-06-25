b43fff89535f80639c2be84d3a335e2a4f8d928b
W.M. Roshan Weerasuriya <roshan@apache.org>
2003-06-25 07:39:08 +0000

    committing c++ code base

---
+++ include/axis/server/SoapFault.h
@@ -0,0 +1,105 @@
+/* -*- C++ -*- */
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ *
+ *
+ *
+ * @author Roshan
+ *
+ */
+
+// SoapFault.h: interface for the SoapFault class.
+//
+//////////////////////////////////////////////////////////////////////
+#pragma warning (disable : 4786)
+
+#if !defined(AFX_SOAPFAULT_H__1E4329DF_59B2_4C55_971D_4B84B0FED7FF__INCLUDED_)
+#define AFX_SOAPFAULT_H__1E4329DF_59B2_4C55_971D_4B84B0FED7FF__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <string>
+#include <map>
+#include "SoapFaults.h"
+
+using namespace std;
+
+class SoapFault  
+{
+public:
+	static SoapFault* getSoapFault(int);
+	static void initialize();
+	//string& serialize();
+	int serialize(string&);	
+	virtual ~SoapFault();
+private:
+	SoapFault();
+	//string m_sFaultSerialized;
+	string m_sDetail;
+	string m_sFaultactor;
+	string m_sFaultstring;
+	string m_sFaultcode;
+	static map<int, SoapFaultStruct> m_sFaultMap;
+	static bool m_bInit;
+	//int setDetail(const string& sDetail);
+	//int setFaultactor(const string& sFaultactor);
+	//int setFaultstring(const string& sFaultstring);
+	//int setFaultcode(const string& sFaultcode);
+};
+
+#endif // !defined(AFX_SOAPFAULT_H__1E4329DF_59B2_4C55_971D_4B84B0FED7FF__INCLUDED_)
