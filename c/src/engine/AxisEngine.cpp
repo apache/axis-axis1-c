80e7494c077cecd88e1b21222a9c14c4f534d9ba
R.A. Susantha Kumara <susantha@apache.org>
2003-06-25 05:13:45 +0000

    committing c++ code base

---
+++ c/src/engine/AxisEngine.cpp
@@ -0,0 +1,482 @@
+/* -*- C++ -*- */
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+
+
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ *
+ *
+ *
+ * @author Susantha Kumara (skumara@virtusa.com)
+ *
+ */
+
+#include <stdio.h>
+#include "AxisEngine.h"
+#include "../common/MessageData.h"
+#include "../soap/SoapSerializer.h"
+#include "../soap/SoapDeSerializer.h"
+#include "../soap/SoapFaults.h"
+#include "../soap/URIMapping.h"
+#include "../common/Debug.h"
+
+
+DEBUG_INCLUDE;
+#ifdef WIN32
+#define WSDDFILEPATH "C:/Apache/Axis/server.wsdd"
+#else //For linux
+#define WSDDFILEPATH "/usr/local/axiscpp/axis/server.wsdd"
+#endif
+
+AxisEngine* AxisEngine::m_pObject = NULL;
+
+
+
+AxisEngine::AxisEngine()
+{
+	//Create Serializer and Deserializer
+	m_pSZ = new SoapSerializer();
+	m_pDZ = new SoapDeSerializer();  
+	m_pWSDD = new WSDDDeployment();
+	m_pHandlerPool = new HandlerPool();
+	m_pWebService = NULL;
+  
+
+#if defined( DEBUG)
+DEBUG_ONE_PARA_LEVEL("Created DeployedServiceInfo No problem ..........");
+#endif
+  //printf("Done WSDD\n");
+}
+
+AxisEngine::~AxisEngine()
+{
+	delete m_pSZ;
+	delete m_pDZ;
+	delete m_pWSDD;
+	delete m_pHandlerPool;
+	//unload xerces DLL
+	XMLPlatformUtils::Terminate();
+}
+
+AxisEngine* AxisEngine::GetAxisEngine()
+{
+	if (!m_pObject)
+	{
+		try
+		{
+#if defined( DEBUG)
+DEBUG_ONE_PARA_LEVEL("GetAxisEngine********************");
+#endif
+			XMLPlatformUtils::Initialize();
+			m_pObject = new AxisEngine();
+			if (!m_pObject) return NULL;
+			if(SUCCESS != m_pObject->Initialize())
+			{
+				m_pObject->UnInitialize();
+				delete m_pObject;
+				m_pObject = NULL;
+				return NULL;
+			}
+		}
+		catch (...)
+		{
+			return NULL;
+		}	
+	}
+	return m_pObject;
+}
+
+int AxisEngine::Process(soapstream* soap) 
+{
+	MessageData* pMsg = NULL;
+	MemBufInputSource* pSoapInput = NULL;
+	WSDDHandlerList* pHandlerList = NULL;
+	WSDDService* pService = NULL;
+	do {
+		//Initialize Serializer and Deserializer objects
+		m_pSZ->init();
+		m_pDZ->Init();
+		//create and populate MessageData
+		pMsg = new MessageData();
+		pMsg->m_Protocol = soap->trtype;
+		pMsg->SetSerializer(m_pSZ);
+		pMsg->SetDeSerializer(m_pDZ);
+
+		//Adding SoapEnvelop and SoapBody to Serializer
+		SoapEnvelope* pEnv = new SoapEnvelope();
+		pMsg->m_pSZ->setSoapEnvelope(pEnv);
+		pMsg->m_pSZ->setSoapBody(new SoapBody());
+
+		string service = "Maths";//getheader(soap, SOAPACTIONHEADER);
+
+#if defined( DEBUG)         
+DEBUG_TWO_PARA_LEVEL("service name is :",service.c_str())  
+#endif    
+		if (service.empty()) 
+		{
+			pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_SOAPACTIONEMPTY));
+			break; //do .. while(0)
+		}
+		pService = m_pWSDD->GetService(service);
+		if (!pService) {
+			pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_SERVICENOTFOUND));
+			break; //do .. while(0)
+		}
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("Before SetService***************");
+#endif
+    pMsg->SetService(pService);
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("After SetService***************");
+#endif    
+		//Deserialize
+		//---------START XERCES SAX2 SPCIFIC CODE---------//
+		pSoapInput = new MemBufInputSource((const unsigned char*)soap->so.http.ip_soap, soap->so.http.ip_soapcount,"bufferid",false); 
+		if (SUCCESS != m_pDZ->SetStream(pSoapInput)) //this parses the full soap request.
+		{
+			pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_SOAPCONTENTERROR));
+			break; //do .. while(0)
+		}
+		//---------END XERCES SAX2 SPCIFIC CODE---------//
+		//check for soap version in the request and decide whether we support it or not
+		//if we do not support send a soapfault with version mismatch.
+
+		int nSoapVersion = pMsg->m_pDZ->GetVersion();
+		if (nSoapVersion == VERSION_LAST) //version not supported
+		{
+			pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_VERSION_MISMATCH));
+			break; //do .. while(0)		
+		}
+		//Set Soap version in the Serializer
+		pMsg->m_pSZ->setSoapVersion((SOAP_VERSION)nSoapVersion);
+
+		//add namespace URIs of the SoapEnvelope of the response corresponding to the soap version.
+		Attribute* pNS = new Attribute(g_sObjSoapEnvVersionsStruct[nSoapVersion].pchEnvelopePrefix,
+			"xmlns","",g_sObjSoapEnvVersionsStruct[nSoapVersion].pchEnvelopeNamespaceUri);
+		pEnv->addNamespaceDecl(pNS);
+		//add namespace URIs for xsd and xsi
+		pNS = new Attribute("xsd","xmlns","","http://www.w3.org/2001/XMLSchema");
+		pEnv->addNamespaceDecl(pNS);
+		pNS = new Attribute("xsi","xmlns","","http://www.w3.org/2001/XMLSchema-instance");
+		pEnv->addNamespaceDecl(pNS);
+
+		SoapMethod* pSm = m_pDZ->GetMethod();
+		if (pSm) 
+		{
+			string method = pSm->getMethodName();
+
+#if defined( DEBUG)       
+DEBUG_TWO_PARA_LEVEL("method name is :", method.c_str());     
+#endif      
+			if (!method.empty())
+			{
+
+#if defined( DEBUG)         
+DEBUG_ONE_PARA_LEVEL("method is not empty");    
+#endif        
+				//this is done here when we use SAX parser
+				//if we use XML pull parser this check is done within the invoke method of the wrapper class
+				if (pService->IsAllowedMethod(method))
+				{
+
+#if defined( DEBUG)           
+DEBUG_ONE_PARA_LEVEL("method is allowed*****************");         
+#endif          
+					//load actual web service handler
+
+#if defined( DEBUG)           
+DEBUG_ONE_PARA_LEVEL("before loading web service");          
+#endif          
+					m_pWebService = m_pHandlerPool->LoadWebService(pService);
+
+#if defined( DEBUG)           
+DEBUG_ONE_PARA_LEVEL("web service is loaded**********************");          
+#endif          
+					if (!m_pWebService)
+					{
+
+#if defined( DEBUG)             
+DEBUG_ONE_PARA_LEVEL("web service is empty*************************");         
+#endif            
+						pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_COULDNOTLOADSRV));
+						//Error couldnot load web service
+						break; //do .. while(0)
+					}
+				}
+				else
+				{
+					pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_METHODNOTALLOWED));
+					//method is not an exposed allowed method
+					break; //do .. while(0)
+				}
+			}
+			else
+			{
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_NOSOAPMETHOD));
+				//no method to be invoked
+				break; //do .. while(0)
+			}
+		}
+		//create any service specific handlers
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("before getting handlers*********************");     
+#endif    
+		pHandlerList = pService->GetRequestFlowHandlers();
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("after calling request handlers list***************");     
+#endif    
+		if (pHandlerList)
+		{
+
+#if defined( DEBUG)       
+DEBUG_ONE_PARA_LEVEL("after getting request handlers list**************");       
+#endif      
+			if(SUCCESS != m_pHandlerPool->LoadServiceRequestFlowHandlers(pHandlerList))
+			{        
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_COULDNOTLOADHDL));
+				break; //do .. while(0)
+			}
+		}
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("before getting response handlers****************");     
+#endif    
+		pHandlerList = pService->GetResponseFlowHandlers();
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("after getting response handlers****************");     
+#endif    
+		if (pHandlerList)
+		{
+			if(SUCCESS != m_pHandlerPool->LoadServiceResponseFlowHandlers(pHandlerList))
+			{
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_COULDNOTLOADHDL));
+				break; //do .. while(0)
+			}
+		}
+		Invoke(pMsg); //we generate response in the same way even if this has failed
+	}
+	while(0);
+	//and handlers may add headers to the Serializer.
+	//Invoke all handlers including the webservice
+	//in case of failure coresponding soap fault message will be sent
+	if (pMsg) delete pMsg; //MessageData is no longer needed
+	if (pSoapInput) delete pSoapInput; //this should not be done if we use progressive parsing
+	//set soap version to the serializer.
+	//Serialize
+  m_sResponse = "";
+	int iStatus= m_pSZ->getStream(m_sResponse);
+	soap->so.http.op_soap = m_sResponse.c_str();
+	//soap->so.http.op_soap = new char(sResponse.length() + 1); 
+	//strcpy(soap->so.http.op_soap, sResponse.c_str());
+#ifndef _DEBUG //this caused program to crash in debug mode
+	m_pSZ->init();
+#endif 
+	//unload webservice handler
+#ifndef WIN32 //this crashes in Win32 at stl map::find(..) function - I cannot find what is wrong.
+	if (m_pWebService) m_pHandlerPool->UnloadWebService(pService);
+	//Unload service specific handlers
+	pHandlerList = pService->GetRequestFlowHandlers();
+	if (pHandlerList)
+		m_pHandlerPool->UnLoadServiceRequestFlowHandlers(pHandlerList);
+	pHandlerList = pService->GetResponseFlowHandlers();
+	if (pHandlerList)
+		m_pHandlerPool->UnLoadServiceResponseFlowHandlers(pHandlerList);
+#endif
+	return SUCCESS;
+}
+
+int AxisEngine::Invoke(MessageData* pMsg)
+{
+	enum AE_LEVEL {AE_START=1, AE_TRH, AE_GLH, AE_SERH, AE_SERV};
+	HandlerChain* pChain;
+	int ret = FAIL;
+	int level = AE_START;
+	do
+	{
+		//invoke transport request handlers
+		pChain = m_pHandlerPool->GetTransportRequestFlowHandlerChain(pMsg->m_Protocol);
+		if (pChain) {
+			if(SUCCESS != pChain->Invoke(pMsg))
+			{
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_HANDLERFAILED));
+				break; //do .. while (0)
+			}
+
+		}
+		level++; // AE_TRH
+		//invoke global request handlers
+		pChain = m_pHandlerPool->GetGlobalRequestFlowHandlerChain();
+		if (pChain)
+		{
+			if(SUCCESS != pChain->Invoke(pMsg))
+			{
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_HANDLERFAILED));
+				break; //do .. while (0)
+			}		
+		}
+		level++; //AE_GLH
+		//invoke service specific request handlers
+		pChain = m_pHandlerPool->GetServiceRequestFlowHandlerChain();
+		if (pChain)
+		{
+			if(SUCCESS != pChain->Invoke(pMsg))
+			{
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_HANDLERFAILED));
+				break; //do .. while (0)
+			}		
+		}
+		level++; //AE_SERH
+		//call actual web service handler
+		if (m_pWebService)
+			if (SUCCESS != m_pWebService->Invoke(pMsg))
+			{
+				pMsg->m_pSZ->setSoapFault(SoapFault::getSoapFault(SF_WEBSERVICEFAILED));
+				break;
+			}
+		level++; //AE_SERV
+	}
+	while(0);
+
+	switch (level)
+	{
+	case AE_SERV: //everything success
+		ret = SUCCESS;
+		//invoke service specific response handlers
+		//no break;
+	case AE_SERH: //actual web service handler has failed
+		//invoke web service specific response handlers
+		pChain = m_pHandlerPool->GetServiceResponseFlowHandlerChain();
+		if (pChain)
+		{
+			pChain->Invoke(pMsg);
+		}
+		//no break;
+	case AE_GLH: //web service specific handlers have failed
+		//invoke global response handlers
+		pChain = m_pHandlerPool->GetGlobalResponseFlowHandlerChain();
+		if (pChain)
+		{
+			pChain->Invoke(pMsg);
+		}
+		//no break;
+	case AE_TRH: //global handlers have failed
+		pChain = m_pHandlerPool->GetTransportRequestFlowHandlerChain(pMsg->m_Protocol);
+		if (pChain) 
+		{
+			pChain->Invoke(pMsg);
+		}
+		//no break;
+	case AE_START: ;//transport handlers have failed
+	};
+
+	return ret;
+}
+
+void AxisEngine::OnFault(MessageData* pMsg)
+{
+	
+}
+
+int AxisEngine::Initialize()
+{
+	string str(WSDDFILEPATH);
+	TypeMapping::Initialize();
+	URIMapping::Initialize();
+	SoapFault::initialize();
+  
+	if (SUCCESS != m_pWSDD->LoadWSDD(str)) return FAIL;
+  
+	//Load Global Handlers to the pool if configured any
+	WSDDHandlerList* pHandlerList = m_pWSDD->GetGlobalRequestFlowHandlers();
+	if (pHandlerList)
+		if(SUCCESS != m_pHandlerPool->LoadGlobalRequestFlowHandlers(pHandlerList))
+			return FAIL;
+	pHandlerList = m_pWSDD->GetGlobalResponseFlowHandlers();
+	if (pHandlerList)
+		if(SUCCESS != m_pHandlerPool->LoadGlobalResponseFlowHandlers(pHandlerList))
+			return FAIL;
+
+	//Load Transport Handlers to the pool if configured any
+	WSDDTransport* pTransport = m_pWSDD->GetTransport();
+	if (pTransport) {
+		//HTTP
+		pHandlerList = pTransport->GetRequestFlowHandlers(APTHTTP);
+		if (pHandlerList)
+			if(SUCCESS != m_pHandlerPool->LoadTransportRequestFlowHandlers(APTHTTP, pHandlerList))
+				return FAIL;
+		//FTP
+		pHandlerList = pTransport->GetRequestFlowHandlers(APTFTP);
+		if (pHandlerList)
+			if(SUCCESS != m_pHandlerPool->LoadTransportRequestFlowHandlers(APTFTP, pHandlerList))
+				return FAIL;
+		//SMTP
+		pHandlerList = pTransport->GetRequestFlowHandlers(APTSMTP);
+		if (pHandlerList)
+			if(SUCCESS != m_pHandlerPool->LoadTransportRequestFlowHandlers(APTSMTP, pHandlerList))
+				return FAIL;
+	}
+	return SUCCESS;
+}
+
+void AxisEngine::UnInitialize()
+{
+	//nothing to do with m_pWSDD because its destructor deletes its objects
+	//nothing to do with m_pHandlerPool because its destructor deletes its objects
+}
---
+++ c/src/engine/HandlerPool.cpp
@@ -0,0 +1,335 @@
+/* -*- C++ -*- */
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ *
+ *
+ *
+ * @author Susantha Kumara (skumara@virtusa.com)
+ *
+ */
+
+// HandlerPool.cpp: implementation of the HandlerPool class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "HandlerPool.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+#include "../common/Debug.h"
+
+DEBUG_INCLUDE
+
+HandlerPool::HandlerPool()
+{
+	m_pGReqFChain = NULL;
+	m_pGResFChain = NULL;
+	m_pServiceReqFChain = NULL;
+	m_pServiceResFChain = NULL;	
+}
+
+HandlerPool::~HandlerPool()
+{
+	//unload all the handlers
+	LoadedHandler* pLh = NULL;
+	for (map<string, LoadedHandler*>::iterator it = m_Handlers.begin();
+	it != m_Handlers.end(); it++)
+	{
+		pLh = (*it).second;
+		pLh->m_DL->Finalize();
+		delete pLh;
+	}
+	m_Handlers.clear();
+
+	//delete all handlers and chains etc
+	if(m_pGReqFChain) delete m_pGReqFChain;
+	if(m_pGResFChain) delete m_pGResFChain;
+	if(m_pServiceReqFChain) delete m_pServiceReqFChain;
+	if(m_pServiceResFChain) delete m_pServiceResFChain;
+
+	map<AXIS_PROTOCOL_TYPE, HandlerChain*>::iterator it1;
+	for (it1 = m_TReqFChains.begin();it1 != m_TReqFChains.end(); it1++)
+	{
+		delete (*it1).second;
+	}
+	for (it1 = m_TResFChains.begin();it1 != m_TResFChains.end(); it1++)
+	{
+		delete (*it1).second;
+	}
+}
+
+HandlerPool::LoadedHandler::LoadedHandler(string &sFile, int nOptions)
+{
+	m_DL = new HandlerLoader(sFile, nOptions);
+	m_Handler = NULL;
+}
+
+HandlerPool::LoadedHandler::~LoadedHandler()
+{
+	delete m_DL;
+}
+
+BasicHandler* HandlerPool::LoadHandler(WSDDHandler *pHandlerInfo)
+{
+	LoadedHandler* pLh = NULL;
+	if (pHandlerInfo) 
+	{
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("before loaded handler*********************");
+#endif
+    
+		pLh = new LoadedHandler(pHandlerInfo->GetLibName(), RTLD_LAZY);
+
+#if defined( DEBUG)     
+DEBUG_ONE_PARA_LEVEL("after loaded handler**********************");
+#endif
+		if (SUCCESS == pLh->m_DL->Initialize())
+		{
+
+#if defined( DEBUG)       
+DEBUG_ONE_PARA_LEVEL("loaded handler initialized******************");
+#endif
+			BasicHandler* pBh = pLh->m_DL->GetHandler();
+      //BasicHandler* pBh = NULL;
+			if (pBh) 
+			{
+				pLh->m_Handler = pBh;
+				//Add to m_Handlers - place holder
+				m_Handlers[pHandlerInfo->GetLibName()] = pLh;
+				return pBh;
+			}
+			else
+			{
+				//something wrong
+        return NULL;
+			}
+		}
+		else
+		{
+			//Uninitialize, unload and delete pLh
+		}
+	}
+	return NULL;
+}
+
+int HandlerPool::UnLoadHandler(WSDDHandler *pHandlerInfo)
+{
+	LoadedHandler* pLh = NULL;
+	if (m_Handlers.find(pHandlerInfo->GetLibName()) != m_Handlers.end())
+	{
+		pLh = m_Handlers[pHandlerInfo->GetLibName()];
+		pLh->m_DL->Finalize();
+		m_Handlers.erase(pHandlerInfo->GetLibName());
+		delete pLh;
+		return SUCCESS;
+	}
+	return FAIL;
+}
+
+BasicHandler* HandlerPool::GetHandler(WSDDHandler *pHandlerInfo)
+{
+	if (m_Handlers.find(pHandlerInfo->GetLibName()) != m_Handlers.end())
+	{
+		return m_Handlers[pHandlerInfo->GetLibName()]->m_Handler;
+	}
+	return NULL;
+}
+
+HandlerChain* HandlerPool::GetGlobalRequestFlowHandlerChain()
+{
+	return m_pGReqFChain;
+}
+
+HandlerChain* HandlerPool::GetGlobalResponseFlowHandlerChain()
+{
+	return m_pGResFChain;
+}
+
+HandlerChain* HandlerPool::GetTransportRequestFlowHandlerChain(AXIS_PROTOCOL_TYPE Protocol)
+{
+	if (m_TReqFChains.find(Protocol) != m_TReqFChains.end())
+		return m_TReqFChains[Protocol];
+	return NULL;
+}
+
+HandlerChain* HandlerPool::GetTransportResponseFlowHandlerChain(AXIS_PROTOCOL_TYPE Protocol)
+{
+	if (m_TReqFChains.find(Protocol) != m_TReqFChains.end())
+		return m_TResFChains[Protocol];
+	return NULL;
+}
+
+HandlerChain* HandlerPool::GetServiceRequestFlowHandlerChain()
+{
+	return m_pServiceReqFChain;
+}
+
+HandlerChain* HandlerPool::GetServiceResponseFlowHandlerChain()
+{
+	return m_pServiceResFChain;
+}
+
+int HandlerPool::LoadTransportRequestFlowHandlers(AXIS_PROTOCOL_TYPE Protocol, WSDDHandlerList* pHandlerList)
+{
+	HandlerChain* pHc = LoadHandlerChain(pHandlerList);
+	if (pHc)
+	{
+		m_TReqFChains[Protocol] = pHc;
+		return SUCCESS;
+	}
+	return FAIL;
+}
+
+int HandlerPool::LoadTransportResponseFlowHandlers(AXIS_PROTOCOL_TYPE Protocol, WSDDHandlerList* pHandlerList)
+{
+	HandlerChain* pHc = LoadHandlerChain(pHandlerList);
+	if (pHc)
+	{
+		m_TResFChains[Protocol] = pHc;
+		return SUCCESS;
+	}
+	return FAIL;
+}
+
+int HandlerPool::LoadGlobalResponseFlowHandlers(WSDDHandlerList* pHandlerList)
+{
+	m_pGResFChain = LoadHandlerChain(pHandlerList);
+	return (m_pGResFChain != NULL) ? SUCCESS:FAIL;
+}
+
+int HandlerPool::LoadGlobalRequestFlowHandlers(WSDDHandlerList* pHandlerList)
+{
+	m_pGReqFChain = LoadHandlerChain(pHandlerList);
+	return (m_pGReqFChain != NULL) ? SUCCESS:FAIL;
+}
+
+int HandlerPool::LoadServiceRequestFlowHandlers(WSDDHandlerList *pHandlerList)
+{
+	m_pServiceReqFChain = LoadHandlerChain(pHandlerList);
+	return (m_pServiceReqFChain != NULL) ? SUCCESS:FAIL;
+}
+
+int HandlerPool::LoadServiceResponseFlowHandlers(WSDDHandlerList *pHandlerList)
+{
+	m_pServiceResFChain = LoadHandlerChain(pHandlerList);
+	return (m_pServiceResFChain != NULL) ? SUCCESS:FAIL;
+}
+
+void HandlerPool::UnLoadServiceRequestFlowHandlers(WSDDHandlerList *pHandlerList)
+{
+	UnloadHandlerList(pHandlerList);
+	delete m_pServiceReqFChain;
+	m_pServiceReqFChain = NULL;
+}
+
+void HandlerPool::UnLoadServiceResponseFlowHandlers(WSDDHandlerList *pHandlerList)
+{
+	UnloadHandlerList(pHandlerList);
+	delete m_pServiceResFChain;
+	m_pServiceResFChain = NULL;
+}
+
+HandlerChain* HandlerPool::LoadHandlerChain(WSDDHandlerList *pHandlerList)
+{
+	int nLoaded = 0;
+	HandlerChain* pHc = NULL;
+	if (pHandlerList && !pHandlerList->empty()) {
+		pHc = new HandlerChain();
+		BasicHandler* pBh = NULL;;
+		for (WSDDHandlerList::iterator it=pHandlerList->begin();
+		it != pHandlerList->end(); it++)
+		{
+			if ((pBh = LoadHandler(*it)) != NULL)
+			{
+				nLoaded++;
+				pHc->AddHandler(pBh);
+			}
+		}
+		if (0!=nLoaded) 
+		{
+			return pHc;
+		}
+		else 
+		{
+			delete pHc;
+			return NULL;
+		}
+	}
+	return NULL;
+}
+
+void HandlerPool::UnloadHandlerList(WSDDHandlerList *pHandlerList)
+{
+	if (pHandlerList && !pHandlerList->empty()) {
+		for (WSDDHandlerList::iterator it=pHandlerList->begin();
+		it != pHandlerList->end(); it++)
+		{
+			UnLoadHandler(*it);
+		}
+	}
+}
+
+BasicHandler* HandlerPool::LoadWebService(WSDDService* pService)
+{
+	return LoadHandler(pService);
+}
+
+void HandlerPool::UnloadWebService(WSDDService* pService)
+{
+	UnLoadHandler(pService);
+}
