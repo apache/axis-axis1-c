e0375cd25787eda5d2b8f1aa848ef23ba558864c
W.M. Roshan Weerasuriya <roshan@apache.org>
2003-06-25 07:39:08 +0000

    committing c++ code base

---
+++ src/soap/SoapSerializer.cpp
@@ -0,0 +1,204 @@
+/* -*- C++ -*- */
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ *
+ *
+ *
+ * @author Roshan
+ *
+ */
+
+// SoapSerializer.cpp: implementation of the SoapSerializer class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "SoapSerializer.h"
+#include "../common/GDefine.h"
+
+int SoapSerializer::iCounter=0;
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+SoapSerializer::SoapSerializer()
+{
+	m_pSoapEnvelope= NULL;
+}
+
+SoapSerializer::~SoapSerializer()
+{
+	if (m_pSoapEnvelope) delete m_pSoapEnvelope;
+}
+
+int SoapSerializer::setSoapEnvelope(SoapEnvelope *pSoapEnvelope)
+{
+	m_pSoapEnvelope= pSoapEnvelope;
+
+	return SUCCESS;
+}
+
+int SoapSerializer::setSoapHeader(SoapHeader *pSoapHeader)
+{
+	int intStatus= FAIL;
+
+	if(m_pSoapEnvelope) {
+		m_pSoapEnvelope->setSoapHeader(pSoapHeader);
+		intStatus= SUCCESS;
+	}
+
+	return intStatus;
+}
+
+int SoapSerializer::setSoapBody(SoapBody *pSoapBody)
+{
+	int intStatus= FAIL;
+
+	if(m_pSoapEnvelope) {
+		m_pSoapEnvelope->setSoapBody(pSoapBody);
+		intStatus= SUCCESS;
+	}
+
+	return intStatus;
+}
+
+int SoapSerializer::setSoapMethod(SoapMethod *pSoapMethod)
+{
+	int intStatus= FAIL;
+
+	if(m_pSoapEnvelope && (m_pSoapEnvelope->m_pSoapBody)) {
+		m_pSoapEnvelope->m_pSoapBody->m_pSoapMethod= pSoapMethod;
+		intStatus= SUCCESS;
+	}
+
+	return intStatus;
+}
+
+int SoapSerializer::setResponseParam(Param &param)
+{
+	int intStatus= FAIL;
+
+	if(m_pSoapEnvelope && (m_pSoapEnvelope->m_pSoapBody) && (m_pSoapEnvelope->m_pSoapBody->m_pSoapMethod)) {
+		m_pSoapEnvelope->m_pSoapBody->m_pSoapMethod->setOutputParam(param);
+		intStatus= SUCCESS;
+	}
+
+	return intStatus;
+}
+
+int SoapSerializer::setSoapFault(SoapFault *pSoapFault)
+{
+	int intStatus= FAIL;
+
+	if(m_pSoapEnvelope && (m_pSoapEnvelope->m_pSoapBody)) {
+		m_pSoapEnvelope->m_pSoapBody->m_pSoapFault= pSoapFault;
+		intStatus= SUCCESS;
+	}
+
+	return intStatus;
+}
+
+/*string& SoapSerializer::getStream()
+{
+	m_sSerializedStream= "";
+
+	if(m_pSoapEnvelope) {
+		m_sSerializedStream= m_pSoapEnvelope->serialize();
+	}
+
+	return m_sSerializedStream;
+}*/
+
+int SoapSerializer::getStream(string& sSerialized)
+{
+	int iStatus= SUCCESS;
+
+	if(m_pSoapEnvelope) {
+		iStatus= m_pSoapEnvelope->serialize(sSerialized, (SOAP_VERSION)m_iSoapVersion);
+	}
+
+	return iStatus;
+}
+
+void SoapSerializer::init()
+{
+	//initializing the members of the class. This is needed since
+	// the same object instance of this class, may be used to server
+	// several SOAP requests.
+
+	if(m_pSoapEnvelope) {
+		delete m_pSoapEnvelope;
+		m_pSoapEnvelope= NULL;
+	}
+
+	iCounter=0;
+}
+
+int SoapSerializer::setSoapVersion(SOAP_VERSION eSOAP_VERSION)
+{
+	m_iSoapVersion= eSOAP_VERSION;
+	return SUCCESS;
+}
+
+string SoapSerializer::getNewNamespacePrefix()
+{
+	iCounter++;
+	
+	char cCounter[64];	
+	sprintf(cCounter, "%d", iCounter);
+	
+	return string("ns")+ cCounter;
+}
