44b33c652a1f587a1881773c34230baa89cc9711
Damitha N.M. Kumarage <damitha@apache.org>
2003-07-01 04:45:01 +0000

    *** empty log message ***

--- src/common/Debug.h
+++ src/common/Debug.h
@@ -1,22 +1,28 @@
-#ifndef __DEBUG_H_INCLUDED_
-#define __DEBUG_H_INCLUDED_
+#ifndef __AXISLOG_H_INCLUDED_
+#define __AXISLOG_H_INCLUDED_
 
-#include "AxisLog.h"
-//#include <string>
-
-using namespace std;
+#include "GDefine.h"
+#include <list>
+#include <string>
+#include <fstream>
 
 #define DEBUG
+extern unsigned char chEBuf[1024];
+using namespace std;
+class Debug
+{
+public:
+	Debug();
+	virtual ~Debug();
+  string sFileName;
+  const char * FileName;
+  ofstream* fout;
 
-#define DEBUG_INCLUDE extern unsigned char chEBuf[1024];
+  int debug(const string &sLog);
+  int debug(const string &sLog1, const string &sLog2);
 
-#define DEBUG_ONE_PARA_LEVEL(X) \
-sprintf((char*)chEBuf,X); \
-objAxisLog.writeLog(X);
+};
 
-#define DEBUG_TWO_PARA_LEVEL(X,Y) \
-sprintf((char*)chEBuf,X,Y); \
-objAxisLog.writeLog(X,Y);	
-//objAxisLog.writeLog(X);
+static Debug debugger;
 
 #endif
