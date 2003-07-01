561705f8db65551fd047a6cbba84b46c7bf52082
Damitha N.M. Kumarage <damitha@apache.org>
2003-07-01 04:55:14 +0000

    *** empty log message ***

---
+++ c/src/common/Handler.h
@@ -0,0 +1,19 @@
+#if !defined(__HANDLER_INCLUDED__)
+#define __HANDLER_INCLUDED__
+
+#include "BasicHandler.h"
+
+class Handler : public BasicHandler
+{
+public:
+  Handler(){};
+	virtual ~Handler(){};
+  virtual string GetOption(string sArg)=0;
+  virtual void SetOption(string sOption, string Value)=0;
+  virtual void SetOptionList(map<string, string>* OptionList)=0;
+
+protected:
+map<string, string>* m_Option;
+};
+
+#endif //__HANDLER_INCLUDED__
