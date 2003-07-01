d42a96e9020bf30c1b7e07f0a637973d3c925ce9
Damitha N.M. Kumarage <damitha@apache.org>
2003-07-01 07:32:15 +0000

    *** empty log message ***

--- c/src/server/handlers/LogHandler.h
+++ c/src/server/handlers/LogHandler.h
@@ -1,16 +1,23 @@
-#include "../common/MessageData.h"
-#include "../wsdd/WSDDHandler.h"
-#include "../wsdd/WSDDService.h"
-#include "../common/BasicHandler.h"
+#include "../../common/MessageData.h"
+#include "../../wsdd/WSDDHandler.h"
+#include "../../wsdd/WSDDService.h"
+#include "../../common/Handler.h"
+
 #include <list>
 
-class LogHandler : public BasicHandler, WSDDHandler
+class LogHandler : public Handler
 {
 public:
 	LogHandler();
 	virtual ~LogHandler();
+  int Invoke(MessageData* pMsg);
+  void OnFault(MessageData* pMsg);
 
-	int Invoke(MessageData* pMsg);
-	void OnFault(MessageData* pMsg);
-  
+  string GetOption(string sArg);
+  void SetOption(string sOption, string Value);
+  void SetOptionList(map<string, string>* OptionList);
+
+protected:
+  int m_iNumAccess;
+
 };
