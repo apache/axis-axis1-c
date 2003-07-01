cc2409b532003ee395a609a471ff299fd482899a
Damitha N.M. Kumarage <damitha@apache.org>
2003-07-01 04:45:01 +0000

    *** empty log message ***

--- c/src/common/Debug.cpp
+++ c/src/common/Debug.cpp
@@ -1,5 +1,5 @@
                                                                                     //////////////////////////////////////////////////////////////////////
-#include "AxisLog.h"
+#include "Debug.h"
 #include <time.h>
 
 using namespace std;
@@ -8,9 +8,9 @@ using namespace std;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-AxisLog::AxisLog()
+Debug::Debug()
 {
-  sFileName = "/usr/local/apache/logs/AxisLog";
+  sFileName = "/usr/local/axiscpp/axis/logs/Debug";
   if(!sFileName.empty())
   {
     FileName = sFileName.c_str();
@@ -18,46 +18,48 @@ AxisLog::AxisLog()
   }
 }
 
-AxisLog::~AxisLog()
+Debug::~Debug()
 {
   fout->close();
   fout = NULL;
 }
 
-int AxisLog::writeLog(const string &sLog)
+int Debug::debug(const string &sLog)
 {
   if(fout)
   {
     time_t ltime;
     time(&ltime);
     *fout << "time:" << ctime(&ltime) << ":" << sLog.c_str() << endl;
+    sprintf((char*)chEBuf,sLog.c_str());
     return SUCCESS;
   }
   else
   {
     return FAIL;
-  } 
+  }
 }
 
-int AxisLog::writeLog(const string &sLog1, const string &sLog2)
+int Debug::debug(const string &sLog1, const string &sLog2)
 {
-    if(fout)
-    {
-        time_t ltime;
-	time(&ltime);
-	*fout << "time:" << ctime(&ltime) << ":" << sLog1.c_str() << " " << sLog2.c_str() << endl;
-	return SUCCESS;
-    }
-    else
-    {
-        return FAIL;
-    } 
+  if(fout)
+  {
+    time_t ltime;
+    time(&ltime);
+    *fout << "time:" << ctime(&ltime) << ":" << sLog1.c_str() << " " << sLog2.c_str() << endl;
+    sprintf((char*)chEBuf,sLog1.c_str(), sLog2.c_str());
+    return SUCCESS;
+  }
+  else
+  {
+    return FAIL;
+  }
 }
 
 /*
 int main(int argc, char* argv[])
 {
-  objAxisLog.writeLog("damitha*****************");
+  debugger.debug("damitha");
   return 0;
 }
 */
