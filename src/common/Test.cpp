03d61927251a0107c540612dce9f635df223ea96
Damitha N.M. Kumarage <damitha@apache.org>
2003-07-14 04:36:38 +0000

    initial C++ codebase

--- src/common/Test.cpp
+++ src/common/Test.cpp
@@ -1,18 +1,20 @@
-#include "HandlerLoader.h"
 #include <stdio.h>
 #include <string>
 
 using namespace std;
-
-int main(int argc, char *argv[])
+class Test
 {
-	string sLib = "E:/axiscpp/release/win32/WsService.dll";
-	HandlerLoader* pDL = new HandlerLoader(sLib);
-	if (SUCCESS == pDL->Initialize())
-	{	
-		printf("Loaded Library %s\n", sLib.c_str());
-		pDL->Finalize();
-		printf("Unloaded Library %s\n", sLib.c_str());
-	}
+  public:
+  Test(){}
+  virtual ~Test(){}
+  void testMethod()
+  {
+  }
+};
+
+int main()
+{
+	
+	
 	return 0;
 }
