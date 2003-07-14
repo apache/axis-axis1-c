413d3760141ed7f29071de1942f233f29f89eb4c
Damitha N.M. Kumarage <damitha@apache.org>
2003-07-14 04:36:38 +0000

    initial C++ codebase

---
+++ c/src/common/AxisException.cpp
@@ -0,0 +1,87 @@
+#include "AxisException.h"
+
+AxisException::AxisException(int exceptionCode)
+{
+  processException(exceptionCode);
+}
+
+AxisException::AxisException(exception* e)
+{
+  processException(e);
+}
+
+AxisException::AxisException(exception* e, int exceptionCode)
+{
+  processException(e);
+}
+
+void AxisException::processException(exception* e, int exceptionCode)
+{
+  m_sMessage = getMessage(e) + getMessage(exceptionCode);
+}
+
+void AxisException::processException(exception* e)
+{
+  m_sMessage = getMessage(e);
+}
+
+void AxisException::processException(int exceptionCode)
+{
+  m_sMessage = getMessage(exceptionCode);
+}
+
+string AxisException::getMessage(exception* e)
+{
+  string sMessage;
+  exception *objType = dynamic_cast<bad_alloc*> (e);
+  if(objType != NULL)
+  {
+    //cout << "bad_alloc" << endl;
+    sMessage = "thrown by new";
+  }
+
+  objType = dynamic_cast<bad_cast*> (e);
+  if(objType != NULL)
+  {
+    //cout << "bad_cast" << endl;
+    sMessage = "thrown by dynamic_cast when fails with a referenced type";
+  }
+
+  objType = dynamic_cast<bad_exception*> (e);
+  if(objType != NULL)
+  {
+    //cout << "bad_exception" << endl;
+    sMessage = "thrown when an exception doesn't match any catch";
+  }
+
+  objType = dynamic_cast<bad_typeid*> (e);
+  if(objType != NULL)
+  {
+    //cout << "bad_typeid" << endl;
+    sMessage = "thrown by typeid";
+  }
+
+   return sMessage;
+}
+
+string AxisException::getMessage(int e)
+{
+  string sMessage;
+  if(e == 11)
+  sMessage = "AXIS_CUSTOM_ERROR";
+  //cout <<  "AXIS_CUSTOM_ERROR" << endl;
+
+  return sMessage;
+
+}
+
+AxisException::~AxisException() throw()
+{
+
+}
+
+const char* AxisException::what() const throw()
+{
+  return m_sMessage.c_str();
+}
+
