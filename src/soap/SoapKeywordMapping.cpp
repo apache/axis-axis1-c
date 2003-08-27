04b825e65528277d97f46bbea2a8b378f5919184
R.A. Susantha Kumara <susantha@apache.org>
2003-08-27 12:18:39 +0000

    new files added for following tasks
    Avoided using XMLString::transcode function in the xerces parser
    Reduced the use of std::string
    Introduced wide char for char where applicable
    Introduced std::wstring for std::string where applicable
    
    By doing above things achived some 40-50% performance improvement (according to profile given by Rational Quantify)

---
+++ src/soap/SoapKeywordMapping.cpp
@@ -0,0 +1,103 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ *
+ *
+ *
+ * @author Susantha Kumara (skumara@virtusa.com)
+ *
+ */
+// SoapKeywordMapping.cpp: implementation of the SoapKeywordMapping class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "SoapKeywordMapping.h"
+#include "Attribute.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+map<SOAP_VERSION, SoapEnvVersionsStruct> SoapKeywordMapping::m_Map;
+volatile bool SoapKeywordMapping::m_bInit = false;
+
+SoapKeywordMapping::SoapKeywordMapping()
+{
+
+}
+
+SoapKeywordMapping::~SoapKeywordMapping()
+{
+
+}
+
+void SoapKeywordMapping::Initialize()
+{
+	if (!m_bInit)
+	{
+		m_Map[SOAP_VER_1_1] = ObjSoapEnvVersionsStruct[SOAP_VER_1_1];
+		m_Map[SOAP_VER_1_1].pEnv = new Attribute(L"SOAP-ENV",L"xmlns",L"",L"http://schemas.xmlsoap.org/soap/envelope/");
+		m_Map[SOAP_VER_1_1].pXsi = new Attribute(L"xsi",L"xmlns",L"",L"http://www.w3.org/2001/XMLSchema-instance");
+		m_Map[SOAP_VER_1_1].pXsd = new Attribute(L"xsd",L"xmlns",L"",L"http://www.w3.org/2001/XMLSchema");
+		m_Map[SOAP_VER_1_2] = ObjSoapEnvVersionsStruct[SOAP_VER_1_2];
+		m_Map[SOAP_VER_1_2].pEnv = new Attribute(L"env",L"xmlns",L"",L"http://www.w3.org/2003/05/soap-envelope");
+		m_Map[SOAP_VER_1_2].pXsi = new Attribute(L"xsi",L"xmlns",L"",L"http://www.w3.org/2001/XMLSchema-instance");
+		m_Map[SOAP_VER_1_2].pXsd = new Attribute(L"xsd",L"xmlns",L"",L"http://www.w3.org/2001/XMLSchema");
+		m_bInit = true;
+	}
+}
+
+const SoapEnvVersionsStruct& SoapKeywordMapping::Map(SOAP_VERSION nVersion)
+{
+	return m_Map[nVersion];
+}
