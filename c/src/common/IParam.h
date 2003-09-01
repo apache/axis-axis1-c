62d3a5928cea0b00984a46251361b111b65cddff
Damitha N.M. Kumarage <damitha@apache.org>
2003-09-01 13:44:39 +0000

    *** empty log message ***

--- c/src/common/IParam.h
+++ c/src/common/IParam.h
@@ -69,6 +69,9 @@
 #include "IWrapperSoapDeSerializer.h"
 #include "IWrapperSoapSerializer.h"
 
+#include "TypeMapping.h"
+#include "time.h"
+
 #include <string>
 using namespace std;
 
@@ -97,14 +100,27 @@ public:
 typedef union uParamValue
 {
 	int nValue;
+    unsigned int unValue;
+    short sValue;
+    unsigned short usValue;
+    long lValue;
+    unsigned ulValue;
+    char cValue;
+    unsigned char ucValue;
 	float fValue;
 	double dValue;
+    struct tm tValue;/* this will hold the c type tm struct*/
+    long lDuration;/* duration in seconds*/
+    
+
 	//all basic types should come here
 	class ArrayBean* pArray; //this is used to hold arrays
 	class IArrayBean* pIArray; //used by wrapper classes
+
 //	class AccessBean* pBean; //this is used to hold user types
 //	class IAccessBean* pIBean; //used by wrapper classes
 	ComplexObjectHandler* pCplxObj;
+
 	//following is used by the wrapper class to set return value in case of strings	
 	const AxisChar* pStrValue; 
 } uParamValue;
@@ -114,17 +130,50 @@ class IParam
 public:
 	IParam(){};
 	virtual ~IParam(){};
-	virtual int GetInt()=0;
+ 
+    virtual int GetInt() = 0;
+    virtual unsigned int GetUnsignedInt() = 0;
+    virtual short GetShort() = 0;
+    virtual unsigned short GetUnsignedShort() = 0;
+    virtual char GetByte() = 0;
+    virtual unsigned char GetUnsignedByte() = 0;
+    virtual long GetLong() = 0;
+    virtual long GetInteger() = 0;
+    virtual unsigned long GetUnsignedLong() = 0;
+	virtual float GetFloat() = 0;
+    virtual double GetDouble() = 0;
+    virtual double GetDecimal() = 0;
+	virtual const AxisString& GetString() = 0;
+    virtual const AxisString& GetAnyURI() = 0;
+    virtual const AxisString& GetQName() = 0;
+	virtual const AxisString& GetHexString() = 0;
+	virtual const AxisString& GetBase64String() = 0;
+    /*return a tm struct which contain year-month-date-hour-
+      minute-second*/
+    virtual struct tm GetDateTime() = 0;
+    virtual struct tm GetDate() = 0;
+    virtual struct tm GetTime() = 0;
+    /*return a tm struct which contain years-months-dates-hours-
+      minutes-seconds which represents a duration*/
+    virtual long GetDuration() = 0;
+
+    virtual int GetArraySize()= 0;
+	virtual int SetArrayElements(void* pElements)= 0;
+	//virtual int SetUserType(IAccessBean* pObject)= 0;
+	//virtual void SetName(const AxisChar* sName)= 0;
+	/*virtual int GetInt()=0;
 	virtual float GetFloat()=0;
 	virtual const AxisString& GetString()=0;
 	virtual const AxisString& GetHexString()=0;
 	virtual const AxisString& GetBase64String()=0;
 	virtual int GetArraySize()=0;
-	virtual int SetArrayElements(void* pElements)=0;
+	virtual int SetArrayElements(void* pElements)=0;*/
+
 	virtual int SetArrayElements(void* pObject, AXIS_DESERIALIZE_FUNCT pDZFunct, AXIS_OBJECT_DELETE_FUNCT pDelFunct, AXIS_OBJECT_SIZE_FUNCT pSizeFunct)=0;
 //	virtual int SetUserType(IAccessBean* pObject)=0;
 	virtual int SetUserType(void* pObject, AXIS_DESERIALIZE_FUNCT pDZFunct, AXIS_OBJECT_DELETE_FUNCT pDelFunct)=0;
 	virtual void SetName(const AxisChar* sName)=0;
+
 };
 
 #endif // !defined(AFX_IPARAM_H__25C278BB_5875_49E6_A3EC_B6AD3E543D69__INCLUDED_)
