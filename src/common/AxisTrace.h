ff4a690f08295582cba333211472b602cbc0d1cb
Damitha N.M. Kumarage <damitha@apache.org>
2003-09-04 06:51:41 +0000

    PR: Obtained from: Submitted by: Reviewed by:
    log:To trace debugging messages we have a class called Debug. As this is name is not appropriate
    to use I changed it to AxisTrace. So I modified the above files appropriately.

--- src/common/AxisTrace.h
+++ src/common/AxisTrace.h
@@ -55,7 +55,7 @@
  *
  *
  *
- * @author Damitha Kumarage (damitha@opensource.lk)
+ * @author Damitha Kumarage (damitha@opensource.lk, damitha@jkcsworld.com)
  *
  */
 
@@ -67,32 +67,38 @@
 #include <string>
 #include <fstream>
 
-//#define DEBUG
-#if defined(DEBUG)
-  #define DEBUG1(X) debugger.debug(X,__FILE__,__LINE__);  
-  #define DEBUG2(X,Y) debugger.debug(X,Y,__FILE__,__LINE__);
+#ifdef WIN32
+    #define TRACE_PATH "E:\\Program Files\\Apache Group\\Apache\\Axis\\logs\\Debug.txt";
+#else //linux
+    #define TRACE_PATH "/usr/local/axiscpp/axis/AxisLog";
 #endif
-#if !defined(DEBUG)
-  #define DEBUG1(X) "";
-  #define DEBUG2(X,Y) ""; 
+
+//#define AXISTRACE
+#if defined(AXISTRACE)
+  #define AXISTRACE1(X) tracer.trace(X,__FILE__,__LINE__);
+  #define AXISTRACE2(X,Y) tracer.trace(X,Y,__FILE__,__LINE__);
+#endif
+#if !defined(AXISTRACE)
+  #define AXISTRACE1(X) "";
+  #define AXISTRACE2(X,Y) "";
 #endif
 
 //extern unsigned char chEBuf[1024];
 using namespace std;
-class Debug
+class AxisTrace
 {
 public:
-	Debug();
-	virtual ~Debug();
+	AxisTrace();
+	virtual ~AxisTrace();
   string sFileName;
   const char * FileName;
   ofstream* fout;
 
-  int debug(const string &sLog, string arg2, int arg3);
-  int debug(const string &sLog1, const string &sLog2, string arg3, int arg4);
+  int trace(const string &sLog, string arg2, int arg3);
+  int trace(const string &sLog1, const string &sLog2, string arg3, int arg4);
 
 };
 
-static Debug debugger;
+static AxisTrace tracer;
 
 #endif
