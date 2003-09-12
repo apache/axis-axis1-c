9bc6a14589e2edb7ad89496919fe112816e149da
W.M. Roshan Weerasuriya <roshan@apache.org>
2003-09-12 14:25:24 +0000

    minor changes

--- src/server/simple_axis_server/ServerHelper.h
+++ src/server/simple_axis_server/ServerHelper.h
@@ -1,5 +1,10 @@
+#if !defined(AFX_SERVERHELPER_H__INCLUDED_)
+#define AFX_SERVERHELPER_H__INCLUDED_
+
 #include <string>
 
 using namespace std;
 
 int getSeperatedHTTPParts(string sClientReqStream, string& sHTTPHeaders, string& sHTTPBody);
+
+#endif // !defined(AFX_SERVERHELPER_H__INCLUDED_)
