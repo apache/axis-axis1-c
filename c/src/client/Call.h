531d47a9e3f82efcf1bef9509735168992b96019
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2003-09-16 08:40:35 +0000

    modified to support simple types. Added a set of AddParameter methods

--- c/src/client/Call.h
+++ c/src/client/Call.h
@@ -11,16 +11,33 @@
 class Call  
 {
 public:
+	int SetHeader(char * key, char* value);
+	int SetProtocol(AXIS_PROTOCOL_TYPE protocol);
 	int UnInitialize();
 	int Initialize();
-	const uParamValue& GetResult();
+	Param * GetResult();
 	int Invoke();
 	void SetReturnType(void* pObject, void* pDZFunct, void* pDelFunct);
 	void SetReturnType(XSDTYPE nType);
+
 	void AddParameter(void* pObject, void* pSZFunct, void* pDelFunct, const char* pchName);
 	void AddParameter(IArrayBean* pArrayBean, const char* pchName);
-	void AddParameter(XSDTYPE nType, uParamValue Value, const char* pchName);
+
+	void AddParameter(int nValue,const char* pchName);
+	void AddParameter(unsigned int unValue,const char* pchName);
+	void AddParameter(short sValue,const char* pchName);
+	void AddParameter(unsigned short usValue,const char* pchName);
+	void AddParameter(long lValue,const char* pchName);
+	void AddParameter(unsigned long ulValue,const char* pchName);
+	void AddParameter(char cValue,const char* pchName);
+	void AddParameter(unsigned char ucValue,const char* pchName);
+	void AddParameter(float fValue,const char* pchName);
+	void AddParameter(double dValue,const char* pchName);
+	void AddParameter(struct tm tValue,const char* pchName);
+	void AddParameter(const AxisChar* pStrValue,const char* pchName);
+
 	void SetOperation(const char* pchOperation, const char* pchNamespace);
+	void SetOperation(const char* pchOperation, const char* pchNamespace,const char* pchName);
 	int SetEndpointURI(const char* pchEndpointURI);
 	Call();
 	virtual ~Call();
@@ -33,7 +50,7 @@ private:
 	XSDTYPE m_nReturnType;
 	ComplexObjectHandler m_ReturnCplxObj;
 	Ax_soapstream m_Soap;
-	Param m_Param;
+	Param * m_Param;
 };
 
 #endif // !defined(AFX_CALL_H__D13E5626_0A9B_43EA_B606_364B98CEDAA8__INCLUDED_)
