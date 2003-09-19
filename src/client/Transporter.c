0c996d8dd0b0a7d03ffec2ca6d4f481a465cf521
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2003-09-19 14:53:50 +0000

    trivial change

--- src/client/Transporter.c
+++ src/client/Transporter.c
@@ -3,11 +3,12 @@
 #include <iostream.h>
 #include "../common/Packet.h"
 
-char * resp = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><SOAP-ENV:Body><ns1:AddResponse xmlns:ns1=\"http://www.opensource.lk/CPPService\"><AddReturn xsi:type=\"xsd:int\">7</AddReturn></ns1:AddResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>";
-int len = 354;
+//char * resp = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><SOAP-ENV:Body><ns1:AddResponse xmlns:ns1=\"http://www.opensource.lk/CPPService\"><AddReturn xsi:type=\"xsd:int\">7</AddReturn></ns1:AddResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>";
+//int len = 354;
 
-//char * resp = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><SOAP-ENV:Body><ns1:AddPointResponse xmlns:ns1=\"http://www.opensource.lk/CPPService\"><Point xsi:type=\"ns3:Point xmlns:ns3=\"http://www.opensource.lk/Point\"><x xsi:type=\"xsd:int\">33</x><y xsi:type=\"xsd:int\">44</y></Point></ns1:AddResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>";
-//int len = 450;
+
+char * resp = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><SOAP-ENV:Body><ns1:AddPointResponse xmlns:ns1=\"http://www.opensource.lk/CPPService\"><Point xsi:type=\"ns2:Point\" xmlns:ns2=\"http://www.opensource.lk/Point\"><x xsi:type=\"xsd:int\">444</x><y xsi:type=\"xsd:int\">999</y></Point></ns1:AddPointResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>";
+int len = 500;
 
 
 int send_response_bytes(const char* res, const void* opstream)
@@ -20,7 +21,7 @@ int get_request_bytes(char* req, int reqsize, int* retsize, const void* ipstream
 {
 	printf("%s","inside get request bytes");
 	strcpy(req, resp);
-	*retsize = 354;
+	*retsize = len;
 	return 0;
 }
 
