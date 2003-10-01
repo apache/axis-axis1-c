19060bf740ca19f10f5a2b741384d9da10544fa3
R.A. Susantha Kumara <susantha@apache.org>
2003-10-01 12:47:40 +0000

    Improved the Call class so that it supports passing and returning arrays by the client applications. Changed few places in the code base too as required.

--- c/src/engine/client/Call.cpp
+++ c/src/engine/client/Call.cpp
@@ -83,6 +83,8 @@ Call::Call()
 	m_Soap.so.http.ip_headers = NULL;
 	initialize_module(0, WSDDFILEPATH);
 	m_pTransport = NULL;
+	m_nReturnType = XSD_UNKNOWN;
+	m_nArrayType = XSD_UNKNOWN;
 }
 
 Call::~Call()
@@ -176,10 +178,14 @@ void Call::AddParameter(const AxisChar* pStrValue,const char* pchName)
 	pRetParam->SetName(pchName);
 }
 
-void Call::AddParameter(IArrayBean *pArrayBean, const char* pchName)
+void Call::AddParameter(Axis_Array* pArray, void* pSZFunct, void* pDelFunct, void* pSizeFunct, const char* pchTypeName)
 {
-	IParam* pRetParam = m_pIWSSZ->AddOutputParam(pArrayBean);
-	pRetParam->SetName(pchName);
+	IArrayBean* pAb = m_pIWSSZ->makeArrayBean((void*)(pArray->m_Array), pSZFunct, pDelFunct, pDelFunct);
+	pAb->AddDimension(pArray->m_Size);
+	pAb->SetItemName("item");
+	pAb->SetTypeName(pchTypeName);
+	IParam* pRetParam = m_pIWSSZ->AddOutputParam(pAb);
+	pRetParam->SetName(pchTypeName);
 }
 
 void Call::AddParameter(void *pObject, void *pSZFunct, void *pDelFunct, const char* pchName)
@@ -193,14 +199,43 @@ void Call::SetReturnType(XSDTYPE nType)
 	m_nReturnType = nType;
 }
 
-void Call::SetReturnType(void *pObject, void *pDZFunct, void *pDelFunct, const char* pchTypeName, const char * pchUri)
+void Call::SetReturnType(void *pDZFunct, void* pCreFunct, void *pDelFunct, const char* pchTypeName, const char * pchUri)
 {
 	m_nReturnType = USER_TYPE;
-	m_ReturnCplxObj.pObject = pObject;
+	m_ReturnCplxObj.pObject = NULL;
 	m_ReturnCplxObj.pDZFunct = (AXIS_DESERIALIZE_FUNCT)pDZFunct;
+	m_ReturnCplxObj.pCreFunct = (AXIS_OBJECT_CREATE_FUNCT)pCreFunct;
 	m_ReturnCplxObj.pDelFunct = (AXIS_OBJECT_DELETE_FUNCT)pDelFunct;
 	m_ReturnCplxObj.m_TypeName = pchTypeName;
 	m_ReturnCplxObj.m_URI = pchUri;
+}
+
+/**
+ * This function is used to set that the return type is an array of complex types
+ */
+void Call::SetReturnType(Axis_Array* pArray, void* pDZFunct, void* pCreFunct, void* pDelFunct, const char* pchTypeName, const char* pchUri)
+{
+	m_pArray = pArray;
+	m_nReturnType = XSD_ARRAY;
+	m_nArrayType = USER_TYPE;
+	m_ReturnCplxObj.pDZFunct = (AXIS_DESERIALIZE_FUNCT)pDZFunct;
+	m_ReturnCplxObj.pCreFunct = (AXIS_OBJECT_CREATE_FUNCT)pCreFunct;
+	m_ReturnCplxObj.pDelFunct = (AXIS_OBJECT_DELETE_FUNCT)pDelFunct;
+	m_ReturnCplxObj.m_TypeName = pchTypeName;
+	m_ReturnCplxObj.m_URI = pchUri;
+}
+
+/**
+ * This function is used to set that the return type is an array of basic types.
+ * @param pArray Array to which the deserialized object array is set an returned
+ *				 to the client application.
+ * @param nType Basic type of the array elements
+ */
+void Call::SetReturnType(Axis_Array* pArray, XSDTYPE nType)
+{
+	m_pArray = pArray;
+	m_nReturnType = XSD_ARRAY;
+	m_nArrayType = nType;
 }
 
 int Call::Invoke()
@@ -209,29 +244,55 @@ int Call::Invoke()
 	Param *pParam = NULL;
 	if (SUCCESS == (nStatus = m_pAxisEngine->Process(&m_Soap)))
 	{
-		if (m_nReturnType == USER_TYPE)
+		if (USER_TYPE == m_nReturnType)
 		{
 			/*
-			The first element of the soap message (after the soap			
-			body element) is taken as a parameter itself so that
-			the return type can be checked. So need to call GetParam
+			The second element of the soap message (after the method element) 
+			is taken as a parameter. But if it is a custom type remove the 
+			wrapper element that describes the type. Optionally you can check
+			whether the returned type is of expected type.
 			on the deserializer before getting the actual params
 			*/
 			pParam = (Param*)m_pIWSDZ->GetParam();
-
-//			if (pParam && (m_ReturnCplxObj.m_TypeName == pParam->GetTypeName()))
-//			{
+			/*
+			Following lines check whether the returned type is expected type or not.
+			But this type checking is optional. So commented.
+			if (pParam && (m_ReturnCplxObj.m_TypeName == pParam->GetTypeName()))
+			{
 				m_ReturnCplxObj.pDZFunct(m_ReturnCplxObj.pObject, m_pMsgData->m_pDZ);
-//			}
-//			else
-//			{
-//				return FAIL;
-//			}
+			}
+			else
+			{
+				return FAIL;
+			}
+			*/
+			if (!m_ReturnCplxObj.pCreFunct || !m_ReturnCplxObj.pDZFunct)
+				return FAIL; 
+			m_ReturnCplxObj.pObject = m_ReturnCplxObj.pCreFunct();
+			if (!m_ReturnCplxObj.pObject)
+				return FAIL;
+			m_ReturnCplxObj.pDZFunct(m_ReturnCplxObj.pObject, m_pMsgData->m_pDZ);
 
 		}
-		else if (m_nReturnType == XSD_ARRAY)
+		else if (XSD_ARRAY == m_nReturnType)
 		{
-			
+			IParam *param0 = m_pIWSDZ->GetParam(); 
+			/* now we know that this is an array. if needed we can check that too */
+			if (!m_pArray) return FAIL; //No array expected ?
+			m_pArray->m_Size = param0->GetArraySize();
+			if (m_pArray->m_Size > 0)
+			{
+				if (SUCCESS == MakeArray()) //Array is allocated successfully
+				{
+					if (USER_TYPE == m_nArrayType)
+						param0->SetArrayElements((void*)(m_pArray->m_Array), m_ReturnCplxObj.pDZFunct, m_ReturnCplxObj.pDelFunct, m_ReturnCplxObj.pSizeFunct);
+					else
+						param0->SetArrayElements((void*)(m_pArray->m_Array));
+					m_pIWSDZ->Deserialize(param0,0);
+				}
+			}
+			else 
+				return FAIL; //CF_ZERO_ARRAY_SIZE_ERROR
 		}
 		else //basic type
 		{
@@ -247,8 +308,26 @@ uParamValue Call::GetResult()
 	return m_uReturnValue;
 }
 
+void Call::GetResult(void** pReturn)
+{
+	if (m_ReturnCplxObj.pObject)
+	{
+		*pReturn = m_ReturnCplxObj.pObject;
+		m_ReturnCplxObj.pObject = NULL; //note that returned object is handed over to the client.
+	}
+	else
+	{
+		*pReturn = NULL;
+	}
+}
+
 int Call::Initialize()
 {
+	/* 
+	   Initialize re-usable objects of this instance (objects may have been populated by
+	   the previous call.
+	 */
+	InitializeObjects();
 	m_Soap.sessionid = "somesessionid1234";
 	if (SUCCESS != OpenConnection()) return FAIL;
 	if (m_pAxisEngine) delete m_pAxisEngine;
@@ -269,6 +348,20 @@ int Call::Initialize()
 		return SUCCESS;
 	}
 	return FAIL;
+}
+
+void Call::InitializeObjects()
+{
+	m_nReturnType = XSD_UNKNOWN;
+	m_pArray = NULL;
+	m_nArrayType = XSD_UNKNOWN;
+	m_ReturnCplxObj.m_TypeName = "";
+	m_ReturnCplxObj.m_URI = "";
+	m_ReturnCplxObj.pCreFunct = NULL;
+	m_ReturnCplxObj.pDelFunct = NULL;
+	m_ReturnCplxObj.pObject = NULL;
+	m_ReturnCplxObj.pSizeFunct = NULL;
+	m_ReturnCplxObj.pSZFunct = NULL;
 }
 
 int Call::UnInitialize()
@@ -321,4 +414,70 @@ void Call::CloseConnection()
 void Call::SetSOAPVersion(SOAP_VERSION version)
 {
 	m_pIWSSZ->setSoapVersion(version);
+}
+
+/**
+ * This function will create (allocate memory) to an array of objects depending on
+ * the information available in m_pArray and m_nArrayType. Basically it gets the 
+ * array size from m_pArray->m_Size and element type from m_nArrayType. In case of 
+ * complex types it uses the Create function provided in m_ReturnCplxObj structure
+ */
+int Call::MakeArray()
+{
+	if (USER_TYPE == m_nArrayType)
+	{
+		m_pArray->m_Array = m_ReturnCplxObj.pCreFunct(true, m_pArray->m_Size);
+	}
+	else
+	{
+		switch (m_nArrayType)
+		{
+		case XSD_INT:
+		case XSD_UNSIGNEDINT:
+			m_pArray->m_Array = new int[m_pArray->m_Size];
+			break;
+		case XSD_FLOAT:
+			m_pArray->m_Array = new float[m_pArray->m_Size];
+			break;
+		case XSD_STRING:
+		case XSD_HEXBINARY:
+		case XSD_BASE64BINARY:
+		case XSD_ANYURI:
+		case XSD_QNAME:
+		case XSD_NOTATION:
+			m_pArray->m_Array = new AxisString[m_pArray->m_Size];
+			break;
+		case XSD_LONG:
+		case XSD_UNSIGNEDLONG:
+		case XSD_INTEGER:
+		case XSD_DURATION:
+			m_pArray->m_Array = new long[m_pArray->m_Size];
+			break;
+		case XSD_SHORT:
+		case XSD_UNSIGNEDSHORT:
+			m_pArray->m_Array = new short[m_pArray->m_Size];
+			break;
+		case XSD_BYTE:
+		case XSD_UNSIGNEDBYTE:
+			m_pArray->m_Array = new char[m_pArray->m_Size];
+			break;
+		case XSD_DOUBLE:
+		case XSD_DECIMAL:
+			m_pArray->m_Array = new double[m_pArray->m_Size];
+			break;
+		case XSD_DATETIME:
+		case XSD_TIME:
+		case XSD_DATE:
+		case XSD_YEARMONTH:
+		case XSD_YEAR:
+		case XSD_MONTHDAY:
+		case XSD_DAY:
+		case XSD_MONTH:
+			m_pArray->m_Array = new tm[m_pArray->m_Size];
+			break;
+		default:
+			return FAIL;
+		}
+	}
+	return SUCCESS;
 }
