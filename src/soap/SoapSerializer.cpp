3c828b22def40e2e1d8dd3b5974ebfbd4ee1184e
R.A. Susantha Kumara <susantha@apache.org>
2003-10-02 05:58:30 +0000

    Added <?xml version='1.0' encoding='utf-8' ?> to the serialized SOAP stream

--- src/soap/SoapSerializer.cpp
+++ src/soap/SoapSerializer.cpp
@@ -288,6 +288,7 @@ int SoapSerializer::SetOutputStream(const Ax_soapstream* pStream)
 	int iStatus= SUCCESS;
 
 	if(m_pSoapEnvelope) {
+		*this << "<?xml version='1.0' encoding='utf-8' ?>";
 		iStatus= m_pSoapEnvelope->serialize(*this, (SOAP_VERSION)m_iSoapVersion);
 	}
 	return iStatus;
