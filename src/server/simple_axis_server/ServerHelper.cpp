2fe709aed71cebfe1ba96ca884de8ac915f18bf1
R.A. Susantha Kumara <susantha@apache.org>
2003-10-20 08:24:11 +0000

    removed .h and .hpp files from the c/src/... folders and moved them to c/include/axis/... folders

--- src/server/simple_axis_server/ServerHelper.cpp
+++ src/server/simple_axis_server/ServerHelper.cpp
@@ -68,6 +68,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <axis/server/simple_axis_server/ServerHelper.h>
+#include <axis/common/AxisUtils.h>
 
 int getSeperatedHTTPParts(string sClientReqStream, string& sHTTPHeaders, string& sHTTPBody, map<HTTP_MAP_KEYWORDS, HTTP_MAP_TYPE*> *map_HTTP_Headers) {
 			
