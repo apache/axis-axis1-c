22c303d8021a9094b9ed2481548eb31517abb9f8
dev-null@apache.org <dev-null@apache.org>
2003-10-30 08:10:33 +0000

    This commit was manufactured by cvs2svn to create branch 'CWrapperSupport'.

--- contrib/axismora/docs/conf-bak/scripts/windows/axis.bat
+++
@@ -1,1 +0,0 @@
-java -classpath .;../lib/servlet.jar;../lib/axis-ant.jar;../lib/axis.jar;../lib/commons-discovery.jar;../lib/commons-logging.jar;../lib/jaxrpc.jar;../lib/log4j-1.2.4.jar;../lib/saaj.jar;../lib/wsdl4j.jar;../lib/xpp3-1.1.3.2.jar;../lib/xercesImpl.jar;../lib/xml-apis.jar;../lib/xmlParserAPIs.jar org.apache.axis.transport.http.SimpleAxisServer
