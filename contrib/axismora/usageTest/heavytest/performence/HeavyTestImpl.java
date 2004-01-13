919767304cd9718d4f6d5426f6fd3446aedef740
Hemapani Srinath Perera <hemapani@apache.org>
2004-01-13 10:34:17 +0000

    It is a mistake to put the genarated code in to the CVS that will change with every simple change. We should replace them with just a set of wsdl's and put code to genarate the gen using wsdl2Ws. This commit replace them.

--- contrib/axismora/usageTest/heavytest/performence/HeavyTestImpl.java
+++
@@ -1,8 +0,0 @@
-package heavytest.performence;
-/**
-* This is the service class build with the information given
- * by the wsdl file the user should fill the logic here 
- */
-public class HeavyTestImpl  {
-	public heavytest.performence.Details[] test(heavytest.performence.Details[] param0){return param0;}
-}
--- contrib/axismora/usageTest/xmltoday_delayed_quotes/GetQuoteImpl.java
+++
@@ -1,9 +0,0 @@
-package xmltoday_delayed_quotes;
-/**
-* This is the service class build with the information given
- * by the wsdl file the user should fill the logic here 
- */
-public class GetQuoteImpl  {
-	public float getQuote(java.lang.String param0){return 2345;}
-	public java.lang.String test(){return "return value";}
-}
