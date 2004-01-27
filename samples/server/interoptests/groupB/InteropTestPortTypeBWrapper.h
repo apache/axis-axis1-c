740b0d7200d1844ade6b7de79c05341aadf2aaeb
Damitha N.M. Kumarage <damitha@apache.org>
2004-01-27 05:47:07 +0000

    Copied the samples from src/client/samples and from src/server/samples into root. Samples will remain in the orginal places for a grace period of time.

---
+++ samples/server/interoptests/groupB/InteropTestPortTypeBWrapper.h
@@ -0,0 +1,38 @@
+/////////////////////////////////////////////////////////////////////////////
+// This is the Service Class genarated by the tool WSDL2Ws
+//		InteropTestPortTypeBWrapper.h: interface for the InteropTestPortTypeBWrapperclass.
+//
+//////////////////////////////////////////////////////////////////////
+#if !defined(__INTEROPTESTPORTTYPEBWRAPPER_SERVERWRAPPER_H__INCLUDED_)
+#define __INTEROPTESTPORTTYPEBWRAPPER_SERVERWRAPPER_H__INCLUDED_
+
+#include "InteropTestPortTypeB.h"
+#include <axis/common/WrapperClassHandler.h>
+#include <axis/common/IMessageData.h>
+#include <axis/common/GDefine.h>
+#include <axis/common/IWrapperSoapDeSerializer.h>
+#include <axis/common/IWrapperSoapSerializer.h>
+
+class InteropTestPortTypeBWrapper : public WrapperClassHandler
+{
+private:// Actual web service object
+	InteropTestPortTypeB *pWs;
+public:
+	InteropTestPortTypeBWrapper();
+public:
+	virtual ~InteropTestPortTypeBWrapper();
+public://implementation of WrapperClassHandler interface
+	int AXISCALL Invoke(IMessageData* mc);
+	void AXISCALL OnFault(IMessageData* pMsg);
+	int AXISCALL Init();
+	int AXISCALL Fini();
+	AXIS_BINDING_STYLE AXISCALL GetBindingStyle(){return RPC_ENCODED;};
+private://Methods corresponding to the web service methods
+	int echoStructAsSimpleTypes(IMessageData* mc);
+	int echoSimpleTypesAsStruct(IMessageData* mc);
+	int echo2DStringArray(IMessageData* mc);
+	int echoNestedStruct(IMessageData* mc);
+	int echoNestedArray(IMessageData* mc);
+};
+
+#endif // !defined(__INTEROPTESTPORTTYPEBWRAPPER_SERVERWRAPPER_H__INCLUDED_)
