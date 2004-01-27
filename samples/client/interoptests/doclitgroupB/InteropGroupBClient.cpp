8276945eef0623610a48600dc19ef1018d3e8d92
Damitha N.M. Kumarage <damitha@apache.org>
2004-01-27 05:47:07 +0000

    Copied the samples from src/client/samples and from src/server/samples into root. Samples will remain in the orginal places for a grace period of time.

---
+++ samples/client/interoptests/doclitgroupB/InteropGroupBClient.cpp
@@ -0,0 +1,71 @@
+// InteropGroupBClient.cpp : Defines the entry point for the console application.
+//
+#include "InteropTestPortTypeB.h"
+
+#define ARRAYSIZE 5
+
+int main(int argc, char* argv[])
+{
+
+	int x;
+	InteropTestPortTypeB ws;
+	//we do not support multi-dimensional arrays.
+	//ws.echo2DStringArray
+
+	//testing Nested Arrays
+	SOAPArrayStruct sas;
+	sas.varFloat = 12345.67890;
+	sas.varInt = 5000;
+	sas.varString = "varString content of SOAPArrayStruct";
+	sas.varArray.m_Array = new AxisChar*[ARRAYSIZE];
+	sas.varArray.m_Size = ARRAYSIZE;
+	for (x=0; x<ARRAYSIZE; x++)
+	{
+		sas.varArray.m_Array[x] = "content of string array element";
+	}
+	printf("invoking echoNestedArray...\n");
+	if (ws.echoNestedArray(&sas) != NULL)
+		printf("successful\n");
+	else
+		printf("failed\n");
+
+	//testing Nested Structs
+	SOAPStructStruct sss;
+	sss.varFloat = 12345.67890;
+	sss.varInt = 5000;
+	sss.varString = "varString content of SOAPStructStruct";
+	sss.varStruct = new SOAPStruct();
+	sss.varStruct->varFloat = 67890.12345;
+	sss.varStruct->varInt = 54321;
+	sss.varStruct->varString = "varString content of SOAPStruct";
+	printf("invoking echoNestedStruct...\n");
+	if (ws.echoNestedStruct(&sss) != NULL)
+		printf("successful\n");
+	else
+		printf("failed\n");
+
+	//testing echo Simple types as struct
+	char* str = strdup("content of string passed");
+	printf("invoking echoSimpleTypesAsStruct...\n");
+	if (ws.echoSimpleTypesAsStruct(12345.67890, 5000, str) != NULL)
+		printf("successful\n");
+	else
+		printf("failed\n");
+
+	//testing echo Struct as simple types.
+	SOAPStruct ss;
+	ss.varFloat = 12345.67890;
+	ss.varInt = 5000;
+	ss.varString = "content of string passed";
+	char* outStr;
+	int outInt;
+	float outFloat;
+	printf("invoking echoStructAsSimpleTypes...\n");
+	ws.echoStructAsSimpleTypes(&ss, &outStr, &outInt, &outFloat);
+	if (outInt == 5000 && (0 == strcmp(outStr, "content of string passed")) && outFloat > 12345.67)
+		printf("successful\n");
+	else
+		printf("failed\n");	
+	getchar();
+	return 0;
+}
