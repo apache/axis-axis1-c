4e041d84668f9000f6fa34b4e4df0b15c29d6b33
Damitha N.M. Kumarage <damitha@apache.org>
2004-02-03 14:17:11 +0000

    PR: Obtained from: Submitted by: Reviewed by:

--- samples/client/interoptests/base/InteropTestPortType.cpp
+++ samples/client/interoptests/base/InteropTestPortType.cpp
@@ -18,7 +18,7 @@ InteropTestPortType::InteropTestPortType()
 {
 	m_pCall = new Call();
 	m_pCall->SetProtocol(APTHTTP);
-	m_pCall->SetEndpointURI("http://localhost:5555/axis/InteropBase");
+	m_pCall->SetEndpointURI("http://localhost:80/axis/InteropBase");
 }
 
 InteropTestPortType::~InteropTestPortType()
