b7e351d7a6362031fd180d06d43fab2daec980fd
Hemapani Srinath Perera <hemapani@apache.org>
2004-02-10 04:53:00 +0000

    Add the acsess to the MessageContext more nicely ... rather than using the static varable which is Ugly.
    But we servcie should implements ContextAccssible interface ..
    but that is gererated so no problems

--- contrib/ews/src/org/apache/geronimo/ews/ws4j2ee/wsutils/J2EEProvider.java
+++ contrib/ews/src/org/apache/geronimo/ews/ws4j2ee/wsutils/J2EEProvider.java
@@ -1,19 +1,30 @@
 package org.apache.geronimo.ews.ws4j2ee.wsutils;
 
+import org.apache.axis.AxisFault;
 import org.apache.axis.MessageContext;
 import org.apache.axis.providers.java.RPCProvider;
 
 /**
- * register the MessageContext in the jax-rpc runtime of the JSR109
- * 
+ * register the MessageContext in the jax-rpc runtime of the JSR109 
  * @author Srinath Perera(hemapani@opensource.lk)
  */
-public class J2EEProvider extends RPCProvider {
-    protected Object makeNewServiceObject(MessageContext msgContext,
-                                          String clsName)
-            throws Exception {
-        JaxRpcRuntime.setMessageContext(msgContext);
-        return super.makeNewServiceObject(msgContext, clsName);
-    }
+public class J2EEProvider extends RPCProvider{
+	protected Object makeNewServiceObject(
+		MessageContext msgContext,
+		String clsName)
+		throws Exception {
+			ContextAccssible webservice =  
+				(ContextAccssible)super.makeNewServiceObject(msgContext, clsName);
+			webservice.setMessageContext(msgContext);
+			System.out.println("message Context set");
+			return webservice;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.axis.Handler#invoke(org.apache.axis.MessageContext)
+	 */
+	public void invoke(MessageContext msgContext) throws AxisFault {
+		super.invoke(msgContext);
+	}
 
 }
