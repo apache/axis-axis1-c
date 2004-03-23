21e2bad8911ad91cef44e57285dde71f94b7e610
R.A. Susantha Kumara <susantha@apache.org>
2004-03-23 13:25:01 +0000

    Functionality to serialize the WSDD objects to WSDD config file

--- c/src/wsdd/WSDDService.cpp
+++ c/src/wsdd/WSDDService.cpp
@@ -200,3 +200,63 @@ const list<AxisString> WSDDService::getAllowedMethods() const
 {
 	return m_AllowedMethods;
 }
+
+int WSDDService::UpdateWSDD(FILE* wsddfile, int tabcount)
+{
+	m_file = wsddfile;
+	PrintTabs(tabcount); *this << "<service name=\"" << m_sName.c_str() << "\" provider=\"" << GetProviderString() << "\" description=\"" << m_sDescription.c_str() << "\">\n";
+	if (m_Params)
+	{
+		WSDDHandlerList::iterator iter;
+		if(m_RequestHandlers)
+		{
+			*this << "\t\t<requestFlow>\n";
+			for(iter=m_RequestHandlers->begin(); iter!=m_RequestHandlers->end(); iter++)
+			{
+				(*iter)->UpdateWSDD(wsddfile, 3);
+			}
+			*this << "\t\t</requestFlow>\n";
+		}
+
+		if(m_ResponseHandlers)
+		{
+			*this << "\t\t<responseFlow>\n";
+			for(iter=m_ResponseHandlers->begin(); iter!=m_ResponseHandlers->end(); iter++)
+			{
+				(*iter)->UpdateWSDD(wsddfile, 3);
+			}
+			*this << "\t\t</responseFlow>\n";
+		}
+		PrintTabs(tabcount+1); *this << "<parameter name=\"allowedMethods\" value=\"";
+		//for looop
+		for (list<AxisString>::iterator itrs = m_AllowedMethods.begin(); itrs != m_AllowedMethods.end(); itrs++)
+		{
+			*this << (*itrs).c_str() << " ";
+		}
+		*this << "\"/>\n"; 
+		PrintTabs(tabcount+1); *this << "<parameter name=\"className\" value=\"" << m_sLibName.c_str();
+		*this << "\" />\n"; 
+		map<AxisString, AxisString>::iterator itr;
+		for (itr = m_Params->begin(); itr != m_Params->end(); itr++)
+		{
+			PrintTabs(tabcount+1); *this << "<parameter name=\"" << (*itr).first.c_str() << "\" value=\"" << (*itr).second.c_str() << "\" />";
+		}
+	}
+	PrintTabs(tabcount); *this << "</service>\n";
+	m_file = 0;
+	return AXIS_SUCCESS;
+}
+
+const char* WSDDService::GetProviderString()
+{
+	switch(m_Provider)
+	{
+	case C_RPC_PROVIDER: return "C:RPC";
+	case C_DOC_PROVIDER: return "C:DOCUMENT";
+	case CPP_RPC_PROVIDER: return "CPP:RPC";
+	case CPP_DOC_PROVIDER: return "CPP:DOCUMENT";
+	case COM_PROVIDER: return "COM";
+	default: return "unknown";
+	}
+	return 0;
+}
