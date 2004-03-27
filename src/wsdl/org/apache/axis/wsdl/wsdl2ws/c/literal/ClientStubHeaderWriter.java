f93f41ebea10918ad1dde54a44911ac16e8636fb
Jean-Yves Baudy <baudyjy@apache.org>
2004-03-27 15:24:42 +0000

    New structure adaptation and Aix xlc port.

--- src/wsdl/org/apache/axis/wsdl/wsdl2ws/c/literal/ClientStubHeaderWriter.java
+++ src/wsdl/org/apache/axis/wsdl/wsdl2ws/c/literal/ClientStubHeaderWriter.java
@@ -52,146 +52,169 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
- 
 /**
  * @author Srinath Perera(hemapani@openource.lk)
  * @author Susantha Kumara(susantha@opensource.lk, skumara@virtusa.com)
  */
-
 package org.apache.axis.wsdl.wsdl2ws.c.literal;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
-
 import org.apache.axis.wsdl.wsdl2ws.WrapperFault;
 import org.apache.axis.wsdl.wsdl2ws.WrapperUtils;
 import org.apache.axis.wsdl.wsdl2ws.info.MethodInfo;
 import org.apache.axis.wsdl.wsdl2ws.info.ParameterInfo;
 import org.apache.axis.wsdl.wsdl2ws.info.Type;
 import org.apache.axis.wsdl.wsdl2ws.info.WebServiceContext;
-
-public class ClientStubHeaderWriter extends HeaderFileWriter{
+public class ClientStubHeaderWriter extends HeaderFileWriter {
 	private WebServiceContext wscontext;
-	private ArrayList methods;	
-	public ClientStubHeaderWriter(WebServiceContext wscontext)throws WrapperFault{
-		super(WrapperUtils.getClassNameFromFullyQualifiedName(wscontext.getSerInfo().getQualifiedServiceName()));
+	private ArrayList methods;
+	public ClientStubHeaderWriter(WebServiceContext wscontext)
+			throws WrapperFault {
+		super(WrapperUtils.getClassNameFromFullyQualifiedName(wscontext
+				.getSerInfo().getQualifiedServiceName()));
 		this.wscontext = wscontext;
 		this.methods = wscontext.getSerInfo().getMethods();
 	}
-
 	protected File getFilePath() throws WrapperFault {
-		String targetOutputLocation = this.wscontext.getWrapInfo().getTargetOutputLocation();
-		if(targetOutputLocation.endsWith("/"))
-			targetOutputLocation = targetOutputLocation.substring(0, targetOutputLocation.length() - 1);
+		String targetOutputLocation = this.wscontext.getWrapInfo()
+				.getTargetOutputLocation();
+		if (targetOutputLocation.endsWith("/"))
+			targetOutputLocation = targetOutputLocation.substring(0,
+					targetOutputLocation.length() - 1);
 		new File(targetOutputLocation).mkdirs();
 		String fileName = targetOutputLocation + "/" + classname + ".h";
 		return new File(fileName);
 	}
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.axis.wsdl.wsdl2ws.cpp.HeaderFileWriter#writeAttributes()
 	 */
-	protected void writeAttributes() throws WrapperFault {}
-
-	/* (non-Javadoc)
+	protected void writeAttributes() throws WrapperFault {
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.axis.wsdl.wsdl2ws.cpp.HeaderFileWriter#writeClassComment()
 	 */
 	protected void writeClassComment() throws WrapperFault {
-		try{
+		try {
 			writer.write("/*\n");
-			writer.write(" * This is the Client Stub Class genarated by the tool WSDL2Ws\n");
-			writer.write(" * "+classname+".h: interface for the "+classname+"class.\n");
+			writer
+					.write(" * This is the Client Stub Class genarated by the tool WSDL2Ws\n");
+			writer.write(" * " + classname + ".h: interface for the "
+					+ classname + "class.\n");
 			writer.write(" *\n");
 			writer.write(" */\n");
-		}catch(IOException e){
+		} catch (IOException e) {
 			throw new WrapperFault(e);
 		}
 	}
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.axis.wsdl.wsdl2ws.cpp.HeaderFileWriter#writeMethods()
 	 */
 	protected void writeMethods() throws WrapperFault {
 		MethodInfo minfo;
 		boolean isSimpleType;
-		try{
-			writer.write("extern void* get_"+classname+"_stub();\n");
-			writer.write("extern void destroy_"+classname+"_stub(void* p);\n");			
-		  	for(int i = 0; i < methods.size(); i++){
-			  	minfo = (MethodInfo)this.methods.get(i);
+		try {
+			writer.write("extern void* get_" + classname + "_stub();\n");
+			writer.write("extern void destroy_" + classname
+					+ "_stub(void* p);\n");
+			for (int i = 0; i < methods.size(); i++) {
+				minfo = (MethodInfo) this.methods.get(i);
 				boolean isAllTreatedAsOutParams = false;
-			  	int noOfOutParams = minfo.getOutputParameterTypes().size();
-			  	//write return type
-			  	if(0==noOfOutParams)
-				  	writer.write("extern void ");
-			  	else if(1==noOfOutParams){
-			  	  	ParameterInfo returnParam = (ParameterInfo)minfo.getOutputParameterTypes().iterator().next();
-			  	  	String outparam = returnParam.getLangName();
-					writer.write("extern "+WrapperUtils.getClassNameFromParamInfoConsideringArrays(returnParam,wscontext)+" ");
-			  	}
-			  	else{
+				int noOfOutParams = minfo.getOutputParameterTypes().size();
+				//write return type
+				if (0 == noOfOutParams)
+					writer.write("extern void ");
+				else if (1 == noOfOutParams) {
+					ParameterInfo returnParam = (ParameterInfo) minfo
+							.getOutputParameterTypes().iterator().next();
+					String outparam = returnParam.getLangName();
+					writer
+							.write("extern "
+									+ WrapperUtils
+											.getClassNameFromParamInfoConsideringArrays(
+													returnParam, wscontext)
+									+ " ");
+				} else {
 					isAllTreatedAsOutParams = true;
 					writer.write("extern void ");
-			  	}
-			  	writer.write(minfo.getMethodname()+"(void* pStub");
-            
-			  	//write parameter names 
-			  	Iterator params = minfo.getInputParameterTypes().iterator();
-			  	if(params.hasNext()){
-			  	  	ParameterInfo fparam = (ParameterInfo)params.next();
-				  	writer.write(", "+WrapperUtils.getClassNameFromParamInfoConsideringArrays(fparam,wscontext)+" Value"+0);
-			  	}
-			  	for(int j =1; params.hasNext();j++){
-				  	ParameterInfo nparam = (ParameterInfo)params.next();
-				  	writer.write(", "+WrapperUtils.getClassNameFromParamInfoConsideringArrays(nparam,wscontext)+" Value"+j);
-			  	}
-			  	if (isAllTreatedAsOutParams){
+				}
+				writer.write(minfo.getMethodname() + "(void* pStub");
+				//write parameter names
+				Iterator params = minfo.getInputParameterTypes().iterator();
+				if (params.hasNext()) {
+					ParameterInfo fparam = (ParameterInfo) params.next();
+					writer
+							.write(", "
+									+ WrapperUtils
+											.getClassNameFromParamInfoConsideringArrays(
+													fparam, wscontext)
+									+ " Value" + 0);
+				}
+				for (int j = 1; params.hasNext(); j++) {
+					ParameterInfo nparam = (ParameterInfo) params.next();
+					writer
+							.write(", "
+									+ WrapperUtils
+											.getClassNameFromParamInfoConsideringArrays(
+													nparam, wscontext)
+									+ " Value" + j);
+				}
+				if (isAllTreatedAsOutParams) {
 					params = minfo.getOutputParameterTypes().iterator();
-					for(int j =0; params.hasNext();j++){
-						ParameterInfo nparam = (ParameterInfo)params.next();
-						writer.write(", AXIS_OUT_PARAM "+WrapperUtils.getClassNameFromParamInfoConsideringArrays(nparam,wscontext)+" *OutValue"+j);
+					for (int j = 0; params.hasNext(); j++) {
+						ParameterInfo nparam = (ParameterInfo) params.next();
+						writer
+								.write(", AXIS_OUT_PARAM "
+										+ WrapperUtils
+												.getClassNameFromParamInfoConsideringArrays(
+														nparam, wscontext)
+										+ " *OutValue" + j);
 					}
-			  	}
-			  	writer.write(");\n");
-		  	}
-		}catch (Exception e) {
-			  e.printStackTrace();
-			  throw new WrapperFault(e);
-		}	
-	}
-
-	/* (non-Javadoc)
-	 * @see org.apache.axis.wsdl.wsdl2ws.cpp.HeaderFileWriter#writePreprocssorStatements()
-	 */
-	protected void writePreprocssorStatements() throws WrapperFault {
-		try{
-			writer.write("#include <axis/client/Call.h>\n");
-			writer.write("#include <axis/common/AxisWrapperAPI.h>\n");
-			Type atype;
-			Iterator types = this.wscontext.getTypemap().getTypes().iterator();
-			HashSet typeSet = new HashSet();
-			while(types.hasNext()){
-				atype = (Type)types.next();
-				if (atype.getLanguageSpecificName().startsWith(">")) continue;
-				typeSet.add(atype.getLanguageSpecificName());
+				}
+				writer.write(");\n");
 			}
-			Iterator itr = typeSet.iterator();
-			while(itr.hasNext())
-			{
-				writer.write("#include \""+itr.next().toString()+".h\"\n");
-			}			
-			writer.write("\n");
-		}catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new WrapperFault(e);
 		}
 	}
-	protected String getFileType()
-	{
-		return "ClientStub";	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.axis.wsdl.wsdl2ws.cpp.HeaderFileWriter#writePreprocssorStatements()
+	 */
+	protected void writePreprocssorStatements() throws WrapperFault {
+		try {
+			writer.write("#include <axis/client/Call.h>\n");
+			writer.write("#include <axis/server/AxisWrapperAPI.h>\n");
+			Type atype;
+			Iterator types = this.wscontext.getTypemap().getTypes().iterator();
+			HashSet typeSet = new HashSet();
+			while (types.hasNext()) {
+				atype = (Type) types.next();
+				if (atype.getLanguageSpecificName().startsWith(">"))
+					continue;
+				typeSet.add(atype.getLanguageSpecificName());
+			}
+			Iterator itr = typeSet.iterator();
+			while (itr.hasNext()) {
+				writer.write("#include \"" + itr.next().toString() + ".h\"\n");
+			}
+			writer.write("\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new WrapperFault(e);
+		}
+	}
+	protected String getFileType() {
+		return "ClientStub";
 	}
 }
-
