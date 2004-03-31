7665914abfdbe626a3a829372ea3cfe8d8b742fe
W.M. Roshan Weerasuriya <roshan@apache.org>
2004-03-31 14:48:58 +0000

    A new client handler for testing

---
+++ samples/client/testHandler/THandler.cpp
@@ -0,0 +1,159 @@
+/* -*- C++ -*- */
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "SOAP" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ *
+ *
+ *
+ * @author Roshan Weerasuriya (roshan@opensource.lk, roshanw@jkcs.slt.lk)
+ *
+ */
+
+// THandler.cpp: implementation of the THandler class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "THandler.h"
+#include <axis/server/GDefine.h>
+#include <axis/server/IHandlerSoapSerializer.h>
+#include <axis/server/IHandlerSoapDeSerializer.h>
+#include <axis/server/HeaderBlock.h>
+#include <axis/server/SoapHeader.h>
+#include <axis/server/BasicNode.h>
+#include <axis/server/AxisTrace.h>
+#include <iostream>
+
+#include <axis/server/CharacterElement.h>
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+THandler::THandler()
+{
+    m_pOption = NULL;
+    m_sEmpty = "";
+}
+
+THandler::~THandler()
+{
+
+}
+
+const string& THandler::GetOption(const string& sArg)
+{
+  map<string, string>::const_iterator it = m_pOption->find(sArg);
+  if (it != m_pOption->end())
+  {
+      return (*it).second;
+  }
+  return m_sEmpty;	
+}
+
+void THandler::SetOptionList(const map<string, string>* OptionList)
+{
+   m_pOption = OptionList;
+}
+
+int THandler::Invoke(void *pvIMsg)
+{
+	IMessageData *pIMsg = (IMessageData*) pvIMsg;
+
+	if(pIMsg->isPastPivot()) {
+		/*this is a response*/
+
+		
+		
+	} else {
+		/*this is a request*/
+		
+		IHandlerSoapSerializer* pISZ;
+		pIMsg->getSoapSerializer(&pISZ);
+
+		IHeaderBlock* pIHeaderBlock= pISZ->createHeaderBlock();
+
+		pIHeaderBlock->setLocalName("testClientHandlerHeader");
+		pIHeaderBlock->setUri("http://opensource.lk/testClientHandlerHeader/");
+        
+		const AxisChar* pachHeaderVal = "test Header Val";
+
+		BasicNode* pBasicNode = pIHeaderBlock->createChild(CHARACTER_NODE);
+		pBasicNode->setValue(pachHeaderVal);
+		
+		pIHeaderBlock->addChild(pBasicNode);
+		
+	}
+
+	return AXIS_SUCCESS;
+}
+
+void THandler::OnFault(void *pvIMsg)
+{
+
+}
+
+int THandler::Init()
+{
+	//do any initialization, resetting of values
+
+	return AXIS_SUCCESS;
+}
+
+int THandler::Fini()
+{
+	//do any finalizatoin
+
+	return AXIS_SUCCESS;
+}
