8cfbf21ef775d1e414d7eaadf0bfd6b9c3e084ee
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2004-03-31 12:20:03 +0000

    adding files for xerces parser support

---
+++ src/soap/SoapBinInputStream.cpp
@@ -0,0 +1,54 @@
+/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+// SoapBinInputStream.cpp: implementation of the SoapBinInputStream class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "SoapBinInputStream.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+SoapBinInputStream::SoapBinInputStream(AXIS_MODULE_CALLBACK_GET_MESSAGE_BYTES pReadFunct, const void* pContext)
+{
+	m_pContext = pContext;
+	m_pReadFunct = pReadFunct;
+	m_nByteCount = 0;
+}
+
+SoapBinInputStream::~SoapBinInputStream()
+{
+
+}
+
+/**
+ * I dont really understand the use of this function
+ */
+unsigned int SoapBinInputStream::curPos() const
+{
+	return m_nByteCount;
+}
+
+unsigned int SoapBinInputStream::readBytes(XMLByte* const toFill, const unsigned int maxToRead)
+{
+	int nRead = 0;
+	m_pReadFunct((const char**)toFill, (int*)&maxToRead, m_pContext);
+    
+	return nRead;
+}
---
+++ src/soap/SoapParserXerces.cpp
@@ -0,0 +1,83 @@
+/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/*
+ *	@author sanjaya singharage (sanjayas@opensource.lk)
+ */
+
+
+#ifdef WIN32
+#pragma warning (disable : 4786)
+#endif
+
+#include "SoapParserXerces.h"
+#include <xercesc/sax2/XMLReaderFactory.hpp>
+
+SoapParserXerces::SoapParserXerces()
+{
+	m_nStatus = NULL;
+	firstParsed = false;
+	m_pParser = XMLReaderFactory::createXMLReader();
+
+}
+
+SoapParserXerces::~SoapParserXerces()
+{
+	m_nStatus = NULL;
+}
+
+int SoapParserXerces::SetInputStream(const Ax_soapstream* pInputStream)
+{
+	m_pInputStream = pInputStream;
+	is = new SoapInputSource(m_pInputStream->transport.pGetFunct, m_pInputStream->str.ip_stream);
+	//SoapInputSource is(m_pInputStream->transport.pGetFunct, m_pInputStream->str.ip_stream);
+	m_pParser->setContentHandler(&Xhandler);
+	//return m_pHandler->Success();
+	return AXIS_SUCCESS;
+}
+
+int SoapParserXerces::Init()
+{
+	return 0;
+}
+
+const XML_Ch* SoapParserXerces::GetNS4Prefix(const XML_Ch* prefix)
+{
+	return Xhandler.NS4Prefix(prefix);
+}
+
+int SoapParserXerces::GetStatus()
+{
+	m_nStatus = AXIS_SUCCESS; /*TODO:Check if an error occured in expat */
+	return m_nStatus;
+}
+
+const AnyElement* SoapParserXerces::Next()
+{
+
+	if(!firstParsed)
+	{
+		m_pParser->parseFirst(*is, token);
+		firstParsed = true;
+	}
+
+	m_pParser->parseNext(token);
+
+	return Xhandler.getAnyElement();
+	//handler
+	//return NULL;
+}
