426ec68442068c0f93e5a3ccdd2b3f112f3190d0
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-07 13:20:56 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- src/common/AxisSocketUtils.cpp
+++ src/common/AxisSocketUtils.cpp
@@ -1,120 +1,93 @@
+/* -*- C++ -*- */
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
  *
  * @author Roshan Weerasuriya (roshan@jkcs.slt.lk, roshan@opensource.lk)
  *
  */
 
-// AxisSocketUtils.cpp: implementation of the AxisSocketUtils class.
-//
-//////////////////////////////////////////////////////////////////////
-
 #include "AxisSocketUtils.h"
 #include <axis/server/GDefine.h>
 #include <stdio.h>
 #if defined(WIN32) && !defined(CYGWIN)
-#include <winsock.h>    /* for socket(),... */
+#include <winsock.h>            /* for socket(),... */
 #else //Linux
 #endif
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
-
-AxisSocketUtils::AxisSocketUtils()
+AxisSocketUtils::AxisSocketUtils ()
 {
 
 }
 
-AxisSocketUtils::~AxisSocketUtils()
+AxisSocketUtils::~AxisSocketUtils ()
 {
 
 }
 
-int AxisSocketUtils::printSocketErrorDescription()
+int AxisSocketUtils::printSocketErrorDescription ()
 {
-  #if defined(WIN32) && !defined(CYGWIN)
-		int iErrorCode = WSAGetLastError();		
+#if defined(WIN32) && !defined(CYGWIN)
+    int iErrorCode = WSAGetLastError ();
 
-		if (iErrorCode == WSANOTINITIALISED) {
-			printf("%s\n", "WSANOTINITIALISED");
-		} else if (iErrorCode == WSAENETDOWN) {
-			printf("%s\n", "WSAENETDOWN");
-		} else if (iErrorCode == WSAEFAULT) {
-			printf("%s\n", "WSAEFAULT");
-		} else if (iErrorCode == WSAEINTR) {
-			printf("%s\n", "WSAEINTR");
-		} else if (iErrorCode == WSAEINPROGRESS) {
-			printf("%s\n", "WSAEINPROGRESS");
-		} else if (iErrorCode == WSAEINVAL) {
-			printf("%s\n", "WSAEINVAL");
-		} else if (iErrorCode == WSAEMFILE) {
-			printf("%s\n", "WSAEMFILE");
-		} else if (iErrorCode == WSAENOBUFS) {
-			printf("%s\n", "WSAENOBUFS");
-		} else if (iErrorCode == WSAENOTSOCK) {
-			printf("%s\n", "WSAENOTSOCK");
-		} else if (iErrorCode == WSAEOPNOTSUPP) {
-			printf("%s\n", "WSAEOPNOTSUPP");
-		} else if (iErrorCode == WSAEWOULDBLOCK) {
-			printf("%s\n", "WSAEWOULDBLOCK");
-		} 
-	#else //Linux
-	#endif
+    if (iErrorCode == WSANOTINITIALISED)
+    {
+        printf ("%s\n", "WSANOTINITIALISED");
+    }
+    else if (iErrorCode == WSAENETDOWN)
+    {
+        printf ("%s\n", "WSAENETDOWN");
+    }
+    else if (iErrorCode == WSAEFAULT)
+    {
+        printf ("%s\n", "WSAEFAULT");
+    }
+    else if (iErrorCode == WSAEINTR)
+    {
+        printf ("%s\n", "WSAEINTR");
+    }
+    else if (iErrorCode == WSAEINPROGRESS)
+    {
+        printf ("%s\n", "WSAEINPROGRESS");
+    }
+    else if (iErrorCode == WSAEINVAL)
+    {
+        printf ("%s\n", "WSAEINVAL");
+    }
+    else if (iErrorCode == WSAEMFILE)
+    {
+        printf ("%s\n", "WSAEMFILE");
+    }
+    else if (iErrorCode == WSAENOBUFS)
+    {
+        printf ("%s\n", "WSAENOBUFS");
+    }
+    else if (iErrorCode == WSAENOTSOCK)
+    {
+        printf ("%s\n", "WSAENOTSOCK");
+    }
+    else if (iErrorCode == WSAEOPNOTSUPP)
+    {
+        printf ("%s\n", "WSAEOPNOTSUPP");
+    }
+    else if (iErrorCode == WSAEWOULDBLOCK)
+    {
+        printf ("%s\n", "WSAEWOULDBLOCK");
+    }
+#else //Linux
+#endif
 
-	return AXIS_SUCCESS;
+    return AXIS_SUCCESS;
 }
