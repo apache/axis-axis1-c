d68d2b149b8f6d4d512f8afa16f829c492e144a6
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-07 13:11:40 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- c/src/common/AxisException.cpp
+++ c/src/common/AxisException.cpp
@@ -1,103 +1,62 @@
+/* -*- C++ -*- */
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
- *
- *
+ * 
  * @author  Damitha Kumarage (damitha@opensource.lk, damitha@jkcsworld.com)
  *
  */
 
 #include <axis/server/AxisException.h>
-
 #include <exception>
 using namespace std;
 
-AxisException::AxisException(int exceptionCode)
+AxisException::AxisException (int exceptionCode)
 {
-  processException(exceptionCode);
+    processException (exceptionCode);
 }
 
-AxisException::AxisException(exception* e)
+AxisException::AxisException (exception* e)
 {
-  processException(e);
+    processException (e);
 }
 
-AxisException::AxisException(exception* e, int exceptionCode)
+AxisException::AxisException (exception* e, int exceptionCode)
 {
-  processException(e);
+    processException (e);
 }
 
-void AxisException::processException(exception* e, int exceptionCode)
+void AxisException::processException (exception* e, int exceptionCode)
 {
-  m_sMessage = getMessage(e) + getMessage(exceptionCode);
+    m_sMessage = getMessage (e) + getMessage (exceptionCode);
 }
 
-void AxisException::processException(exception* e)
+void AxisException::processException (exception* e)
 {
-  m_sMessage = getMessage(e);
+    m_sMessage = getMessage (e);
 }
 
-void AxisException::processException(int exceptionCode)
+void AxisException::processException (int exceptionCode)
 {
-  m_sMessage = getMessage(exceptionCode);
+    m_sMessage = getMessage (exceptionCode);
 }
 
-string AxisException::getMessage(exception* e)
+string AxisException::getMessage (exception* e)
 {
-  string sMessage = "to do";
-  /* to do */
+    string sMessage = "to do";
+    /* to do */
 /*  exception *objType = static_cast<std::bad_alloc*> (e);
   if(objType != NULL)
   {
@@ -123,23 +82,23 @@ string AxisException::getMessage(exception* e)
   }
 */
 
-   return sMessage;
+    return sMessage;
 }
 
-string AxisException::getMessage(int e)
+string AxisException::getMessage (int e)
 {
-	const char* sMessage = NULL;
-    SoapFault* objSoap = SoapFault::getSoapFault(e);
-	//following is not the proper thing to do. but a temporary solution
-	if (objSoap)
-	{
-		sMessage = objSoap->getSoapString();
-		delete objSoap;
-	}
-   
+    const char* sMessage = NULL;
+    SoapFault* objSoap = SoapFault::getSoapFault (e);
+    //following is not the proper thing to do. but a temporary solution
+    if (objSoap)
+    {
+        sMessage = objSoap->getSoapString ();
+        delete objSoap;
+    }
+
     if (sMessage == NULL)
     {
-        switch(e)
+        switch (e)
         {
             case TEST_EXCEPTION:
                 sMessage = "Axis test exception";
@@ -149,23 +108,21 @@ string AxisException::getMessage(int e)
                 sMessage = "Exception on sending the message";
             case HTTP_ERROR:
                 sMessage = "HTTP Error, cannot process response message...";
-
             default:
                 sMessage = "Undefined exception";
-         }
-     }
-         
-     return sMessage;
+        }
+    }
+
+    return sMessage;
 
 }
 
-AxisException::~AxisException() throw()
+AxisException::~AxisException() throw ()
 {
 
 }
 
-const char* AxisException::what() const throw()
+const char* AxisException::what() const throw ()
 {
-  return m_sMessage.c_str();
+    return m_sMessage.c_str ();
 }
-
