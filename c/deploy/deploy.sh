cd741612e008fff5936b3dcd36ed0ce949c5b1f5
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-07 10:35:49 +0000

    making apache2 the default

--- c/deploy/deploy.sh
+++ c/deploy/deploy.sh
@@ -1,9 +1,9 @@
-rm ./libaxiscpp_mod.so
+rm ./libaxiscpp2_mod.so
 rm ../Axis/webservices/libAdminService.so
 rm ../Axis/libs/libserver_engine.so
 cp -f $AXISCPP_HOME/bin/libAdminService.so ../Axis/webservices/
 cp -f $AXISCPP_HOME/bin/libserver_engine.so ../Axis/libs/
-cp -f $AXISCPP_HOME/bin/libaxiscpp_mod.so ./
+cp -f $AXISCPP_HOME/bin/libaxiscpp_mod2.so ./
 ../bin/apachectl stop
 sleep 2
 ../bin/apachectl start
