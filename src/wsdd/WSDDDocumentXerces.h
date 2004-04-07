062fc5a35094b95db37e8b734ce6870eb771ec5a
W.M. Roshan Weerasuriya <roshan@apache.org>
2004-04-07 13:52:01 +0000

    Applied Coding Conventions

--- src/wsdd/WSDDDocumentXerces.h
+++ src/wsdd/WSDDDocumentXerces.h
@@ -1,61 +1,19 @@
-/* -*- C++ -*- */
-
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
+
 #ifdef WIN32
 #pragma warning(disable : 4786)
 #endif
@@ -75,38 +33,40 @@ using namespace std;
 
 XERCES_CPP_NAMESPACE_USE;
 
-/**
-    @class WSDDDocumentXerces
-    @brief
-    @author sanjaya sinharage(sanjaya@opensource.lk)
-    @author Suasntha Kumara (skumara@virtusa.com, susantha@opensource.lk)
-*/
+/*
+ *  @class WSDDDocumentXerces
+ *  @brief
+ *  @author sanjaya sinharage(sanjaya@opensource.lk)
+ *  @author Suasntha Kumara (skumara@virtusa.com, susantha@opensource.lk)
+ */
 class WSDDDocumentXerces : public WSDDDocument, public DefaultHandler
 {
 private:
-	const AxisChar* __XTRC(const XMLCh* pChar);
-	void ProcessAttributes(WSDDLevels ElementType, const Attributes &attrs);
-	void GetParameters(WSDDLevels ElementType, const Attributes &attrs);
-	void AddAllowedRolesToService(const AxisXMLCh* value);
-	void AddAllowedMethodsToService(const AxisXMLCh* value);
-	int ParseDocument(const AxisChar* sWSDD);
+    const AxisChar* __XTRC(const XMLCh* pChar);
+    void ProcessAttributes(WSDDLevels ElementType, const Attributes &attrs);
+    void GetParameters(WSDDLevels ElementType, const Attributes &attrs);
+    void AddAllowedRolesToService(const AxisXMLCh* value);
+    void AddAllowedMethodsToService(const AxisXMLCh* value);
+    int ParseDocument(const AxisChar* sWSDD);
 
 public:
-	WSDDDocumentXerces(map<AxisString, int>* pLibNameIdMap);
-	~WSDDDocumentXerces();
-	int GetDeployment(const AxisChar* sWSDD, WSDDDeployment* pDeployment);
-	int UpdateDeployment(const AxisChar* sWSDD, WSDDDeployment* pDeployment);
+    WSDDDocumentXerces(map<AxisString, int>* pLibNameIdMap);
+    ~WSDDDocumentXerces();
+    int GetDeployment(const AxisChar* sWSDD, WSDDDeployment* pDeployment);
+    int UpdateDeployment(const AxisChar* sWSDD, WSDDDeployment* pDeployment);
 
     // -----------------------------------------------------------------------
     //  Implementations of the SAX DocumentHandler interface
     // -----------------------------------------------------------------------
-	void startElement(const XMLCh *const uri, const XMLCh *const localname,	const XMLCh *const qname, const Attributes &attrs);
-	void characters (const XMLCh *const chars, const unsigned int length);
-	void endElement (const XMLCh *const uri, const XMLCh *const localname,	const XMLCh *const qname);
-	void startPrefixMapping(const XMLCh* const prefix, const XMLCh* const uri);
-	void endPrefixMapping(const XMLCh* const prefix);
+    void startElement(const XMLCh *const uri, const XMLCh *const localname, 
+        const XMLCh *const qname, const Attributes &attrs);
+    void characters (const XMLCh *const chars, const unsigned int length);
+    void endElement (const XMLCh *const uri, const XMLCh *const localname, 
+        const XMLCh *const qname);
+    void startPrefixMapping(const XMLCh* const prefix, const XMLCh* const uri);
+    void endPrefixMapping(const XMLCh* const prefix);
 
-	// -----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
     //  Implementations of the SAX ErrorHandler interface
     // -----------------------------------------------------------------------
     void warning(const SAXParseException& exception);
@@ -114,4 +74,5 @@ public:
     void fatalError(const SAXParseException& exception);
 };
 
-#endif //__WSDDDOCUMENTXERCES_H_INCLUDED__
+#endif 
+
