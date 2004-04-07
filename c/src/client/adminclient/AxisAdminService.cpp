f0a7a85271cab3a13c5e3fc85580c5bee0b244c5
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-07 09:56:04 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- c/src/client/adminclient/AxisAdminService.cpp
+++ c/src/client/adminclient/AxisAdminService.cpp
@@ -1,3 +1,20 @@
+/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 /*
  * This is the Client Stub implementation file genarated by WSDL2Ws tool.
  * AxisAdminService.cpp: implemtation for the AxisAdminService.
@@ -5,21 +22,20 @@
  */
 
 #include "AxisAdminService.h"
-
 #include <axis/server/AxisWrapperAPI.h>
 
 bool CallBase::bInitialized;
 CallFunctions CallBase::ms_VFtable;
-AxisAdminService::AxisAdminService(const char* pchUri)
+AxisAdminService::AxisAdminService (const char *pchUri)
 {
-	m_pCall = new Call();
-	m_pCall->SetProtocol(APTHTTP);
-	m_pCall->SetEndpointURI(pchUri);
+    m_pCall = new Call ();
+    m_pCall->SetProtocol (APTHTTP);
+    m_pCall->SetEndpointURI (pchUri);
 }
 
-AxisAdminService::~AxisAdminService()
+AxisAdminService::~AxisAdminService ()
 {
-	delete m_pCall;
+    delete m_pCall;
 }
 
 
@@ -28,22 +44,23 @@ AxisAdminService::~AxisAdminService()
 /*
  * This method wrap the service methodupdateWSDD
  */
-xsd__boolean AxisAdminService::updateWSDD(xsd__base64Binary Value0)
+xsd__boolean AxisAdminService::updateWSDD (xsd__base64Binary Value0)
 {
-	xsd__boolean Ret;
-	if (AXIS_SUCCESS != m_pCall->Initialize(CPP_DOC_PROVIDER, NORMAL_CHANNEL)) return Ret;
-		m_pCall->SetTransportProperty(SOAPACTION_HEADER , "AxisAdmin#updateWSDD");
-	m_pCall->SetSOAPVersion(SOAP_VER_1_1);
-	m_pCall->SetOperation("updateWSDD", "http://www.opensource.lk/xsd");
-	m_pCall->AddParameter((void*)&Value0, "wsdd", XSD_BASE64BINARY);
-	if (AXIS_SUCCESS == m_pCall->Invoke())
-	{
-		if(AXIS_SUCCESS == m_pCall->CheckMessage("updateWSDDResponse", "http://www.opensource.lk/xsd"))
-		{
-			Ret = m_pCall->GetElementAsBoolean("return", 0);
-		}
-	}
-	m_pCall->UnInitialize();
-	return Ret;
+    xsd__boolean Ret;
+    if (AXIS_SUCCESS != m_pCall->Initialize (CPP_DOC_PROVIDER, NORMAL_CHANNEL))
+        return Ret;
+    m_pCall->SetTransportProperty (SOAPACTION_HEADER, "AxisAdmin#updateWSDD");
+    m_pCall->SetSOAPVersion (SOAP_VER_1_1);
+    m_pCall->SetOperation ("updateWSDD", "http://www.opensource.lk/xsd");
+    m_pCall->AddParameter ((void *) &Value0, "wsdd", XSD_BASE64BINARY);
+    if (AXIS_SUCCESS == m_pCall->Invoke ())
+    {
+        if (AXIS_SUCCESS == m_pCall->CheckMessage ("updateWSDDResponse",
+            "http://www.opensource.lk/xsd"))
+        {
+            Ret = m_pCall->GetElementAsBoolean ("return", 0);
+        }
+    }
+    m_pCall->UnInitialize ();
+    return Ret;
 }
-
