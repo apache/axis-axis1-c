7fe2d4aef7607256d2fdb2897df03399006db506
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-08 06:17:05 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- src/common/MessageData.cpp
+++ src/common/MessageData.cpp
@@ -1,207 +1,167 @@
 /* -*- C++ -*- */
-
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
  *
  * @author Susantha Kumara (skumara@virtusa.com)
  *
  */
 
-// MessageData.cpp: implementation of the MessageData class.
-//
-//////////////////////////////////////////////////////////////////////
 
 #include <axis/server/MessageData.h>
 #include "IDeployerUtils.h"
 #include "../wsdd/WSDDDeployment.h"
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
 
 const AxisChar* MessageData::m_pachBlankPropertyValue = "";
 extern WSDDDeployment* g_pWSDDDeployment;
 IMessageDataFunctions IMessageData::ms_VFtable;
-AdminUtils  MessageData::m_AdminUtil;
+AdminUtils MessageData::m_AdminUtil;
 
-MessageData::MessageData()
+MessageData::MessageData ()
 {
-	m_pSZ = NULL;
-	m_pDZ = NULL;
-	m_bPastPivotState= false;
+    m_pSZ = NULL;
+    m_pDZ = NULL;
+    m_bPastPivotState = false;
 }
 
-MessageData::~MessageData()
+MessageData::~MessageData ()
 {
-	map <AxisChar*, AxisChar*, ltstr>::iterator itCurrentItem= m_Properties.begin();
+    map < AxisChar*, AxisChar*, ltstr >::iterator itCurrentItem =
+        m_Properties.begin ();
 
-	while (itCurrentItem != m_Properties.end()) {
-		free((*itCurrentItem).first);
-		free((*itCurrentItem).second);
+    while (itCurrentItem != m_Properties.end ())
+    {
+        free ((*itCurrentItem).first);
+        free ((*itCurrentItem).second);
 
-		itCurrentItem++;
-	}
+        itCurrentItem++;
+    }
 
-	m_Properties.clear();
+    m_Properties.clear ();
 }
 
-void MessageData::SetSerializer(SoapSerializer *pSZ)
+void MessageData::SetSerializer (SoapSerializer* pSZ)
 {
-	m_pSZ = pSZ;
+    m_pSZ = pSZ;
 }
 
-void MessageData::SetDeSerializer(SoapDeSerializer *pDZ)
+void MessageData::SetDeSerializer (SoapDeSerializer* pDZ)
 {
-	m_pDZ = pDZ;
+    m_pDZ = pDZ;
 }
 
-void MessageData::SetUserName(string& sUserName)
+void MessageData::SetUserName (string &sUserName)
 {
-  m_sUserName = sUserName;
-}
-string& MessageData::GetUserName()
-{
-  return m_sUserName;
- // return "damitha kumarage"; 
+    m_sUserName = sUserName;
 }
 
-void MessageData::SetService(const WSDDService* argService)
+string &MessageData::GetUserName ()
 {
-  m_Service = argService;
+    return m_sUserName;
+    // return "damitha kumarage"; 
 }
 
-const WSDDService* MessageData::GetService()
+void MessageData::SetService (const WSDDService* argService)
 {
-  return m_Service;
-
+    m_Service = argService;
 }
 
-/*
-comm on 25Jul2003 at 10.00am
-ISoapSerializer* MessageData::getSoapSerializer()
+const WSDDService* MessageData::GetService ()
 {
-	return m_pSZ;
-}
-*/
-
-IWrapperSoapDeSerializer* MessageData::getSoapDeserializer()
-{
-	return m_pDZ;
-}
-
-bool MessageData::isPastPivot()
-{
-	return m_bPastPivotState;
-}
-
-int MessageData::setPastPivotState(bool bState)
-{
-	m_bPastPivotState = bState;
-
-	return AXIS_SUCCESS;
-}
-
-void MessageData::getSoapSerializer(IHandlerSoapSerializer **pIHandlerSoapSerializer)
-{
-	*pIHandlerSoapSerializer = static_cast<IHandlerSoapSerializer*>(m_pSZ);
-}
-
-void MessageData::GetSoapSerializer(IWrapperSoapSerializer **pIWrapperSoapSerializer)
-{
-	*pIWrapperSoapSerializer = static_cast<IWrapperSoapSerializer*>(m_pSZ);
-}
-
-void MessageData::GetSoapDeSerializer(IWrapperSoapDeSerializer **pIWrapperSoapDeSerializer)
-{
-	*pIWrapperSoapDeSerializer= static_cast<IWrapperSoapDeSerializer*>(m_pDZ);
-}
-
-void MessageData::getSoapDeSerializer(IHandlerSoapDeSerializer **pIHandlerSoapDeSerializer)
-{
-	*pIHandlerSoapDeSerializer= static_cast<IHandlerSoapDeSerializer*>(m_pDZ);
-}
-
-int MessageData::setProperty(AxisChar* pachName, const AxisChar* pachValue)
-{
-	AxisChar* pachTmpName = (AxisChar*) malloc(strlen(pachName)+1);
-	strcpy(pachTmpName, pachName);
-	AxisChar* pachTmpValue = (AxisChar*) malloc(strlen(pachValue)+1);
-	strcpy(pachTmpValue, pachValue);
-
-	m_Properties[pachTmpName]= pachTmpValue;
-
-	return AXIS_SUCCESS;
+    return m_Service;
 }
 
 /*
- *The method caller has to check whether the returned string is empty or
- * not. If it is empty then the idea is that the property is not 
- * available.
+ *    comm on 25Jul2003 at 10.00am
+ *    ISoapSerializer* MessageData::getSoapSerializer()
+ *    {
+ *        return m_pSZ;
+ *    }
  */
-const AxisChar* MessageData::getProperty(AxisChar* pachName)
-{	
-	if (m_Properties.find(pachName) != m_Properties.end())
-	{
-		return m_Properties[pachName];
-	}
-	
-	return m_pachBlankPropertyValue;
+
+IWrapperSoapDeSerializer* MessageData::getSoapDeserializer ()
+{
+    return m_pDZ;
 }
 
-void MessageData::GetAdminUtils(IAdminUtils** pIAdminUtils)
+bool MessageData::isPastPivot ()
 {
-	*pIAdminUtils = &m_AdminUtil;
+    return m_bPastPivotState;
+}
+
+int MessageData::setPastPivotState (bool bState)
+{
+    m_bPastPivotState = bState;
+
+    return AXIS_SUCCESS;
+}
+
+void MessageData::getSoapSerializer 
+    (IHandlerSoapSerializer** pIHandlerSoapSerializer)
+{
+    *pIHandlerSoapSerializer = static_cast < IHandlerSoapSerializer* >(m_pSZ);
+}
+
+void MessageData::GetSoapSerializer (IWrapperSoapSerializer**
+    pIWrapperSoapSerializer)
+{
+    *pIWrapperSoapSerializer = static_cast < IWrapperSoapSerializer* >(m_pSZ);
+}
+
+void MessageData::GetSoapDeSerializer (IWrapperSoapDeSerializer**
+    pIWrapperSoapDeSerializer)
+{
+    *pIWrapperSoapDeSerializer = 
+        static_cast < IWrapperSoapDeSerializer* >(m_pDZ);
+}
+
+void MessageData::getSoapDeSerializer (IHandlerSoapDeSerializer**
+    pIHandlerSoapDeSerializer)
+{
+    *pIHandlerSoapDeSerializer = 
+        static_cast < IHandlerSoapDeSerializer * >(m_pDZ);
+}
+
+int MessageData::setProperty (AxisChar* pachName, const AxisChar* pachValue)
+{
+    AxisChar* pachTmpName = (AxisChar*) malloc (strlen (pachName) + 1);
+    strcpy (pachTmpName, pachName);
+    AxisChar* pachTmpValue = (AxisChar*) malloc (strlen (pachValue) + 1);
+    strcpy (pachTmpValue, pachValue);
+
+    m_Properties[pachTmpName] = pachTmpValue;
+
+    return AXIS_SUCCESS;
+}
+
+/*
+ *    The method caller has to check whether the returned string is empty or
+ *    not. If it is empty then the idea is that the property is not 
+ *    available.
+ */
+const AxisChar* MessageData::getProperty (AxisChar* pachName)
+{
+    if (m_Properties.find (pachName) != m_Properties.end ())
+    {
+        return m_Properties[pachName];
+    }
+
+    return m_pachBlankPropertyValue;
+}
+
+void MessageData::GetAdminUtils (IAdminUtils** pIAdminUtils)
+{
+    *pIAdminUtils = &m_AdminUtil;
 }
