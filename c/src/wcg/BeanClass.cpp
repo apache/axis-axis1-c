a505e38aa9ecb19c6b56bec941b0b727b9bb49f9
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2004-04-09 08:57:53 +0000

    Applied coding conventions.

--- c/src/wcg/BeanClass.cpp
+++ c/src/wcg/BeanClass.cpp
@@ -1,78 +1,30 @@
-/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
 /*
- * The Apache Software License, Version 1.1
- *
- *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
- *
- *
  * @author Susantha Kumara (skumara@virtusa.com)
  *
  */
 
-// BeanClass.cpp: implementation of the BeanClass class.
-//
-//////////////////////////////////////////////////////////////////////
 #ifdef WIN32
 #pragma warning (disable : 4786)
 #endif
 
 #include <axis/wcg/BeanClass.h>
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
 
 BeanClass::BeanClass()
 {
@@ -81,83 +33,96 @@ BeanClass::BeanClass()
 
 BeanClass::~BeanClass()
 {
-	for (list<Variable*>::iterator it = m_Variables.begin(); it != m_Variables.end(); it++)
-	{
-		delete *it;
-	}
+    for (list<Variable*>::iterator it = m_Variables.begin(); 
+    it != m_Variables.end(); it++)
+    {
+        delete *it;
+    }
 }
 
 void BeanClass::AddVariable(Variable *pVar)
 {
-	m_Variables.push_back(pVar);
+    m_Variables.push_back(pVar);
 }
 
 void BeanClass::SetClassName(string &sName)
 {
-	m_Name = sName;
+    m_Name = sName;
 }
 
 int BeanClass::GenerateSerializerAndDeSerializerImpl(File &file)
 {
-	list<Variable*>::iterator it;
-	file << "//Parameters and wrapper methos to manipulate Point" << endl;
-	file << "static const AxisChar* Axis_URI_" << m_Name << " = \"http://www.opensource.lk/" << m_Name << "\";" << endl;
-	file << "static const AxisChar* Axis_TypeName_" << m_Name << " = \"" << m_Name << "\";" << endl;
-	file << endl;
-	file << "int Axis_DeSerialize_" << m_Name << "(" << m_Name << "* p, IWrapperSoapDeSerializer *pDZ)" << endl;
-	file << "{" << endl;
-	for (it = m_Variables.begin(); it != m_Variables.end(); it++)
-	{
-		(*it)->GenerateDeserializerImpl(file);
-	}	
-	file << "\treturn AXIS_SUCCESS;" << endl;
-	file << "}" << endl;
-	file << endl;
+    list<Variable*>::iterator it;
+    file << "//Parameters and wrapper methos to manipulate Point" << endl;
+    file << "static const AxisChar* Axis_URI_" << m_Name << 
+        " = \"http://www.opensource.lk/" << m_Name << "\";" << endl;
+    file << "static const AxisChar* Axis_TypeName_" << m_Name << 
+        " = \"" << m_Name << "\";" << endl;
+    file << endl;
+    file << "int Axis_DeSerialize_" << m_Name << "(" << m_Name << 
+        "* p, IWrapperSoapDeSerializer *pDZ)" << endl;
+    file << "{" << endl;
+    for (it = m_Variables.begin(); it != m_Variables.end(); it++)
+    {
+        (*it)->GenerateDeserializerImpl(file);
+    }    
+    file << "\treturn AXIS_SUCCESS;" << endl;
+    file << "}" << endl;
+    file << endl;
 
-	file << "int Axis_Serialize_" << m_Name <<  "(" << m_Name << "* p, IWrapperSoapSerializer& pSZ, bool bArray = false)" << endl;
-	file << "{" << endl;
-	file << "\tif (bArray)" << endl;
-	file << "\t{" << endl;
-	file << "\tpSZ << \"<\" << Axis_TypeName_" << m_Name << " << \">\";" << endl;
-	file << "\t}" << endl;
-	file << "\telse" << endl;
-	file << "\t{" << endl;
-	file << "\t\tconst AxisChar* sPrefix = pSZ.getNewNamespacePrefix();" << endl;
-//	file << "\tm_URI = \"" << g_ClassNamespaces[m_Name] << "\";" << endl;
-	file << "\t\tpSZ << \"<\" << Axis_TypeName_" << m_Name << "<< \" xsi:type=\\\"\" << sPrefix <<\":\" << Axis_TypeName_" << m_Name << " << \" xmlns:\" << sPrefix << \"=\\\"\" << Axis_URI_" << m_Name << " << \"\\\">\";" << endl;
-	file << "\t}" << endl;
-	for (it = m_Variables.begin(); it != m_Variables.end(); it++)
-	{
-		(*it)->GenerateSerializerImpl(file);
-	}	
-	file << "\tpSZ << \"</\" << Axis_TypeName_" << m_Name << " << \">\";" << endl;
-	file << "\treturn AXIS_SUCCESS;" << endl;
-	file << "}" << endl;
-	file << endl;
-	file << "int Axis_GetSize_" << m_Name << "(" << m_Name << "* p)" << endl;
-	file << "{" << endl;
-	file << "\treturn sizeof(" << m_Name <<");" << endl;
-	file << "}" << endl;
-	file << endl;
-	file << "void Axis_Delete_" << m_Name << "(" << m_Name << "* p, bool bArray = false, int nSize=0)" << endl;
-	file << "{" << endl;
-	file << "\tif (bArray)" << endl;
-	file << "\t\tdelete [] p;" << endl;
-	file << "else" << endl;
-	file << "delete p;" << endl;
-	file << "}" << endl;
-	return 0;
+    file << "int Axis_Serialize_" << m_Name <<  "(" << m_Name << 
+        "* p, IWrapperSoapSerializer& pSZ, bool bArray = false)" << endl;
+    file << "{" << endl;
+    file << "\tif (bArray)" << endl;
+    file << "\t{" << endl;
+    file << "\tpSZ << \"<\" << Axis_TypeName_" << m_Name << " << \">\";" 
+        << endl;
+    file << "\t}" << endl;
+    file << "\telse" << endl;
+    file << "\t{" << endl;
+    file << "\t\tconst AxisChar* sPrefix = pSZ.getNewNamespacePrefix();" 
+        << endl;
+/*    file << "\tm_URI = \"" << g_ClassNamespaces[m_Name] << "\";" << endl; */
+    file << "\t\tpSZ << \"<\" << Axis_TypeName_" << m_Name << 
+        "<< \" xsi:type=\\\"\" << sPrefix <<\":\" << Axis_TypeName_" 
+        << m_Name << " << \" xmlns:\" << sPrefix << \"=\\\"\" << Axis_URI_" 
+        << m_Name << " << \"\\\">\";" << endl;
+    file << "\t}" << endl;
+    for (it = m_Variables.begin(); it != m_Variables.end(); it++)
+    {
+        (*it)->GenerateSerializerImpl(file);
+    }    
+    file << "\tpSZ << \"</\" << Axis_TypeName_" << m_Name << " << \">\";" << 
+        endl;
+    file << "\treturn AXIS_SUCCESS;" << endl;
+    file << "}" << endl;
+    file << endl;
+    file << "int Axis_GetSize_" << m_Name << "(" << m_Name << "* p)" << endl;
+    file << "{" << endl;
+    file << "\treturn sizeof(" << m_Name <<");" << endl;
+    file << "}" << endl;
+    file << endl;
+    file << "void Axis_Delete_" << m_Name << "(" << m_Name 
+        << "* p, bool bArray = false, int nSize=0)" << endl;
+    file << "{" << endl;
+    file << "\tif (bArray)" << endl;
+    file << "\t\tdelete [] p;" << endl;
+    file << "else" << endl;
+    file << "delete p;" << endl;
+    file << "}" << endl;
+    return 0;
 }
 
 int BeanClass::GenerateWSDLSchema(File &file)
 {
-	file << "<complexType name=\"" << m_Name << "\">" << endl;
-	file << "<all>"<< endl;
-	for (list<Variable*>::iterator it = m_Variables.begin(); it != m_Variables.end(); it++)
-	{
-		(*it)->GenerateWSDLSchema(file);
-	}		
-	file << "</all>" << endl;
-	file << "</complexType>" << endl;
-	return 0;
+    file << "<complexType name=\"" << m_Name << "\">" << endl;
+    file << "<all>"<< endl;
+    for (list<Variable*>::iterator it = m_Variables.begin(); 
+    it != m_Variables.end(); it++)
+    {
+        (*it)->GenerateWSDLSchema(file);
+    }        
+    file << "</all>" << endl;
+    file << "</complexType>" << endl;
+    return 0;
 }
--- c/src/wcg/actions.h
+++ c/src/wcg/actions.h
@@ -1,10 +1,28 @@
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 #ifdef WIN32
 #pragma warning (disable : 4786)
 #endif
 
 #ifndef __ACTIONS_H_INCLUDED__
 #define __ACTIONS_H_INCLUDED__
-//this file contains all auxiliary structures needed to parse web service declaration files
+/* this file contains all auxiliary structures needed to parse web service 
+ * declaration files
+ */
 #include "TranslationUnit.h"
 
 #include <list>
@@ -13,12 +31,12 @@
 
 using namespace std;
 
-extern map<string,int> lexer_keys; //keyword map in the lexical analyzer.
+extern map<string,int> lexer_keys; /* keyword map in the lexical analyzer. */
 
-//auxiliary enums used by the parser
+/* auxiliary enums used by the parser */
 enum CLASSTYPE {WSCLASS=1, BEANCLASS};
 
-//auxiliary structs and type definitions used by the parser.
+/* auxiliary structs and type definitions used by the parser. */
 
 typedef list<int> token_list;
 typedef list<string*> string_list;
@@ -35,16 +53,16 @@ typedef list<expression*> expression_list;
 
 struct param_declarator
 {
-  token_list ptr_ops;        //optional
-  string decl_id;    //must;
-  expression_list arr_dim; //optional
+  token_list ptr_ops;        /* optional */
+  string decl_id;    /* must; */
+  expression_list arr_dim; /* optional */
 };
 
 struct param_decl
 {
-  string_list* decl_specs; //must
-  param_declarator* param; //must
-  bool bdef; //true if there is a default value
+  string_list* decl_specs; /* must */
+  param_declarator* param; /* must */
+  bool bdef; /* true if there is a default value */
   expression* defval;
 };
 
@@ -52,41 +70,44 @@ typedef list<param_decl*> param_decl_list;
 
 struct base_specifier
 {
-	int access;
-	string* class_name;
+    int access;
+    string* class_name;
 };
 
 typedef list<base_specifier*> base_specifier_list;
 
 struct class_head
 {
-	string* class_name;
-	base_specifier_list* baselist;
+    string* class_name;
+    base_specifier_list* baselist;
 };
 
 struct member_declarator
 {
-	string* declarator_id;    //must
-	param_decl_list* params;  //optional
-  expression_list array_indexes;  //optional
-  token_list ptr_ops;       //optional
-  bool bpure;               //optional
-  bool bdestructor;					//optional true if this is a destructor
-  bool method;              //must
-  expression* expr;         //optional
+    string* declarator_id;    /* must */
+    param_decl_list* params;  /* optional */
+  expression_list array_indexes;  /* optional */
+  token_list ptr_ops;       /* optional */
+  bool bpure;               /* optional */
+  bool bdestructor;         /* optional true if this is a destructor */
+  bool method;              /* must */
+  expression* expr;         /* optional */
 };
 
 typedef list<member_declarator*> member_declarator_list;
 
-//global functions used by the parser
+/* global functions used by the parser */
 void translation_unit_start();
 bool is_defined_class(const char* identifier);
 bool is_webservice_class(const char* classname);
-void add_member_declaration(string_list* decl_specs, member_declarator_list* member_decl_list);
+void add_member_declaration(string_list* decl_specs, 
+                            member_declarator_list* member_decl_list);
 
-//global variables used by the parser
-extern TranslationUnit* g_pTranslationUnit; //ultimate container of parsed information
-extern string g_classname; //this holds the class name while the body of a class is being parsed.
+/* global variables used by the parser */
+extern TranslationUnit* g_pTranslationUnit;
+/* ultimate container of parsed information */
+extern string g_classname;
+/* this holds the class name while the body of a class is being parsed. */
 extern list<string> g_classesfound;
 extern int g_currentclasstype;
 extern WSClass* g_pCurrentWSClass;
@@ -94,4 +115,4 @@ extern BeanClass* g_pCurrentBeanClass;
 extern int g_currentaccessspecifier;
 extern bool g_bdestructor_seen;
 
-#endif //__ACTIONS_H_INCLUDED__
+#endif
