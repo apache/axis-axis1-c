b6259bef4ee3d543eee78357ec2e63e1ef5892eb
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-09 06:15:42 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- src/engine/client/ClientAxisEngine.h
+++ src/engine/client/ClientAxisEngine.h
@@ -1,31 +1,44 @@
-// ClientAxisEngine.h: interface for the ClientAxisEngine class.
-//
-//////////////////////////////////////////////////////////////////////
+/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
-#if !defined(AFX_CLIENTAXISENGINE_H__4DA1EBC5_C7D8_4747_8069_C3E8B6A2E929__INCLUDED_)
-#define AFX_CLIENTAXISENGINE_H__4DA1EBC5_C7D8_4747_8069_C3E8B6A2E929__INCLUDED_
+#if !defined(AXIS_CLIENTAXISENGINE_H__INCLUDED_)
+#define AXIS_CLIENTAXISENGINE_H__INCLUDED_
 
 #include <axis/server/AxisEngine.h>
-/**
-    @class ClientAxisEngine
-    @brief
-
-
-    @author Susantha Kumara (skumara@virtusa.com)
-*/
-class ClientAxisEngine : public AxisEngine  
+/*
+ *   @class ClientAxisEngine
+ *   @brief
+ *
+ *
+ *   @author Susantha Kumara (skumara@virtusa.com)
+ */
+class ClientAxisEngine:public AxisEngine
 {
-public:
-	MessageData* GetMessageData();
-	ClientAxisEngine();
-	virtual ~ClientAxisEngine();
-	virtual int Process(Ax_soapstream* pSoap);
-protected:
-	virtual int Invoke(MessageData* pMsg);
-	virtual void OnFault(MessageData* pMsg);
-private:
-	Ax_soapstream* m_pSoap;
-	char* get_service_name(const char* pch_uri_path);
+    public:
+        MessageData* GetMessageData ();
+        ClientAxisEngine ();
+        virtual ~ ClientAxisEngine ();
+        virtual int Process (Ax_soapstream* pSoap);
+    protected:
+        virtual int Invoke (MessageData* pMsg);
+        virtual void OnFault (MessageData* pMsg);
+    private:
+        Ax_soapstream* m_pSoap;
+        char* get_service_name (const char* pch_uri_path);
 };
 
-#endif // !defined(AFX_CLIENTAXISENGINE_H__4DA1EBC5_C7D8_4747_8069_C3E8B6A2E929__INCLUDED_)
+#endif 
