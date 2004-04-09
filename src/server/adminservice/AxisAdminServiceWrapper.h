e34123bebe62a16e879afcbb655820b643ceb4e7
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-09 09:44:19 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- src/server/adminservice/AxisAdminServiceWrapper.h
+++ src/server/adminservice/AxisAdminServiceWrapper.h
@@ -1,8 +1,25 @@
+/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 /*
  * This is the Service Class genarated by the tool WSDL2Ws
- *		AxisAdminServiceWrapper.h: interface for the AxisAdminServiceWrapperclass.
- *
+ * AxisAdminServiceWrapper.h: interface for the AxisAdminServiceWrapperclass.
  */
+
 #if !defined(__AXISADMINSERVICEWRAPPER_SERVERWRAPPER_H__INCLUDED_)
 #define __AXISADMINSERVICEWRAPPER_SERVERWRAPPER_H__INCLUDED_
 
@@ -11,20 +28,23 @@
 #include <axis/server/GDefine.h>
 #include <axis/server/AxisWrapperAPI.h>
 
-class AxisAdminServiceWrapper : public WrapperClassHandler
+class AxisAdminServiceWrapper:public WrapperClassHandler
 {
-public:
-	AxisAdminServiceWrapper();
-public:
-	virtual ~AxisAdminServiceWrapper();
-public:/*implementation of WrapperClassHandler interface*/
-	int AXISCALL Invoke(void* pMsg);
-	void AXISCALL OnFault(void* pMsg);
-	int AXISCALL Init();
-	int AXISCALL Fini();
-	AXIS_BINDING_STYLE AXISCALL GetBindingStyle(){return DOC_LITERAL;};
-private:/*Methods corresponding to the web service methods*/
-	int updateWSDD(void* pMsg);
+    public:
+        AxisAdminServiceWrapper ();
+    public:
+        virtual ~ AxisAdminServiceWrapper ();
+    public: /*implementation of WrapperClassHandler interface */
+        int AXISCALL Invoke (void* pMsg);
+        void AXISCALL OnFault (void* pMsg);
+        int AXISCALL Init ();
+        int AXISCALL Fini ();
+        AXIS_BINDING_STYLE AXISCALL GetBindingStyle ()
+        {
+            return DOC_LITERAL;
+        };
+    private: /*Methods corresponding to the web service methods */
+        int updateWSDD (void* pMsg);
 };
 
-#endif /* !defined(__AXISADMINSERVICEWRAPPER_SERVERWRAPPER_H__INCLUDED_)*/
+#endif 
