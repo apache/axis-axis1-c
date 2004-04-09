2308b083d723842283a2ccd1c977caabd8b8019f
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-09 04:16:03 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- c/src/engine/SerializerPool.cpp
+++ c/src/engine/SerializerPool.cpp
@@ -1,118 +1,71 @@
+/* -*- C++ -*- */
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
  *
  * @author Susantha Kumara (skumara@virtusa.com)
  *
  */
 
-// SerializerPool.cpp: implementation of the SerializerPool class.
-//
-//////////////////////////////////////////////////////////////////////
-
 #include "SerializerPool.h"
 #include <axis/server/AxisTrace.h>
 extern AxisTrace* g_pAT;
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
-
-SerializerPool::SerializerPool()
+SerializerPool::SerializerPool ()
 {
 
 }
 
-SerializerPool::~SerializerPool()
+SerializerPool::~SerializerPool ()
 {
-	for (list<SoapSerializer*>::iterator it = m_SZList.begin(); it != m_SZList.end(); it++)
-	{
-		delete (*it);
-	}
+    for (list <SoapSerializer*>::iterator it = m_SZList.begin ();
+         it != m_SZList.end (); it++)
+    {
+        delete (*it);
+    }
 }
 
-//pooling should be implemented
-int SerializerPool::GetInstance(SoapSerializer** ppSZ)
+// Pooling should be implemented
+int SerializerPool::GetInstance (SoapSerializer** ppSZ)
 {
-	lock();
-	if (!m_SZList.empty())
-	{
-		*ppSZ = m_SZList.front();
-		m_SZList.pop_front();
-	}
-	else
-	{
-		*ppSZ = new SoapSerializer();
-	}
-	if (AXIS_SUCCESS != (*ppSZ)->Init())
-	{
-		delete *ppSZ;
-		*ppSZ = NULL;
-		unlock();
-        AXISTRACE1("Serializer pool could not be initialized", CRITICAL);
-		return AXIS_FAIL;
-	}
-	unlock();
-	return AXIS_SUCCESS;
+    lock ();
+    if (!m_SZList.empty ())
+    {
+        *ppSZ = m_SZList.front ();
+        m_SZList.pop_front ();
+    }
+    else
+    {
+        *ppSZ = new SoapSerializer ();
+    }
+    if (AXIS_SUCCESS != (*ppSZ)->Init ())
+    {
+        delete *ppSZ;
+        *ppSZ = NULL;
+        unlock ();
+        AXISTRACE1 ("Serializer pool could not be initialized", CRITICAL);
+        return AXIS_FAIL;
+    }
+    unlock ();
+    return AXIS_SUCCESS;
 }
 
-int SerializerPool::PutInstance(SoapSerializer* pSZ)
+int SerializerPool::PutInstance (SoapSerializer* pSZ)
 {
-	lock();
-	m_SZList.push_back(pSZ);
-	unlock();
-	return AXIS_SUCCESS;
+    lock ();
+    m_SZList.push_back (pSZ);
+    unlock ();
+    return AXIS_SUCCESS;
 }
