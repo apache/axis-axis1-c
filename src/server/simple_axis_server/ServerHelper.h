5f6319e115372011b976e429e2582c9010e8ef5d
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-09 12:30:00 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- src/server/simple_axis_server/ServerHelper.h
+++ src/server/simple_axis_server/ServerHelper.h
@@ -1,59 +1,18 @@
 /* -*- C++ -*- */
-
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
  *
  *
@@ -72,29 +31,42 @@
 
 using namespace std;
 
-enum CONTENT_TYPE {
-	HTTP_KEYWORD_TYPE,
-	STRING_TYPE
-};
-
-union uHttpMapContent /*this is used to store both the HTTP_KEYWORDS enumerated contents and strings*/
+enum CONTENT_TYPE
 {
-	HTTP_KEYWORDS eHTTP_KEYWORD; //for HTTP_KEYWORDS enumerated contents
-	const char* msValue; //for string content
-};	
-
-struct HTTP_MAP_TYPE {
-	CONTENT_TYPE eCONTENT_TYPE;
-	uHttpMapContent* objuHttpMapContent;
+    HTTP_KEYWORD_TYPE,
+    STRING_TYPE
 };
 
-struct HttpHeaders {
-	char* header_name;
-	char* header_value;
+union uHttpMapContent /* This is used to store both the HTTP_KEYWORDS enumerated
+                       * contents and strings 
+		       */
+{
+    HTTP_KEYWORDS eHTTP_KEYWORD; //for HTTP_KEYWORDS enumerated contents
+    const char* msValue;         //for string content
 };
 
-int getSeperatedHTTPParts(string sClientReqStream, string& sHTTPHeaders, string& sHTTPBody, map<HTTP_MAP_KEYWORDS, HTTP_MAP_TYPE*> *map_HTTP_Headers, Ax_soapstream* str, HttpHeaders* pHttpHeaders, int iHeaderArraySize, int* piHeaderCount);
-int	initializeHeaderMap(const string &HeaderLine, map<HTTP_MAP_KEYWORDS, HTTP_MAP_TYPE*> *map_HTTP_Headers,  Ax_soapstream* str, HttpHeaders* pHttpHeaders, int* piHeaderCount);
-int getHttpHeader(HTTP_MAP_KEYWORDS eKeyWord, map<HTTP_MAP_KEYWORDS, HTTP_MAP_TYPE*> *map_HTTP_Headers, HTTP_MAP_TYPE *objMapContent);
+struct HTTP_MAP_TYPE
+{
+    CONTENT_TYPE eCONTENT_TYPE;
+    uHttpMapContent* objuHttpMapContent;
+};
 
-#endif // !defined(AFX_SERVERHELPER_H__INCLUDED_)
+struct HttpHeaders
+{
+    char* header_name;
+    char* header_value;
+};
+
+int getSeperatedHTTPParts (string sClientReqStream, string &sHTTPHeaders,
+    string & sHTTPBody, map < HTTP_MAP_KEYWORDS, 
+    HTTP_MAP_TYPE* > *map_HTTP_Headers, Ax_soapstream* str,
+    HttpHeaders* pHttpHeaders, int iHeaderArraySize, int* piHeaderCount);
+
+int initializeHeaderMap (const string & HeaderLine, map < HTTP_MAP_KEYWORDS,
+    HTTP_MAP_TYPE* > *map_HTTP_Headers, Ax_soapstream* str, 
+    HttpHeaders* pHttpHeaders, int* piHeaderCount);
+
+int getHttpHeader (HTTP_MAP_KEYWORDS eKeyWord, map < HTTP_MAP_KEYWORDS,
+    HTTP_MAP_TYPE* > *map_HTTP_Headers, HTTP_MAP_TYPE* objMapContent);
+
+#endif 
