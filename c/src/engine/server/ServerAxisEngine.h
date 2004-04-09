4e4a2c02a6f1c2299203cd4cdc9a65f1f888d3fb
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-09 09:07:07 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- c/src/engine/server/ServerAxisEngine.h
+++ c/src/engine/server/ServerAxisEngine.h
@@ -1,30 +1,43 @@
-// ServerAxisEngine.h: interface for the ServerAxisEngine class.
-//
-//////////////////////////////////////////////////////////////////////
+/* -*- C++ -*- */
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
-#if !defined(AFX_SERVERAXISENGINE_H__8E421346_17A9_47EF_9003_6DC9C6F7787A__INCLUDED_)
-#define AFX_SERVERAXISENGINE_H__8E421346_17A9_47EF_9003_6DC9C6F7787A__INCLUDED_
+#if !defined(AXIS_SERVERAXISENGINE_H__INCLUDED_)
+#define AXIS_SERVERAXISENGINE_H__INCLUDED_
 
 #include <axis/server/AxisEngine.h>
-/**
-    @class ServerAxisEngine
-    @brief interface for the ServerAxisEngine class.
-
-
-    @author Susantha Kumara (skumara@virtusa.com)
-*/
-class ServerAxisEngine : public AxisEngine  
+/*
+ *  @class ServerAxisEngine
+ *  @brief interface for the ServerAxisEngine class.
+ *
+ *
+ *  @author Susantha Kumara (skumara@virtusa.com)
+ */
+class ServerAxisEngine:public AxisEngine
 {
-private:
-		BasicHandler* m_pWebService;
-public:
-	ServerAxisEngine();
-	virtual ~ServerAxisEngine();
-public:
-	int Process(Ax_soapstream* soap);
-protected:
-	virtual int Invoke(MessageData* pMsg);
-	virtual void OnFault(MessageData* pMsg);
+    private:
+        BasicHandler* m_pWebService;
+    public:
+        ServerAxisEngine ();
+        virtual ~ ServerAxisEngine ();
+    public:
+        int Process (Ax_soapstream* soap);
+    protected:
+        virtual int Invoke (MessageData* pMsg);
+        virtual void OnFault (MessageData* pMsg);
 };
 
-#endif // !defined(AFX_SERVERAXISENGINE_H__8E421346_17A9_47EF_9003_6DC9C6F7787A__INCLUDED_)
+#endif 
