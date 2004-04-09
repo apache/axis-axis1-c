b994a96a19946b95318ae70a843a31cbff948019
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2004-04-09 11:27:32 +0000

    Applied coding conventions.

--- src/wcg/File.h
+++ src/wcg/File.h
@@ -1,9 +1,24 @@
-// File.h: interface for the File class.
-//
-//////////////////////////////////////////////////////////////////////
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
-#if !defined(AFX_FILE_H__E30356C1_DD4A_4FCF_A529_713455BB6E02__INCLUDED_)
-#define AFX_FILE_H__E30356C1_DD4A_4FCF_A529_713455BB6E02__INCLUDED_
+/* File.h: interface for the File class. */
+
+
+#if !defined(AXIS_FILE_H__INCLUDED_)
+#define AXIS_FILE_H__INCLUDED_
 
 #include <string>
 #include <fstream>
@@ -12,9 +27,9 @@ using namespace std;
 class File : public ofstream
 {
 public:
-	int Write(char* pChars);
-	File(const string& sFileName);
-	virtual ~File();
+    int Write(char* pChars);
+    File(const string& sFileName);
+    virtual ~File();
 };
 
-#endif // !defined(AFX_FILE_H__E30356C1_DD4A_4FCF_A529_713455BB6E02__INCLUDED_)
+#endif
