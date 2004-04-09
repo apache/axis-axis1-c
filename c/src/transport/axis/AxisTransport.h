73741281e33da58e0557dbecf7afb446cf70dcaa
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2004-04-09 06:20:55 +0000

    Applied coding conventions.

--- c/src/transport/axis/AxisTransport.h
+++ c/src/transport/axis/AxisTransport.h
@@ -1,59 +1,20 @@
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2003 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- *
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/*
  * @author Damitha Kumarage (damitha@opensource.lk)
  * @author Susantha Kumara (susantha@opensource.lk, skumara@virtusa.com)
  *
@@ -74,22 +35,32 @@ public:
 
     int OpenConnection(int secure);
     void CloseConnection();
-	AXIS_TRANSPORT_STATUS Send_bytes(const char* pSendBuffer, const void* bufferid, const void* pSStream);
-	AXIS_TRANSPORT_STATUS Get_bytes(const char** res, int* retsize, const void* pSStream);
-    void SetTransportInformation(AXIS_TRANSPORT_INFORMATION_TYPE type, const char* value, const void* pSStream);
+    AXIS_TRANSPORT_STATUS Send_bytes(const char* pSendBuffer, 
+        const void* bufferid, const void* pSStream);
+    AXIS_TRANSPORT_STATUS Get_bytes(const char** res, int* retsize, 
+        const void* pSStream);
+    void SetTransportInformation(AXIS_TRANSPORT_INFORMATION_TYPE type, 
+        const char* value, const void* pSStream);
 
-	static AXIS_TRANSPORT_STATUS AXISCALL s_Send_bytes(const char* pSendBuffer, const void* bufferid, const void* pSStream);
-	static AXIS_TRANSPORT_STATUS AXISCALL s_Get_bytes(const char** res, int* retsize, const void* pSStream);
-    static void AXISCALL s_SetTransportInformation(AXIS_TRANSPORT_INFORMATION_TYPE type, const char* value, const void* pSStream);
-    static const char* AXISCALL s_ReceiveTransportInformation(AXIS_TRANSPORT_INFORMATION_TYPE type, const void* pSStream);
-	static void AXISCALL s_ReleaseReceiveBuffer(const char* buffer, const void* pSStream);
+    static AXIS_TRANSPORT_STATUS AXISCALL s_Send_bytes(const char* pSendBuffer,
+        const void* bufferid, const void* pSStream);
+    static AXIS_TRANSPORT_STATUS AXISCALL s_Get_bytes(const char** res, 
+        int* retsize, const void* pSStream);
+    static void AXISCALL s_SetTransportInformation
+        (AXIS_TRANSPORT_INFORMATION_TYPE type, const char* value, 
+        const void* pSStream);
+    static const char* AXISCALL s_ReceiveTransportInformation
+        (AXIS_TRANSPORT_INFORMATION_TYPE type, const void* pSStream);
+    static void AXISCALL s_ReleaseReceiveBuffer(const char* buffer, 
+        const void* pSStream);
 
 private:
     Transport* m_pHttpTransport;
     Sender* m_pSender;
     Receiver* m_pReceiver;
     Ax_soapstream* m_pSoap;
-	sendbuffers m_SendBuffers[NO_OF_SERIALIZE_BUFFERS]; /* Accumulates the serialized buffers to be transported */
+    sendbuffers m_SendBuffers[NO_OF_SERIALIZE_BUFFERS]; 
+    /* Accumulates the serialized buffers to be transported */
 };
 
 #endif
--- c/src/transport/axis/SecureChannel.cpp
+++ c/src/transport/axis/SecureChannel.cpp
@@ -1,3 +1,20 @@
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+
 #include <iostream>
 #include <errno.h>
 
@@ -33,7 +50,7 @@ int cert_verify_callback(int ok, X509_STORE_CTX *ctx)
  *
  */
 
-SecureChannel::SecureChannel() //: m_Sock(INVALID_SOCKET)
+SecureChannel::SecureChannel() /* : m_Sock(INVALID_SOCKET) */
 {
     if(!g_InitSSL)
     {
@@ -51,7 +68,7 @@ SecureChannel::SecureChannel() //: m_Sock(INVALID_SOCKET)
 
 SecureChannel::~SecureChannel()
 {
-	CloseChannel();
+    CloseChannel();
 }
 
 
@@ -59,79 +76,89 @@ SecureChannel::~SecureChannel()
  * This channel open INET channel for the time being using primitive sockets
  * Do we need any other type of channel; like shared memory, pipes etc. ????
  *
- * @param	p_RemoteNode	End point address as hostname/IP
- * @param	p_RemoteEnd		Port #
+ * @param    p_RemoteNode    End point address as hostname/IP
+ * @param    p_RemoteEnd        Port #
  *
- * @return  true if successfuly open a soket to the endpoint. o/w exception is thrown
+ * @return  true if successfuly open a soket to the endpoint. o/w exception is 
+ * thrown
  */
 
-bool SecureChannel::Open(std::string& p_RemoteNode, unsigned short p_RemoteEnd) throw (ChannelException)
+bool SecureChannel::Open(std::string& p_RemoteNode, unsigned short p_RemoteEnd)
+    throw (ChannelException)
 {
-	m_RemoteNode = p_RemoteNode;
-	m_RemoteEnd  = p_RemoteEnd;
+    m_RemoteNode = p_RemoteNode;
+    m_RemoteEnd  = p_RemoteEnd;
 
-	if(!Init())
-		throw ChannelException("Cannot initialize a channel to the remote end");
+    if(!Init())
+        throw ChannelException("Cannot initialize a channel to the remote \
+        end");
 
-	sockaddr_in clAddr, svAddr;
+    sockaddr_in clAddr, svAddr;
 
-	if ((m_Sock = socket(PF_INET, SOCK_STREAM, 0)) != INVALID_SOCKET)
-	{
-		clAddr.sin_family = AF_INET;     // AF_INET (address family Internet).
-		clAddr.sin_port   = 0; 			 // No Specify Port required
-		clAddr.sin_addr.s_addr = INADDR_ANY;
+    if ((m_Sock = socket(PF_INET, SOCK_STREAM, 0)) != INVALID_SOCKET)
+    {
+        clAddr.sin_family = AF_INET;   /* AF_INET (address family Internet). */
+        clAddr.sin_port   = 0;         /* No Specify Port required */
+        clAddr.sin_addr.s_addr = INADDR_ANY;
 
-		if (bind(m_Sock, (struct sockaddr*) &clAddr, sizeof(clAddr)) == SOCKET_ERROR)
-		{
-			Error("Error - Binding");
-			CloseChannel();
-			throw ChannelException("Cannot open a channel to the remote end, shutting down the channel");
-		}
+        if (bind(m_Sock, (struct sockaddr*) &clAddr, 
+            sizeof(clAddr)) == SOCKET_ERROR)
+        {
+            Error("Error - Binding");
+            CloseChannel();
+            throw ChannelException("Cannot open a channel to the remote end, 
+                shutting down the channel");
+        }
 
-		// Although the above fragment makes use of the bind() API, it would be
-		// just as effective to skip over this call as there are no specific
-		// local port ID requirements for this client. The only advantage that
-		// bind() offers is the accessibility of the port which the system chose
-		// via the .sin_port member of the cli_addr structure which will be set
-		// upon success of the bind() call.
+        /* Although the above fragment makes use of the bind() API, it would be
+         * just as effective to skip over this call as there are no specific
+         * local port ID requirements for this client. The only advantage that
+         * bind() offers is the accessibility of the port which the system chose
+         * via the .sin_port member of the cli_addr structure which will be set
+         * upon success of the bind() call.
+         */
 
-		svAddr.sin_family = AF_INET;
-		svAddr.sin_port   = htons(m_RemoteEnd);
+        svAddr.sin_family = AF_INET;
+        svAddr.sin_port   = htons(m_RemoteEnd);
 
-		struct hostent*  pHostEntry = NULL;
+        struct hostent*  pHostEntry = NULL;
 
-		// probably this is the host-name of the server we are connecting to
-		if((pHostEntry = gethostbyname(m_RemoteNode.c_str())))
-		{
-			svAddr.sin_addr.s_addr = ((struct in_addr *)pHostEntry->h_addr)->s_addr;
-		}
-		else
-		{
-			// no this is the IP address
-			svAddr.sin_addr.s_addr = inet_addr(m_RemoteNode.c_str());
-		}
+        /* probably this is the host-name of the server we are connecting to */
+        if((pHostEntry = gethostbyname(m_RemoteNode.c_str())))
+        {
+            svAddr.sin_addr.s_addr = 
+                ((struct in_addr *)pHostEntry->h_addr)->s_addr;
+        }
+        else
+        {
+            /* no this is the IP address */
+            svAddr.sin_addr.s_addr = inet_addr(m_RemoteNode.c_str());
+        }
 
-		// connect to the remote server.
-		if (connect(m_Sock,(struct sockaddr*)&svAddr, sizeof(svAddr)) == SOCKET_ERROR)
-		{
-			Error("Sockets error: Couldn't connect socket.");
-			CloseChannel();
-			throw ChannelException("Cannot open a channel to the remote end, shutting down the channel");
-		}
-	}
-	else
-	{
-		Error("Sockets error: Couldn't create socket.");
-		CloseChannel();
-		throw ChannelException("Cannot open a channel");
-	}
+        /* connect to the remote server. */
+        if (connect(m_Sock,(struct sockaddr*)&svAddr, 
+            sizeof(svAddr)) == SOCKET_ERROR)
+        {
+            Error("Sockets error: Couldn't connect socket.");
+            CloseChannel();
+            throw ChannelException("Cannot open a channel to the remote end, \
+                shutting down the channel");
+        }
+    }
+    else
+    {
+        Error("Sockets error: Couldn't create socket.");
+        CloseChannel();
+        throw ChannelException("Cannot open a channel");
+    }
 
     SSL_METHOD* req_method = SSLv23_client_method();
     SSL_SESSION* ssl_sessionid = NULL;
 
     m_sslContext = SSL_CTX_new(req_method);
 
-    if(!m_sslContext) {
+    if(!m_sslContext)
+    {
         int sslerror = ERR_get_error();
 
         /* OpenSSL documents that this must be at least 120 bytes long. */
@@ -142,10 +169,12 @@ bool SecureChannel::Open(std::string& p_RemoteNode, unsigned short p_RemoteEnd) 
 
         Error("SSL: couldn't create a context!");
         CloseChannel();
-		throw ChannelException("Cannot open a channel to the remote end, shutting down the channel");
+        throw ChannelException("Cannot open a channel to the remote end, \
+            shutting down the channel");
     }
 
-    SSL_CTX_set_verify(m_sslContext, /*SSL_VERIFY_PEER*/SSL_VERIFY_NONE, cert_verify_callback);
+    SSL_CTX_set_verify(m_sslContext, /*SSL_VERIFY_PEER*/SSL_VERIFY_NONE, 
+        cert_verify_callback);
 
 
     /* Lets make an SSL structure */
@@ -165,10 +194,10 @@ bool SecureChannel::Open(std::string& p_RemoteNode, unsigned short p_RemoteEnd) 
      */
 
 
-	return true;
+    return true;
 }
 
-/**
+/*
  * OS specific initialization should do here
  *
  * @return  true if successfuly initilaize OS specific stuffs. false o/w
@@ -178,70 +207,76 @@ bool SecureChannel::Init()
 {
 #ifdef WIN32
 
-	WSADATA wsaData;			// contains vendor-specific information, such as the
-								// maximum number of sockets available and the maximum
-								// datagram size.
+    WSADATA wsaData;     /* contains vendor-specific information, such as the
+                          * maximum number of sockets available and the maximum
+                          * datagram size.
+                          */
 
-	if(WSAStartup(WS_VERSION_REQD, &wsaData))  //Filled by Windows Sockets DLLs
-	{
-		m_LastErr = "WinSock DLL not responding.";
-		Error((char *)m_LastErr.c_str());
-		return false;
-	}
-	else
-	{
-		// Query to see whether the available version matches what we need
-		if (( LOBYTE (wsaData.wVersion) < WS_VERSION_MAJOR()) ||
-			 (LOBYTE (wsaData.wVersion) == WS_VERSION_MAJOR() &&
-			  HIBYTE (wsaData.wVersion) < WS_VERSION_MINOR()))
-		{
-			char buf[100];
-			sprintf(buf,"Windows Sockets version %d.%d not supported by winsock2.dll",
-						LOBYTE (wsaData.wVersion), HIBYTE (wsaData.wVersion));
-			Error(buf);
-			CloseChannel();
-			return false;
-		}
-	}
+    if(WSAStartup(WS_VERSION_REQD, &wsaData))  
+    /* Filled by Windows Sockets DLLs */
+    {
+        m_LastErr = "WinSock DLL not responding.";
+        Error((char *)m_LastErr.c_str());
+        return false;
+    }
+    else
+    {
+        /* Query to see whether the available version matches what we need */
+        if (( LOBYTE (wsaData.wVersion) < WS_VERSION_MAJOR()) ||
+             (LOBYTE (wsaData.wVersion) == WS_VERSION_MAJOR() &&
+              HIBYTE (wsaData.wVersion) < WS_VERSION_MINOR()))
+        {
+            char buf[100];
+            sprintf(buf,
+                "Windows Sockets version %d.%d not supported by winsock2.dll",
+                LOBYTE (wsaData.wVersion), HIBYTE (wsaData.wVersion));
+            Error(buf);
+            CloseChannel();
+            return false;
+        }
+    }
 #else
-    //cout << "no need for linux" << endl;
-	// other OS specific Intitialization goes here
+    /* cout << "no need for linux" << endl; */
+    /* other OS specific Intitialization goes here */
 #endif
 
-	return true;
+    return true;
 }
 
-/**
+/*
  * Write/send a message to the remote server; sending blocks the app.
  * we may need to do this asynchronizely; preferably either non-blocking
  * send or pthread.
  *
- * @param	Message to be written to the open channel
+ * @param    Message to be written to the open channel
  */
 const Channel& SecureChannel::operator << (const std::string& msg)
 {
-	if(INVALID_SOCKET == m_Sock)
-	{
-		Error("Writing cannot be done without having a open socket to remote end.");
-		throw ChannelException("Output streaming error on undefined channel; please open the channel first");
-	}
+    if(INVALID_SOCKET == m_Sock)
+    {
+        Error("Writing cannot be done without having a open socket to remote \
+            end.");
+        throw ChannelException("Output streaming error on undefined channel; \
+            please open the channel first");
+    }
 
-	int size = msg.size(), nByteSent;
+    int size = msg.size(), nByteSent;
 
     nByteSent = SSL_write(m_sslHandle, (char *)msg.c_str(), size);
 
     if(nByteSent < 0)
-	{
+    {
         char error_string[256];
 
         int err = SSL_get_error(m_sslHandle, nByteSent);
 
-        switch(err) {
+        switch(err)
+        {
             case SSL_ERROR_WANT_READ:
             case SSL_ERROR_WANT_WRITE:
-                /* The operation did not complete; the same TLS/SSL I/O function
-                   should be called again later. This is basically an EWOULDBLOCK
-                   equivalent. */
+                /* The operation did not complete; the same TLS/SSL I/O 
+                   function should be called again later. This is basically 
+                   an EWOULDBLOCK equivalent. */
                 return *this;
 
             case SSL_ERROR_SYSCALL:
@@ -252,10 +287,12 @@ const Channel& SecureChannel::operator << (const std::string& msg)
                     err =  errno;
                 #endif
 
-                sprintf(error_string, "SSL_write() returned SYSCALL, errno = %d\n", err);
+                sprintf(error_string,
+                    "SSL_write() returned SYSCALL, errno = %d\n", err);
                 Error(error_string);
                 CloseChannel();
-	            throw ChannelException("Output streaming error on SecureChannel while writing data");
+                throw ChannelException("Output streaming error on \
+                    SecureChannel while writing data");
 
                 return *this;
             }
@@ -267,13 +304,18 @@ const Channel& SecureChannel::operator << (const std::string& msg)
                     information on the error. */
                 int sslerror = ERR_get_error();
 
-                /* OpenSSL documents that this must be at least 120 bytes long. */
+                /* OpenSSL documents that this must be at least 120 
+                 * bytes long.
+                 */
                 char error_buffer[120];
 
-                sprintf(error_string, "SSL_write(): %s\n", ERR_error_string(sslerror, error_buffer));
+                sprintf(error_string, 
+                    "SSL_write(): %s\n", 
+                    ERR_error_string(sslerror, error_buffer));
                 Error(error_string);
                 CloseChannel();
-		        throw ChannelException("Output streaming error on SecureChannel while writing data");
+                throw ChannelException("Output streaming error on \
+                    SecureChannel while writing data");
 
                 return *this;
             }
@@ -282,44 +324,49 @@ const Channel& SecureChannel::operator << (const std::string& msg)
         }
 
         /* a true error */
-        sprintf(error_string, "Output streaming error: SSL_write() returned error %d\n", err);
+        sprintf(error_string, 
+            "Output streaming error: SSL_write() returned error %d\n", err);
 
-		Error(error_string);
-		CloseChannel();
-		throw ChannelException("Output streaming error on SecureChannel while writing data");
-	}
+        Error(error_string);
+        CloseChannel();
+        throw ChannelException("Output streaming error on SecureChannel \
+            while writing data");
+    }
 
-	return *this;
+    return *this;
 }
 
-/**
- * Read/receive a message from the remote server; reading may be done in chunks.
- *
- * @param	string to hold the read Message
+/*
+ * Read/receive a message from the remote server; reading may be done in 
+ * chunks.
+ * @param    string to hold the read Message
  */
 
 const Channel& SecureChannel::operator >> (std::string& msg)
 {
-	if(INVALID_SOCKET == m_Sock)
-	{
-		Error("Reading cannot be done without having a open socket.");
-		throw ChannelException("Input streaming error on undefined channel; please open the channel first");
-	}
+    if(INVALID_SOCKET == m_Sock)
+    {
+        Error("Reading cannot be done without having a open socket.");
+        throw ChannelException("Input streaming error on undefined channel; \
+            please open the channel first");
+    }
 
-	int nByteRecv = 0;
-	const int BUF_SIZE = 4096;
-	char buf[BUF_SIZE];
+    int nByteRecv = 0;
+    const int BUF_SIZE = 4096;
+    char buf[BUF_SIZE];
 
-	
+    
         nByteRecv = SSL_read(m_sslHandle, buf, BUF_SIZE - 1);
 
-        if(nByteRecv < 0) {
+        if(nByteRecv < 0)
+        {
             char error_string[256];
 
             /* failed SSL_read */
             int err = SSL_get_error(m_sslHandle, nByteRecv);
 
-            switch(err) {
+            switch(err)
+            {
                 case SSL_ERROR_NONE: /* this is not an error */
                 case SSL_ERROR_ZERO_RETURN: /* no more data */
                     break;
@@ -327,20 +374,25 @@ const Channel& SecureChannel::operator >> (std::string& msg)
                 case SSL_ERROR_WANT_WRITE:
                     /* there's data pending, re-invoke SSL_read() */
                     return *this; /* basically EWOULDBLOCK */
-                default: /* openssl/ssl.h says "look at error stack/return value/errno" */
+                default: 
+                /* openssl/ssl.h says "look at error stack/return value/errno"*/
                 {
                     /*  A failure in the SSL library occurred, usually a
                     protocol error.  The OpenSSL error queue contains more
                     information on the error. */
                     int sslerror = ERR_get_error();
 
-                    /* OpenSSL documents that this must be at least 120 bytes long. */
+                    /* OpenSSL documents that this must be at least 120 bytes 
+                     * long. 
+                     */
                     char error_buffer[120];
 
-                    sprintf(error_string, "SSL_read(): %s\n", ERR_error_string(sslerror, error_buffer));
+                    sprintf(error_string, "SSL_read(): %s\n", 
+                        ERR_error_string(sslerror, error_buffer));
                     Error(error_string);
                     CloseChannel();
-		            throw ChannelException("Input streaming error on SecureChannel while reading data");
+                    throw ChannelException("Input streaming error on \
+                        SecureChannel while reading data");
 
                     return *this;
                 }
@@ -348,19 +400,22 @@ const Channel& SecureChannel::operator >> (std::string& msg)
             }
 
             /* a true error */
-            sprintf(error_string, "Input streaming error: SSL_read() returned error %d\n", err);
+            sprintf(error_string, 
+                "Input streaming error: SSL_read() returned error %d\n", err);
 
-		    Error(error_string);
-		    CloseChannel();
-		    throw ChannelException("Input streaming error on SecureChannel while reading data");
+            Error(error_string);
+            CloseChannel();
+            throw ChannelException("Input streaming error on SecureChannel \
+                while reading data");
         }
 
-		if(nByteRecv)
-		{
-			buf[nByteRecv] = '\0';  // got a part of the message, so add it to form
+        if(nByteRecv)
+        {
+            buf[nByteRecv] = '\0';  
+            /* got a part of the message, so add it to form */
             msg = buf;
-		}
-		else
+        }
+        else
             printf("execution break\n");
     
 
@@ -368,7 +423,7 @@ const Channel& SecureChannel::operator >> (std::string& msg)
 }
 
 /**
- *	Close, and clean-up any OS specific stuff
+ *    Close, and clean-up any OS specific stuff
  *
  */
 
@@ -390,22 +445,23 @@ void SecureChannel::CloseChannel()
     }
 
 #ifdef WIN32
-	if(INVALID_SOCKET != m_Sock)
-		closesocket(m_Sock);
+    if(INVALID_SOCKET != m_Sock)
+        closesocket(m_Sock);
 
-	// Check for any possible error conditions from WSACleanup() and report
-	// them before exiting, as this information might indicate a network
-	// layer problem in the system.
+    /* Check for any possible error conditions from WSACleanup() and report
+     * them before exiting, as this information might indicate a network
+     * layer problem in the system.
+     */
 
-	WSACleanup();
+    WSACleanup();
 #else
-	if(INVALID_SOCKET != m_Sock)
-		close(m_Sock);
+    if(INVALID_SOCKET != m_Sock)
+        close(m_Sock);
 
 #endif
 }
 
-/**
+/*
  * Log any errors that cause on channel usage/initilaization
  *
  */
@@ -413,6 +469,6 @@ void SecureChannel::CloseChannel()
 void SecureChannel::Error(const char * err)
 {
 #ifdef _DEBUG
-	std::cerr << err << std::endl;
+    std::cerr << err << std::endl;
 #endif
 }
