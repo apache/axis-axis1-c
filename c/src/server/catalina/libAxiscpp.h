7a1ca078b09bbd525d429a230a1b9149a40ba94e
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-09 11:45:23 +0000

    replaced indent tabs with spaces; added apache 2 licence.

--- c/src/server/catalina/libAxiscpp.h
+++ c/src/server/catalina/libAxiscpp.h
@@ -1,62 +1,21 @@
 /* -*- C++ -*- */
-
 /*
- * The Apache Software License, Version 1.1
+ *   Copyright 2003-2004 The Apache Software Foundation.
  *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Copyright (c) 2003 The Apache Software Foundation.  All rights
- * reserved.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "SOAP" and "Apache Software Foundation" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 /*
- *
  *
  * @author Lilantha Darshana (lilantha@virtusa.com)
  *
@@ -77,21 +36,21 @@
 
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-static void
-jni_throw(JNIEnv* env, const char* exception, const char* msg)
-{
-    if (env->ExceptionOccurred())
-        return;
-    jclass jexception = env->FindClass(exception);
-    if (env->ExceptionOccurred())
-      return;
-    if (jexception == NULL)
-        env->FatalError(exception);
-    env->ThrowNew(jexception, msg);
-}
+    static void jni_throw (JNIEnv* env, const char* exception, const char* msg)
+    {
+        if (env->ExceptionOccurred ())
+            return;
+        jclass jexception = env->FindClass (exception);
+        if (env->ExceptionOccurred ())
+            return;
+        if (jexception == NULL)
+            env->FatalError (exception);
+            env->ThrowNew (jexception, msg);
+    }
 
 
 #ifdef __cplusplus
@@ -111,88 +70,65 @@ jni_throw(JNIEnv* env, const char* exception, const char* msg)
     } while (0)
 
 
-
-
 class JNIVector
 {
-public:
-    JNIVector(JNIEnv* p_Env, jobject p_jVector);
-	///Destructor
-    ~JNIVector();
+    public:
+        JNIVector (JNIEnv* p_Env, jobject p_jVector);
+        ~JNIVector ();
 
     char* operator [] (int i) const;
-	void push_back(const char* str);
-	void clear();
-	int  size();
+    void push_back (const char* str);
+    void clear ();
+    int size ();
 
-private:
-
-    JNIEnv* m_pEnv;
-    jobject m_jVector;
-	jmethodID m_jmGet;
-	jmethodID m_jmClear;
-	jmethodID m_jmAdd;
-	jmethodID m_jmSize;
+    private:
+        JNIEnv* m_pEnv;
+        jobject m_jVector;
+        jmethodID m_jmGet;
+        jmethodID m_jmClear;
+        jmethodID m_jmAdd;
+        jmethodID m_jmSize;
 };
 
-
-
-class JNIOutputStream : public std::strstreambuf
+class JNIOutputStream:public std::strstreambuf
 {
-public:
+    public:
+        JNIOutputStream (JNIEnv* env, jobject stream, unsigned bufsize = 16300);
+        virtual ~ JNIOutputStream ();
 
-    /// jobject writer must be a java.io.OutputStream object
-    JNIOutputStream(JNIEnv* env, jobject stream, unsigned bufsize = 16300);
+    protected:
+        virtual int overflow (int c);
+        virtual int sync ();
 
-    virtual ~JNIOutputStream();
-
-protected:
-
-    virtual int overflow(int c);
-    virtual int sync();
-
-private:
-
-    JNIEnv* m_pEnv;
-
-    jobject   _output;
-    jmethodID _write;
-    jmethodID _flush;
-
-    unsigned _bufsize;
-    jbyteArray _jbuf;
+    private:
+        JNIEnv * m_pEnv;
+        jobject _output;
+        jmethodID _write;
+        jmethodID _flush;
+        unsigned _bufsize;
+        jbyteArray _jbuf;
 };
 
 
-class JNIInputStream
-    : public std::strstreambuf
+class JNIInputStream : public std::strstreambuf
 {
-public:
+    public:
+        JNIInputStream (JNIEnv* env, jobject stream, unsigned bufsize = 16300);
+        virtual ~ JNIInputStream ();
+        int available ();
 
-    /// jobject writer must be a java.io.InputStream object
-    JNIInputStream(JNIEnv* env, jobject stream, unsigned bufsize = 16300);
+    protected:
+        virtual int underflow ();
 
-    virtual ~JNIInputStream();
-
-    int available();
-
-protected:
-
-    virtual int underflow();
-
-private:
-
-    JNIEnv* m_pEnv;
-
-    jobject _input;
-    jmethodID _read;
-    jmethodID _close;
-    jmethodID _available;
-
-    unsigned _bufsize;
-    jbyteArray _jbuf;
-    char* _buf;
+    private:
+        JNIEnv* m_pEnv;
+        jobject _input;
+        jmethodID _read;
+        jmethodID _close;
+        jmethodID _available;
+        unsigned _bufsize;
+        jbyteArray _jbuf;
+        char* _buf;
 };
 
-#endif //_AXIS_LIBAXISCPP_HPP
-
+#endif 
