0215998efc5a21e1d6342d0aeb23e8c823dbd96a
iasandcb <dev-null@apache.org>
2004-04-09 12:47:22 +0000

    Rename documentLiteral to DOCUMENT_LITERAL for new convention from JSR 201.

--- contrib/wsm/samples/MyService.java
+++ contrib/wsm/samples/MyService.java
@@ -1,6 +1,6 @@
 import org.apache.axis.metadata.annotation.*;
 
-@Protocol(soapStyle=EncodingType.documentLiteral)
+@Protocol(soapStyle=EncodingType.DOCUMENT_LITERAL)
 @TargetNamespace("http://ws.apache.org/axis/")
 @ServiceLocation("http://localhost:8080/axis/services/MyService")
 @WsdlFile("MyService.wsdl")
