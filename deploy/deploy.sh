e0a8c9431a1738e7c05f478abc8af0d86ceac41e
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-28 11:41:54 +0000

    making deployment folder Axis indepently position from apache

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,9 @@
-rm ./libaxiscpp_mod2.so
-rm ../Axis/webservices/libAdminService.so
-rm ../Axis/libs/libserver_engine.so
-cp -f $AXISCPP_HOME/bin/libAdminService.so ../Axis/webservices/
-cp -f $AXISCPP_HOME/bin/libserver_engine.so ../Axis/libs/
-cp -f $AXISCPP_HOME/bin/libaxiscpp_mod2.so ./
-../bin/apachectl stop
+rm /usr/local/apache2/modules/libaxiscpp_mod2.so
+rm webservices/libAdminService.so
+rm libs/libserver_engine.so
+cp -f $AXISCPP_HOME/bin/libAdminService.so webservices/
+cp -f $AXISCPP_HOME/bin/libserver_engine.so libs/
+cp -f $AXISCPP_HOME/bin/libaxiscpp_mod2.so /usr/local/apache2/modules/
+/usr/local/apache2/bin/apachectl stop
 sleep 2
-../bin/apachectl start
+/usr/local/apache2/bin/apachectl start
