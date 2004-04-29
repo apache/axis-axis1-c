1c975ac086d8f9de19ec731055dc5c1355e447e8
W.M. Roshan Weerasuriya <roshan@apache.org>
2004-04-29 05:21:14 +0000

    Applied Coding Conventions

--- src/unit_tests/cppunit/client/CallTestCase.h
+++ src/unit_tests/cppunit/client/CallTestCase.h
@@ -1,9 +1,23 @@
-// allTestCase.h: interface for the CallTestCase class.
-//
-//////////////////////////////////////////////////////////////////////
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
-#if !defined(AFX_ALLTESTCASE_H__2F6B4B2E_EACB_4F61_93ED_AEB3DE5943DD__OF_AXIS_INCLUDED_)
-#define AFX_ALLTESTCASE_H__2F6B4B2E_EACB_4F61_93ED_AEB3DE5943DD__OF_AXIS_INCLUDED_
+/* allTestCase.h: interface for the CallTestCase class. */
+
+#if !defined(AXIS_ALLTESTCASE_H__OF_AXIS_INCLUDED_)
+#define AXIS_ALLTESTCASE_H__OF_AXIS_INCLUDED_
 
 #include "cppunit\TestCase.h"
 #include "cppunit\TestSuite.h"
@@ -21,17 +35,17 @@ using namespace std;
 class CallTestCase  : public TestCase
 {
 private:
-	Call* pCall;
-	//SoapDeSerializer* pSoap;
+    Call* pCall;
+    /* SoapDeSerializer* pSoap; */
 public:
-	void testGetElementAsInt();
-	
-	void tearDown();
-	void setUp();
-	CallTestCase();
-	virtual ~CallTestCase();
-	static Test* suite();
+    void testGetElementAsInt();
+    
+    void tearDown();
+    void setUp();
+    CallTestCase();
+    virtual ~CallTestCase();
+    static Test* suite();
 
 };
 
-#endif // !defined(AFX_ALLTESTCASE_H__2F6B4B2E_EACB_4F61_93ED_AEB3DE5943DD__OF_AXIS_INCLUDED_)
+#endif
--- src/unit_tests/cppunit/common/BasicTypeSerializerTestCase.cpp
+++ src/unit_tests/cppunit/common/BasicTypeSerializerTestCase.cpp
@@ -34,112 +34,120 @@ BasicTypeSerializerTest::~BasicTypeSerializerTest()
 
 void BasicTypeSerializerTest::setUp()
 {
-	pBasicTypeSerializer = new BasicTypeSerializer();
+    pBasicTypeSerializer = new BasicTypeSerializer();
 }
 
 
 void BasicTypeSerializerTest::tearDown()
 {
-	delete pBasicTypeSerializer;
-	pBasicTypeSerializer= NULL;
+    delete pBasicTypeSerializer;
+    pBasicTypeSerializer= NULL;
 }
 
 
-//////////////// To test Serialize() Method ////////////////////////
+/*      To test Serialize() Method     */
 
 /*void BasicTypeSerializerTest::testSerialize()
 {
-	const char* ExpectedmyChar  = "<mystring xsi:type=\"xsd:string\">mystring</mystring>";
-	const char* myChar = "mystring";
-	const char*  myCharRes;
-	
-	myCharRes = pBasicTypeSerializer->serialize(myChar, 1 , XSD_STRING);
+    const char* ExpectedmyChar  = "<mystring xsi:type=\"xsd:string\">mystring\
+    </mystring>";
+    const char* myChar = "mystring";
+    const char*  myCharRes;
+    
+    myCharRes = pBasicTypeSerializer->serialize(myChar, 1 , XSD_STRING);
 
-	string sActual = myCharRes;
-	string sExpectedResult = ExpectedmyChar;
-	
-	CPPUNIT_ASSERT_EQUAL(sExpectedResult,sActual);	
+    string sActual = myCharRes;
+    string sExpectedResult = ExpectedmyChar;
+    
+    CPPUNIT_ASSERT_EQUAL(sExpectedResult,sActual);    
 
 }*/
 
 
-//////////////// The BasicTypeSerializer Suite()  ////////////////////////
+/*     The BasicTypeSerializer Suite()     */
 
 Test* BasicTypeSerializerTest::suite()
 {
-	TestSuite *testSuite = new TestSuite("BasicTypeSerializerTest");
+    TestSuite *testSuite = new TestSuite("BasicTypeSerializerTest");
 
-	testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
       ("testBasicTypeStr", &BasicTypeSerializerTest::testBasicTypeStr));
 
-	testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      ("testEncodeToBase64Binary", &BasicTypeSerializerTest::testEncodeToBase64Binary));
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testEncodeToBase64Binary", 
+      &BasicTypeSerializerTest::testEncodeToBase64Binary));
 
-	testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      ("testEncodeToHexBinary", &BasicTypeSerializerTest::testEncodeToHexBinary));
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testEncodeToHexBinary", 
+      &BasicTypeSerializerTest::testEncodeToHexBinary));
 
-	testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      ("testGetEntityReference", &BasicTypeSerializerTest::testGetEntityReference));
-	
-	testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      ("testSerializeAsAttribute", &BasicTypeSerializerTest::testSerializeAsAttribute));
-	
-	testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      ("testSerializeAsElement", &BasicTypeSerializerTest::testSerializeAsElement));
-
-
-	//testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-     // ("testSerializeLong", &BasicTypeSerializerTest::testSerializeLong));
-
-	//testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      //("testSerializeUnsignedLong", &BasicTypeSerializerTest::testSerializeUnsignedLong));
-
-	
-	//testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      //("testSerializeFloat", &BasicTypeSerializerTest::testSerializeFloat));
-
-	//testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
-      //("testSerializeDouble", &BasicTypeSerializerTest::testSerializeDouble));
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testGetEntityReference",
+      &BasicTypeSerializerTest::testGetEntityReference));
+    
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testSerializeAsAttribute",
+      &BasicTypeSerializerTest::testSerializeAsAttribute));
+    
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testSerializeAsElement",
+      &BasicTypeSerializerTest::testSerializeAsElement));
 
 
-	return testSuite;
+/*    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testSerializeLong", &BasicTypeSerializerTest::testSerializeLong));
+
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testSerializeUnsignedLong", 
+      &BasicTypeSerializerTest::testSerializeUnsignedLong));
+
+    
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testSerializeFloat", &BasicTypeSerializerTest::testSerializeFloat));
+
+    testSuite->addTest (new TestCaller <BasicTypeSerializerTest>
+      ("testSerializeDouble", &BasicTypeSerializerTest::testSerializeDouble));
+*/
+
+    return testSuite;
 }
 
 
 
 void BasicTypeSerializerTest::testBasicTypeStr()
 {
-	const AxisChar* ExpectedString = "string";
-	const AxisChar* sResult;
+    const AxisChar* ExpectedString = "string";
+    const AxisChar* sResult;
 
-	//xsd__hexBinary t;
+    /* xsd__hexBinary t; */
 
-	sResult = pBasicTypeSerializer->BasicTypeStr(XSD_STRING);
+    sResult = pBasicTypeSerializer->BasicTypeStr(XSD_STRING);
 
-	string ExpectedString1 = ExpectedString;
-	string ResultString    = sResult;
+    string ExpectedString1 = ExpectedString;
+    string ResultString    = sResult;
 
-	CPPUNIT_ASSERT_EQUAL(ExpectedString1, ResultString);
+    CPPUNIT_ASSERT_EQUAL(ExpectedString1, ResultString);
 
 }
 
 void BasicTypeSerializerTest::testEncodeToBase64Binary()
 {
-	
+    
   xsd__base64Binary*  objXSD ;
  
   const AxisChar* cResult;
 
-	
-	//objXSD->__ptr = (xsd__unsignedByte*)200;
-    //objXSD->__size= 14;
+    
+    /* objXSD->__ptr = (xsd__unsignedByte*)200;
+     * objXSD->__size= 14;
+     */
 
-	cResult = pBasicTypeSerializer->EncodeToBase64Binary(objXSD);
+    cResult = pBasicTypeSerializer->EncodeToBase64Binary(objXSD);
 
-	string sResult = cResult;
-	string ExpectedString = "";
+    string sResult = cResult;
+    string ExpectedString = "";
 
-	CPPUNIT_ASSERT_EQUAL(sResult, ExpectedString);
+    CPPUNIT_ASSERT_EQUAL(sResult, ExpectedString);
 
 }
 
@@ -147,78 +155,80 @@ void BasicTypeSerializerTest::testEncodeToBase64Binary()
 
 void BasicTypeSerializerTest::testEncodeToHexBinary()
 {
-	xsd__hexBinary* objHexBinary;
+    xsd__hexBinary* objHexBinary;
 
-	const AxisChar* cResult;
+    const AxisChar* cResult;
 
-	
-	//objXSD->__ptr = (xsd__unsignedByte*)200;
-    //objXSD->__size= 14;
+    
+    /* objXSD->__ptr = (xsd__unsignedByte*)200;
+     *  objXSD->__size= 14;
+     */
 
-	cResult = pBasicTypeSerializer->EncodeToHexBinary(objHexBinary);
+    cResult = pBasicTypeSerializer->EncodeToHexBinary(objHexBinary);
 
-	string sResult = cResult;
-	string ExpectedString = "";
+    string sResult = cResult;
+    string ExpectedString = "";
 
-	CPPUNIT_ASSERT_EQUAL(sResult, ExpectedString);
+    CPPUNIT_ASSERT_EQUAL(sResult, ExpectedString);
 
 }
 
 
 void BasicTypeSerializerTest::testGetEntityReference()
 {
-	AxisString sAxisString;
-	AxisString sResult;
+    AxisString sAxisString;
+    AxisString sResult;
 
-	sAxisString = "MyTestString";
+    sAxisString = "MyTestString";
 
-	sResult = pBasicTypeSerializer->GetEntityReferenced(sAxisString);
+    sResult = pBasicTypeSerializer->GetEntityReferenced(sAxisString);
 
-	string expectedString = "MyTestString";
-	string actualString = sResult;
+    string expectedString = "MyTestString";
+    string actualString = sResult;
 
-	CPPUNIT_ASSERT_EQUAL(expectedString, actualString);
+    CPPUNIT_ASSERT_EQUAL(expectedString, actualString);
 
 }
 
 void BasicTypeSerializerTest::testSerializeAsAttribute()
 {
-	const AxisChar* cResult;
+    const AxisChar* cResult;
 
-	const AxisChar* pPtr ="TestBool";
-	const AxisChar* pPrefix ="XSD";
+    const AxisChar* pPtr ="TestBool";
+    const AxisChar* pPrefix ="XSD";
 
-	xsd__boolean x = true_;
+    xsd__boolean x = true_;
 
-	void* pVal = &x;
+    void* pVal = &x;
 
-	
-	cResult = pBasicTypeSerializer->SerializeAsAttribute(pPtr,pPrefix, pVal, XSD_BOOLEAN);
+    
+    cResult = pBasicTypeSerializer->SerializeAsAttribute(pPtr,pPrefix, pVal, 
+        XSD_BOOLEAN);
 
-	string ExpectedString=" XSD:TestBool=\"true\"";
-	string sResult = cResult;
-	//string ExpectedString = cResult;
+    string ExpectedString=" XSD:TestBool=\"true\"";
+    string sResult = cResult;
+    /* string ExpectedString = cResult; */
 
-	CPPUNIT_ASSERT_EQUAL(ExpectedString, sResult);
+    CPPUNIT_ASSERT_EQUAL(ExpectedString, sResult);
 }
 
 
 void BasicTypeSerializerTest::testSerializeAsElement()
 {
-	const AxisChar* cResult;
+    const AxisChar* cResult;
 
-	const AxisChar* pPtr = "TestBool";
-	
-	
-	xsd__boolean x = true_;
-	void* pVal = &x;
+    const AxisChar* pPtr = "TestBool";
+    
+    
+    xsd__boolean x = true_;
+    void* pVal = &x;
 
-	cResult = pBasicTypeSerializer->SerializeAsElement(pPtr,pVal, XSD_BOOLEAN);
+    cResult = pBasicTypeSerializer->SerializeAsElement(pPtr,pVal, XSD_BOOLEAN);
 
-	string ExpectedString = "<TestBool>true</TestBool>";
-	string sResult = cResult;
+    string ExpectedString = "<TestBool>true</TestBool>";
+    string sResult = cResult;
 
-	CPPUNIT_ASSERT_EQUAL(ExpectedString, sResult);
+    CPPUNIT_ASSERT_EQUAL(ExpectedString, sResult);
 
 }
 
