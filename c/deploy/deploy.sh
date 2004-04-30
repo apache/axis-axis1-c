d126f775dcd2396102fe787548204d9c138f8f1a
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-30 12:14:49 +0000

    *** empty log message ***

--- c/deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-rm /usr/local/apache2/modules/libaxiscpp_mod2.so
-rm webservices/libAdminService.so
-rm libs/libserver_engine.so
-cp -f $AXISCPP_HOME/bin/libAdminService.so webservices/
-cp -f $AXISCPP_HOME/bin/libserver_engine.so libs/
-cp -f $AXISCPP_HOME/bin/libaxiscpp_mod2.so /usr/local/apache2/modules/
-/usr/local/apache2/bin/apachectl stop
-sleep 2
-/usr/local/apache2/bin/apachectl start
