3cf4e3f84b2cc7542d5961892dd1cf5ec76a6107
W.M. Roshan Weerasuriya <roshan@apache.org>
2004-05-07 07:59:26 +0000

    Reenter the doxygen format commenting to support auto generated documents. (This commenting format has been removed in the application of coding conventions).

--- c/include/axis/server/WSDDHandler.h
+++ c/include/axis/server/WSDDHandler.h
@@ -34,11 +34,13 @@ enum AXIS_HANDLER_SCOPE {AH_APPLICATION=1, AH_SESSION, AH_REQUEST};
 const AxisChar kw_scope_app[] = "application";
 const AxisChar kw_scope_ses[] = "session";
 const AxisChar kw_scope_req[] = "request";
-/*
+
+/**
  *  @class WSDDHandler
  *  @brief interface for the WSDDHandler class.
  *  @author Susantha Kumara (skumara@virtusa.com)
  */
+
 class WSDDHandler  
 {
 public:
