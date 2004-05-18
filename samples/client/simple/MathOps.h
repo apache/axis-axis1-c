191a2117df51d0c024d463ff7902cb6086f115f0
Damitha N.M. Kumarage <damitha@apache.org>
2004-05-18 11:12:45 +0000

    *** empty log message ***

---
+++ samples/client/simple/MathOps.h
@@ -0,0 +1,25 @@
+/*
+ * This is the Client Stub Class genarated by the tool WSDL2Ws
+ * MathOps.h: interface for the MathOpsclass.
+ *
+ */
+#if !defined(__MATHOPS_CLIENTSTUB_H__OF_AXIS_INCLUDED_)
+#define __MATHOPS_CLIENTSTUB_H__OF_AXIS_INCLUDED_
+
+#include <axis/client/Call.h>
+#include <ctype.h>
+
+class MathOps 
+{
+private:
+	Call* m_pCall;
+public:
+	MathOps(const char* pchEndpointUri);
+public:
+	virtual ~MathOps();
+public: 
+	int div(int Value0,int Value1);
+        int getFaultDetail(char** ppcDetail);
+};
+
+#endif /* !defined(__MATHOPS_CLIENTSTUB_H__OF_AXIS_INCLUDED_)*/
