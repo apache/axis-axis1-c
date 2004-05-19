9cadfa5fa146d2972745ef22882d48de4e223f45
Damitha N.M. Kumarage <damitha@apache.org>
2004-05-19 09:34:57 +0000

    PR: Obtained from: Submitted by: Reviewed by:

--- samples/client/rpcfault/MathOps.cpp
+++ samples/client/rpcfault/MathOps.cpp
@@ -43,9 +43,32 @@ int MathOps::div(int Value0, int Value1)
 		{
 			Ret = m_pCall->getElementAsInt("addReturn", 0);
 		}
+                else//Exception handling code goes here
+                {
+                }
 	}
 	m_pCall->unInitialize();
 	return Ret;
+
+
+
+        SOAPStruct* pReturn = NULL;
+        if (AXIS_SUCCESS != m_pCall->initialize(CPP_RPC_PROVIDER, NORMAL_CHANNEL)) return pReturn;
+        m_pCall->setTransportProperty(SOAPACTION_HEADER , "base#echoStruct");
+        m_pCall->setSOAPVersion(SOAP_VER_1_1);
+        m_pCall->setOperation("echoStruct", "http://soapinterop.org/");
+        m_pCall->addCmplxParameter(Value0, (void*)Axis_Serialize_SOAPStruct, (void*)Axis_Delete_SOAPStruct, "inputStruct", Axis_URI_SOAPStruct);
+        if (AXIS_SUCCESS == m_pCall->invoke())
+        {
+                if(AXIS_SUCCESS == m_pCall->checkMessage("echoStructResponse", ""))
+                {
+                        pReturn = (SOAPStruct*)m_pCall->getCmplxObject((void*) Axis_DeSerialize_SOAPStruct, (void*) Axis_Create_SOAPStruct, (void*) Axis_Delete_SOAPStruct,"return", 0);
+                }
+        }
+        m_pCall->unInitialize();
+        return pReturn;
+
+
 }
 
 int MathOps::getFaultDetail(char** ppcDetail)
