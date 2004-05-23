131e229eb128b3c5911a144d8b136cc2c868c6f1
Hemapani Srinath Perera <hemapani@apache.org>
2004-05-23 15:39:44 +0000

    fix the code handle the service referance in the DD and refactor the code and comment it

--- contrib/ews/test/org/apache/geronimo/ews/ws4j2ee/GenerateTest.java
+++ contrib/ews/test/org/apache/geronimo/ews/ws4j2ee/GenerateTest.java
@@ -58,6 +58,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.apache.geronimo.ews.AbstractTestCase;
+import org.apache.geronimo.ews.ws4j2ee.toWs.Ws4J2EEClientwithWSDL;
 import org.apache.geronimo.ews.ws4j2ee.toWs.Ws4J2EEwithWSDL;
 
 /**
@@ -91,9 +92,9 @@ public class GenerateTest
 	public void testBookSample() throws Exception
 	{
 		//client side
-		String args[] = new String[]{getTestFile("src/samples/jaxrpc/book/webservice.xml"),
+		String args[] = new String[]{getTestFile("src/samples/jaxrpc/book/webserviceClient.xml"),
 									 "-o" + getTestFile(outDir+"withWSDL/client/book/"),"-Iuse-remote"};
-		Ws4J2EEwithWSDL.main(args);
+		Ws4J2EEClientwithWSDL.main(args);
 
 		//server side
 		String args1[] = new String[]{getTestFile("src/samples/jaxrpc/book/webservice.xml"),
@@ -105,14 +106,12 @@ public class GenerateTest
 		args1 = new String[]{getTestFile("src/samples/jaxrpc/book/webservice.xml"),
 										   "-o" + getTestFile(outDir+"withWSDL/server/book/internal"),"--server-side","-Iuse-internals","-Cgeronimo"};
 			  Ws4J2EEwithWSDL.main(args1);
-//	String[] args2 = new String[]{"-o"+outDir+"withoutWSDL/server/META-INF/book1.wsdl" ,"-l" ,"http://127.0.0.1/ws4j2ee/","com.jwsbook.jaxrpc.BookQuote"};
-//			Ws4J2EEwithoutWSDL.main(args2);
 	}
 
 	public void testTimeSample() throws Exception{
-		String args[] = new String[]{getTestFile("src/samples/jaxrpc/time/webservices.xml"),
+		String args[] = new String[]{getTestFile("src/samples/jaxrpc/time/webserviceClient.xml"),
 									 "-o" + getTestFile(outDir+"withWSDL/client/time")};
-		Ws4J2EEwithWSDL.main(args);
+		Ws4J2EEClientwithWSDL.main(args);
 		
 		args = new String[]{getTestFile("src/samples/jaxrpc/time/webservices.xml"),
 											 "-o" + getTestFile(outDir+"withWSDL/server/time"),"--server-side"};
@@ -120,18 +119,18 @@ public class GenerateTest
 	}
 	
 	public void testZipSample() throws Exception{
-			String args[] = new String[]{getTestFile("src/samples/mapper/frenchzip/webservices.xml"),
+			String args[] = new String[]{getTestFile("src/samples/mapper/frenchzip/webserviceClient.xml"),
 										 "-o" + getTestFile(outDir+"withWSDL/client/zip")};
-			Ws4J2EEwithWSDL.main(args);
+			Ws4J2EEClientwithWSDL.main(args);
 		
 			args = new String[]{getTestFile("src/samples/mapper/frenchzip/webservices.xml"),
 												 "-o" + getTestFile(outDir+"withWSDL/server/zip"),"--server-side"};
 			Ws4J2EEwithWSDL.main(args);
 	}
 	public void testGoogleSample() throws Exception{
-				String args[] = new String[]{getTestFile("src/samples/mapper/google/webservices.xml"),
+				String args[] = new String[]{getTestFile("src/samples/mapper/google/webserviceClient.xml"),
 											 "-o" + getTestFile(outDir+"withWSDL/client/google")};
-				Ws4J2EEwithWSDL.main(args);
+				Ws4J2EEClientwithWSDL.main(args);
 		
 				args = new String[]{getTestFile("src/samples/mapper/google/webservices.xml"),
 													 "-o" + getTestFile(outDir+"withWSDL/server/google"),"--server-side"};
@@ -139,9 +138,9 @@ public class GenerateTest
 	}	
 	
 	public void testServletBasedSample()throws Exception{
-		String[] args = new String[]{getTestFile("src/samples/mapper/servletBasedSample/webservice.xml"),
-															 "-o" + getTestFile(outDir+"withWSDL/server/servletBasedSample"),"--server-side"};
-		Ws4J2EEwithWSDL.main(args);
+//		String[] args = new String[]{getTestFile("src/test/testData/math/webservice.xml"),
+//															 "-o" + getTestFile(outDir+"withWSDL/server/servletBasedSample"),"--server-side"};
+//		Ws4J2EEwithWSDL.main(args);
 	}
 	
 	
