afd34e67ec02e8e02292943d4da788f7046c41c1
Damitha N.M. Kumarage <damitha@apache.org>
2004-05-27 09:34:11 +0000

    Exceptin handling and fault mapping for document style

---
+++ samples/server/doclitfault/MathOps.cpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////
+//This is the Service implementation CPP file genarated by theWSDL2Ws.
+//		MathOps.cpp: implemtation for the MathOps.
+//
+//////////////////////////////////////////////////////////////////////
+#include "MathOps.h"
+
+
+MathOps::MathOps()
+{
+}
+
+MathOps::~MathOps()
+{
+}
+
+int  MathOps::div(int Value0, int Value1) throw(AxisDivByZeroException)
+{
+    if (Value1 == 0) throw AxisDivByZeroException();
+        return Value0/Value1;
+}
