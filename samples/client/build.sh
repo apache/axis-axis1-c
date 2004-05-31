241b4f67497e30c85f245263667ee4ea5787c013
Damitha N.M. Kumarage <damitha@apache.org>
2004-05-31 04:04:29 +0000

    PR: Obtained from: Submitted by: Reviewed by:
    Build files changed so that in the main build files, when it run, it build
    samples as well.

--- samples/client/build.sh
+++ samples/client/build.sh
@@ -1,5 +1,21 @@
-sh autogen.sh
-sh runconfig
-make
+#!/bin/bash
+echo "run libtoolize."          && 
+libtoolize --force              && 
+echo "run aclocal."             && 
+aclocal                         && 
+echo "run autoconf."            && 
+autoconf                        && 
+echo "run autoheader."          && 
+autoheader                      && 
+echo "run automake."            && 
+automake --add-missing --gnu    &&
+
+echo "./configure --bindir=$AXISCPP_HOME/samples/client/"
+./configure --bindir=$AXISCPP_HOME/samples/client/
+
+echo "make"
+make 2> sample_client_build_errors
+
+echo "make install"
 make install
-#strip -g sslclient base cbase groupB doclitbase doclitgroupB 
+
