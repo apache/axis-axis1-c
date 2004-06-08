bfa5c1f5003e81b037a99c64c3903a31cfff6cf1
Damitha N.M. Kumarage <damitha@apache.org>
2004-06-08 07:31:09 +0000

    *** empty log message ***

--- c/samples/client/rpcfault/AxisDivByZeroException.cpp
+++ c/samples/client/rpcfault/AxisDivByZeroException.cpp
@@ -1,78 +1,59 @@
-/* -*- C++ -*- */
 /*
- *   Copyright 2003-2004 The Apache Software Foundation.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- * 
- * @author  wsdl2ws generated
+ * This is the  implementation file genarated by WSDL2Ws tool.
+ * AxisDivByZeroException.cpp: implemtation for the AxisDivByZeroException.
  *
  */
 
 #include "AxisDivByZeroException.h"
-#include <exception>
-using namespace std;
 
-/**
- *    Default when no parameter passed. When thrown with no parameter
- *    more general AXISC_DIVBYZERO_EXCEPTION is assumed.
-*/
+#include <axis/server/AxisWrapperAPI.h>
 
 AxisDivByZeroException::AxisDivByZeroException()
 {
-    /* This only serves the pupose of indicating that the 
-     * service has thrown an excpetion
-     */
-    m_iExceptionCode = AXISC_SERVICE_THROWN_EXCEPTION;
-    processException(m_iExceptionCode);
+/* This only serves the pupose of indicating that the 
+ * service has thrown an excpetion 
+ */ 
+	m_iExceptionCode = AXISC_SERVICE_THROWN_EXCEPTION; 
+	processException(m_iExceptionCode); 
 }
 
-AxisDivByZeroException::AxisDivByZeroException(SOAPStructFault* pFault)
+AxisDivByZeroException::AxisDivByZeroException(DivByZeroStruct*pFault)
 {
-    m_iExceptionCode = AXISC_SERVICE_THROWN_EXCEPTION;
-    processException(pFault);
+	m_iExceptionCode = AXISC_SERVICE_THROWN_EXCEPTION;
+	processException(pFault);}
+
+AxisDivByZeroException::AxisDivByZeroException(int iExceptionCode)
+{
+
+	m_iExceptionCode = iExceptionCode;
+	processException (iExceptionCode);
 }
 
-AxisDivByZeroException::AxisDivByZeroException (int iExceptionCode)
+AxisDivByZeroException::AxisDivByZeroException(exception* e)
 {
-    m_iExceptionCode = iExceptionCode;
-    processException (iExceptionCode);
+	processException (e);
 }
 
-AxisDivByZeroException::AxisDivByZeroException (exception* e)
+AxisDivByZeroException::AxisDivByZeroException(exception* e,int iExceptionCode)
 {
-    processException (e);
+
+	processException (e, iExceptionCode);
 }
 
-AxisDivByZeroException::AxisDivByZeroException (exception* e, int iExceptionCode)
+AxisDivByZeroException::~AxisDivByZeroException() throw () 
 {
-    processException (e, iExceptionCode);
+	m_sMessage ="";
 }
 
-AxisDivByZeroException::~AxisDivByZeroException() throw ()
+void AxisDivByZeroException:: processException(exception* e, int iExceptionCode)
 {
-        m_sMessage = "";
+	m_sMessage = getMessage (e) + getMessage (iExceptionCode);
 }
 
-void AxisDivByZeroException::processException (exception* e, int iExceptionCode)
+void AxisDivByZeroException::processException (DivByZeroStruct* pFault)
 {
-    m_sMessage = getMessage (e) + getMessage (iExceptionCode);
-}
-
-void AxisDivByZeroException::processException (SOAPStructFault* pFault)
-{
-    /*start user code*/
-    int iSize = strlen(pFault->varString) + 8;
+	/*User can do something like deserializing the struct into a string*/
+        int iSize = strlen(pFault->varString) + 8;
     char* sMessage = (char*) malloc(iSize);
     m_sMessage = (char*) malloc(iSize);
     char carrTempBuff[4];
@@ -86,47 +67,44 @@ void AxisDivByZeroException::processException (SOAPStructFault* pFault)
     strcat(sMessage, "\n");
     m_sMessage = sMessage;
     free(sMessage);
-    /*end user code*/
 }
 
-void AxisDivByZeroException::processException (exception* e)
+void AxisDivByZeroException::processException(exception* e)
 {
-    m_sMessage = getMessage (e);
+	m_sMessage = getMessage (e);
 }
 
 void AxisDivByZeroException::processException(int iExceptionCode)
 {
-    m_sMessage = getMessage (iExceptionCode);
+	m_sMessage = getMessage (iExceptionCode);
 }
 
 const string AxisDivByZeroException::getMessage (exception* objException)
 {
-    string sMessage = objException->what();
-
-    return sMessage;
+	string sMessage = objException->what();
+	return sMessage;
 }
 
 const string AxisDivByZeroException::getMessage (int iExceptionCode)
 {
-    string sMessage;
-    switch(iExceptionCode)
-    {
-        case AXISC_SERVICE_THROWN_EXCEPTION:
-            sMessage = "A service has thrown an exception. see detail";
-            break;
-        default:
-            sMessage = "Unknown Exception has occured";
-    }
-    return sMessage;
+	string sMessage;
+	switch(iExceptionCode)
+	{
+		case AXISC_SERVICE_THROWN_EXCEPTION:
+		sMessage = "A service has thrown an exception. see detail";
+		break;
+		default:
+		sMessage = "Unknown Exception has occured";
+	}
+return sMessage;
 }
 
 const char* AxisDivByZeroException::what() throw ()
 {
-    return m_sMessage.c_str ();
+	return m_sMessage.c_str ();
 }
 
-const int AxisDivByZeroException::getExceptionCode()
-{
-    return m_iExceptionCode;
+const int AxisDivByZeroException::getExceptionCode(){
+	return m_iExceptionCode;
 }
 
--- c/samples/client/rpcfault/AxisDivByZeroException.h
+++ c/samples/client/rpcfault/AxisDivByZeroException.h
@@ -1,56 +1,38 @@
 /*
- *   Copyright 2003-2004 The Apache Software Foundation.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- *
- *   @author wsdl2ws generated
+ * This is the Client Stub Class genarated by the tool WSDL2Ws
+ * AxisDivByZeroException.h: interface for the AxisDivByZeroExceptionclass.
  *
  */
- 
-#ifndef __AXISDIVBYZEROEXCEPTION_H_OF_AXIS_INCLUDED_
-#define __AXISDIVBYZEROEXCEPTION_H_OF_AXIS_INCLUDED_
+#if !defined(__AXISDIVBYZEROEXCEPTION_EXCEPTION_H__INCLUDED_)
+#define __AXISDIVBYZEROEXCEPTION_EXCEPTION_H__INCLUDED_
 
 #include <string>
 #include <exception>
 #include <axis/server/AxisException.h>
-#include "SOAPStructFault.h"
+#include "DivByZeroStruct.h"
 
 using namespace std;
-
-class AxisDivByZeroException : public AxisException
+class AxisDivByZeroException: public AxisException
 {
-
 public:
-    AxisDivByZeroException();
-    AxisDivByZeroException(SOAPStructFault* pFault);
-    AxisDivByZeroException(int iExceptionCode);
-    AxisDivByZeroException(exception* e);
-    AxisDivByZeroException(exception* e, int iExceptionCode);
-    virtual ~AxisDivByZeroException() throw();
-    const char* what() throw();
-    const int getExceptionCode();
-    const string getMessage(exception* e);
-    const string getMessage(int iExceptionCode);
-                                                                                                                             
+	AxisDivByZeroException();
+	AxisDivByZeroException(DivByZeroStruct* pFault);
+	AxisDivByZeroException(int iExceptionCode);
+	AxisDivByZeroException(exception* e);
+	AxisDivByZeroException(exception* e, int iExceptionCode);
+	virtual ~AxisDivByZeroException() throw();
+	 const char* what() throw();
+	 const int getExceptionCode();
+	 const string getMessage(exception* e);
+	 const string getMessage(int iExceptionCode);
 private:
-    void processException(exception* e);
-    void processException(SOAPStructFault* pFault);
-    void processException(exception* e, int iExceptionCode);
-    void processException(int iExceptionCode);
-    string m_sMessage;
-    int m_iExceptionCode;
+	 void processException(exception* e);
+	 void processException(DivByZeroStruct* pFault);
+	 void processException(exception* e, int iExceptionCode);
+	 void processException(int iExceptionCode);
+	 string m_sMessage;
+	 int m_iExceptionCode;
+
 };
 
-#endif
-
+#endif /* !defined(__AXISDIVBYZEROEXCEPTION_EXCEPTION_H__INCLUDED_)*/
