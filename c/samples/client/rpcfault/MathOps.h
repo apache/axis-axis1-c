17da4006bd94808e4ed15e943c297f920e1be20f
Damitha N.M. Kumarage <damitha@apache.org>
2004-06-08 07:31:09 +0000

    *** empty log message ***

--- c/samples/client/rpcfault/MathOps.h
+++ c/samples/client/rpcfault/MathOps.h
@@ -3,25 +3,26 @@
  * MathOps.h: interface for the MathOpsclass.
  *
  */
-#if !defined(__MATHOPS_CLIENTSTUB_H__OF_AXIS_INCLUDED_)
-#define __MATHOPS_CLIENTSTUB_H__OF_AXIS_INCLUDED_
+#if !defined(__MATHOPS_CLIENTSTUB_H__INCLUDED_)
+#define __MATHOPS_CLIENTSTUB_H__INCLUDED_
 
 #include <axis/client/Call.h>
-#include "SOAPStructFault.h"
+/*include Exception header files here*/
 #include "AxisDivByZeroException.h"
-#include <ctype.h>
+#include "DivByZeroStruct.h"
 
 class MathOps 
 {
 private:
 	Call* m_pCall;
 public:
-	MathOps(const char* pchEndpointUri);
+	MathOps();
 public:
 	virtual ~MathOps();
 public: 
 	int div(int Value0,int Value1);
-        int getFaultDetail(char** ppcDetail);
+	int getStatus();
+	int getFaultDetail(char** ppcDetail);
 };
 
-#endif /* !defined(__MATHOPS_CLIENTSTUB_H__OF_AXIS_INCLUDED_)*/
+#endif /* !defined(__MATHOPS_CLIENTSTUB_H__INCLUDED_)*/
