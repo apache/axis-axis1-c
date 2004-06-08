753d087db099bd5bcd629086ab84a435a3739d54
Damitha N.M. Kumarage <damitha@apache.org>
2004-06-08 07:22:08 +0000

    *** empty log message ***

--- samples/server/rpcfault/MathOpsWrapper.cpp
+++ samples/server/rpcfault/MathOpsWrapper.cpp
@@ -6,19 +6,11 @@
 
 #include "MathOpsWrapper.h"
 
-extern int Axis_DeSerialize_SOAPStructFault(SOAPStructFault* param, 
-    IWrapperSoapDeSerializer *pDZ);
-
-extern void* Axis_Create_SOAPStructFault(SOAPStructFault *Obj, bool bArray = false, 
-    int nSize=0);
-
-extern void Axis_Delete_SOAPStructFault(SOAPStructFault* param, bool bArray = false, 
-    int nSize=0);
-
-extern int Axis_Serialize_SOAPStructFault(SOAPStructFault* param, IWrapperSoapSerializer* pSZ, 
-    bool bArray = false);
-
-extern int Axis_GetSize_SOAPStructFault();
+extern int Axis_DeSerialize_DivByZeroStruct(DivByZeroStruct* param, IWrapperSoapDeSerializer *pDZ);
+extern void* Axis_Create_DivByZeroStruct(DivByZeroStruct *Obj, bool bArray = false, int nSize=0);
+extern void Axis_Delete_DivByZeroStruct(DivByZeroStruct* param, bool bArray = false, int nSize=0);
+extern int Axis_Serialize_DivByZeroStruct(DivByZeroStruct* param, IWrapperSoapSerializer* pSZ, bool bArray = false);
+extern int Axis_GetSize_DivByZeroStruct();
 
 MathOpsWrapper::MathOpsWrapper()
 {
@@ -64,11 +56,10 @@ int MathOpsWrapper::invoke(void *pMsg)
 /*
  * This method wrap the service method 
  */
-int MathOpsWrapper::div(void* pMsg) throw(AxisDivByZeroException)
+int MathOpsWrapper::div(void* pMsg)
 {
 	IMessageData* mc = (IMessageData*)pMsg;
 	int nStatus;
-        int ret;
 	IWrapperSoapSerializer *pIWSSZ = NULL;
 	mc->getSoapSerializer(&pIWSSZ);
 	if (!pIWSSZ) return AXIS_FAIL;
@@ -76,30 +67,29 @@ int MathOpsWrapper::div(void* pMsg) throw(AxisDivByZeroException)
 	mc->getSoapDeSerializer(&pIWSDZ);
 	if (!pIWSDZ) return AXIS_FAIL;
 	/* check whether we have got correct message */
-	if (AXIS_SUCCESS != pIWSDZ->checkMessageBody("div", "http://localhost/axis/MathOps")) return AXIS_FAIL;
-	pIWSSZ->createSoapMethod("divResponse", "http://localhost/axis/MathOps");
-	int v0 = pIWSDZ->getElementAsInt("in0",0);
-	int v1 = pIWSDZ->getElementAsInt("in1",0);
+	if (AXIS_SUCCESS != pIWSDZ->checkMessageBody("div", "http://soapinterop.org/wsdl")) return AXIS_FAIL;
+	pIWSSZ->createSoapMethod("divResponse", "http://soapinterop.org/wsdl");
+	int v0 = pIWSDZ->getElementAsInt("int0",0);
+	int v1 = pIWSDZ->getElementAsInt("int1",0);
 	if (AXIS_SUCCESS != (nStatus = pIWSDZ->getStatus())) return nStatus;
-        try
-        {
-	    ret = pWs->div(v0,v1);
-        }
-        catch(AxisDivByZeroException& e)
-        {
-            pIWSSZ->createSoapFault("SOAPStructFault", "http://soapinterop.org/"); 
-            SOAPStructFault* pObjFault = new SOAPStructFault();
-            pObjFault->varString = "Division by zero exception";
-            pObjFault->varInt = 1;
-            pObjFault->varFloat = 10.52;
+	try
+	{
+		int ret = pWs->div(v0,v1);
+		return pIWSSZ->addOutputParam("divReturn", (void*)&ret, XSD_INT);
+	}
+	catch(AxisDivByZeroException& e)
+	{
+		pIWSSZ->createSoapFault("DivByZeroStruct", "http://soapinterop.org/wsdl");
+		DivByZeroStruct* pObjFault = new DivByZeroStruct();
+		/*User may write code here to fill the struct*/
+                pObjFault->varString = "Division by zero exception";
+                pObjFault->varInt = 1;
+                pObjFault->varFloat = 10.52;
+		if (pObjFault)
+			pIWSSZ->addFaultDetail(pObjFault, (void*) Axis_Serialize_DivByZeroStruct,
+			(void*) Axis_Delete_DivByZeroStruct,"DivByZeroFault", Axis_URI_DivByZeroStruct);
+		throw;
+	}
 
-            if(pObjFault)
-                pIWSSZ->addFaultDetail(pObjFault, (void*)Axis_Serialize_SOAPStructFault,
-                    (void*)Axis_Delete_SOAPStructFault, "DivByZeroException", 
-                    Axis_URI_SOAPStructFault);
-
-            throw;
-        }
-	return pIWSSZ->addOutputParam("divReturn", (void*)&ret, XSD_INT);
 }
 
--- samples/server/rpcfault/MathOpsWrapper.h
+++ samples/server/rpcfault/MathOpsWrapper.h
@@ -1,13 +1,12 @@
 /*
  * This is the Service Class genarated by the tool WSDL2Ws
- *		MathOpsWrapper.h: interface for the MathOpsWrapper class.
+ *		MathOpsWrapper.h: interface for the MathOpsWrapperclass.
  *
  */
-#if !defined(__MATHOPSWRAPPER_SERVERWRAPPER_H__OF_AXIS_INCLUDED_)
-#define __MATHOPSWRAPPER_SERVERWRAPPER_H__OF_AXIS_INCLUDED_
+#if !defined(__MATHOPSWRAPPER_SERVERWRAPPER_H__INCLUDED_)
+#define __MATHOPSWRAPPER_SERVERWRAPPER_H__INCLUDED_
 
 #include "MathOps.h"
-#include "SOAPStructFault.h"
 #include <axis/server/WrapperClassHandler.h>
 #include <axis/server/IMessageData.h>
 #include <axis/server/GDefine.h>
@@ -29,7 +28,7 @@ public:/*implementation of WrapperClassHandler interface*/
 	int AXISCALL fini();
 	AXIS_BINDING_STYLE AXISCALL getBindingStyle(){return RPC_ENCODED;};
 private:/*Methods corresponding to the web service methods*/
-	int div(void* pMsg) throw(AxisDivByZeroException);
+	int div(void* pMsg);
 };
 
-#endif /* !defined(__CALCULATORWRAPPER_SERVERWRAPPER_H__OF_AXIS_INCLUDED_)*/
+#endif /* !defined(__MATHOPSWRAPPER_SERVERWRAPPER_H__INCLUDED_)*/
