d82136c14f1bdbfd0145402c89d3c49ed07ca248
Don Samisa Abeysinghe <samisa@apache.org>
2004-06-11 11:02:48 +0000

    Updated the test to help testing set/get/delete SOAP/trasport headers and tested Did not test SOAP method attribute related methods

--- c/tests/client/stubTest/stubBase/InteropBaseClient.cpp
+++ c/tests/client/stubTest/stubBase/InteropBaseClient.cpp
@@ -31,117 +31,167 @@ using namespace std;
 #define ARRAYSIZE 2
 
 int
-main (int argc, char *argv[])
+main(int argc, char *argv[])
 {
-  int x;
-  char buffer1[100];
-  char endpoint[256];
-  const char *server = "localhost";
-  const char *port = "80";
-  if (argc == 3)
+    int x;
+    char buffer1[100];
+    char endpoint[256];
+    const char *server = "localhost";
+    const char *port = "80";
+    if (argc == 3)
     {
-      server = argv[1];
-      port = argv[2];
+	server = argv[1];
+	port = argv[2];
     }
-  printf ("Usage :\n %s <server> <port>\n\n", argv[0]);
-  //sprintf(endpoint, "http://%s:%s/axis/base", server, port);
-  //endpoint for Axis Java sample
-  sprintf (endpoint, "http://%s:%s/axis/services/echo", server, port);
-  InteropTestPortType ws (endpoint);
+    printf("Usage :\n %s <server> <port>\n\n", argv[0]);
+    //sprintf(endpoint, "http://%s:%s/axis/base", server, port);
+    //endpoint for Axis Java sample
+    sprintf(endpoint, "http://%s:%s/axis/services/echo", server, port);
+    InteropTestPortType ws(endpoint);
 
-  //set end point (optional) 
-  //ws.setEndPoint( "http://localhost:8080/axis/services/echo" );
-  //set proxy (optional) 
-  //ws.setProxy( "proxy.my.ibm.com", 80 );
+    //set end point (optional) 
+    //ws.setEndPoint( "http://localhost:8080/axis/services/echo" );
+    //set proxy (optional) 
+    //ws.setProxy( "proxy.my.ibm.com", 80 );
 
-  //set HTTP headers
-  ws.setTransportProperty ("Accept-Language", " da, en-gb;q=0.8, en;q=0.7");
-  ws.setTransportProperty ("Accept-Language", "lang2");
-  ws.setTransportProperty ("Accept-Language", "lang3");
+    //set HTTP headers
+    ws.setTransportProperty("Accept-Language", " da, en-gb;q=0.8, en;q=0.7");
+    ws.setTransportProperty("Accept-Language", "lang2");
+    ws.setTransportProperty("Accept-Language", "lang3");
 
-  /*create a header of the form:
-     <SOAP-ENV:Header>
-         <th:newName xmlns:th="http://ws.apache.org/axisCppTest/">
-             <TestAuthHeader>
-                 <username>Test User</username>
-                 <password>Test Password</password>
-             </TestAuthHeader>
-         </th:newName>
-     </SOAP-ENV:Header>
-   */
+    /*create a header of the form:
+       <SOAP-ENV:Header>
+       <th:newName xmlns:th="http://ws.apache.org/axisCppTest/">
+       <TestAuthHeader>
+       <username>Test User</username>
+       <password>Test Password</password>
+       </TestAuthHeader>
+       </th:newName>
+       </SOAP-ENV:Header>
+     */
 
-  //set SOAP headers
-  IHeaderBlock *phb = ws.createSOAPHeaderBlock ("TestHeader", "th",
-					    "http://ws.apache.org/axisCppTest/");
+    //set SOAP headers
+    IHeaderBlock *phb = ws.createSOAPHeaderBlock("TestHeader", "th",
+						 "http://ws.apache.org/axisCppTest/");
 
-  //create parent node
-  BasicNode *parentNode = phb->createChild (ELEMENT_NODE);
-  parentNode->setLocalName ("Credentials");
-  //create child node     
-  BasicNode *childNode = phb->createChild (ELEMENT_NODE);
-  childNode->setLocalName ("username");
-  //create char node for value
-  BasicNode *valueNode = phb->createChild (CHARACTER_NODE);
-  valueNode->setValue ("Test User");
-  //buld node tree
-  childNode->addChild (valueNode);
-  parentNode->addChild (childNode);
+    //create parent node
+    BasicNode *parentNode = phb->createChild(ELEMENT_NODE);
+    parentNode->setLocalName("Credentials");
+    //create child node     
+    BasicNode *childNode = phb->createChild(ELEMENT_NODE);
+    childNode->setLocalName("username");
+    //create char node for value
+    BasicNode *valueNode = phb->createChild(CHARACTER_NODE);
+    valueNode->setValue("Test User");
+    //buld node tree
+    childNode->addChild(valueNode);
+    parentNode->addChild(childNode);
 
-  //add another node set
-  childNode = phb->createChild (ELEMENT_NODE);
-  childNode->setLocalName ("password");
+    //add another node set
+    childNode = phb->createChild(ELEMENT_NODE);
+    childNode->setLocalName("password");
 
-  valueNode = phb->createChild (CHARACTER_NODE);
-  valueNode->setValue ("Test Password");
+    valueNode = phb->createChild(CHARACTER_NODE);
+    valueNode->setValue("Test Password");
 
-  childNode->addChild (valueNode);
-  parentNode->addChild (childNode);
+    childNode->addChild(valueNode);
+    parentNode->addChild(childNode);
 
-  phb->addChild (parentNode);
+    phb->addChild(parentNode);
 
-  //set timeout
-  ws.setTransportTimeout(4);
+    //set second SOAP headers
+    phb = ws.createSOAPHeaderBlock("TestHeader2", "th",
+				   "http://ws.apache.org/axisCppTest/");
 
-  printf ("Sending Requests to end point %s \n\n", endpoint);
-  printf ("invoking echoString...\n");
-  //testing echoString 
-  string bigstring;
-  for (int ii = 0; ii < 2; ii++)
+    //create parent node
+    parentNode = phb->createChild(ELEMENT_NODE);
+    parentNode->setLocalName("Credentials2");
+    childNode = phb->createChild(ELEMENT_NODE);
+    childNode->setLocalName("username");
+    valueNode = phb->createChild(CHARACTER_NODE);
+    valueNode->setValue("Test User");
+    childNode->addChild(valueNode);
+    parentNode->addChild(childNode);
+    //add another node set
+    childNode = phb->createChild(ELEMENT_NODE);
+    childNode->setLocalName("password");
+    valueNode = phb->createChild(CHARACTER_NODE);
+    valueNode->setValue("Test Password");
+    childNode->addChild(valueNode);
+    parentNode->addChild(childNode);
+    phb->addChild(parentNode);
+
+    //set timeout
+    ws.setTransportTimeout(4);
+
+    printf("Sending Requests to end point %s \n\n", endpoint);
+    printf("invoking echoString...\n");
+    //testing echoString 
+    string bigstring;
+    for (int ii = 0; ii < 2; ii++)
     {
-      bigstring += "hello world ";
+	bigstring += "hello world ";
     }
-  strcpy (buffer1, bigstring.c_str ());
-  //printf(ws.echoString(buffer1));
-  if (0 == strcmp (ws.echoString ("hello world"), "hello world"))
-    printf ("successful\n");
-  else
-    printf ("failed\n");
-  
-  printf( "Test trasport property accessors\n" );
-  printf( "First trasport key = %s\n", ws.getFirstTrasportPropertyKey() );
-  printf( "First trasport value = %s\n", ws.getCurrentTrasportPropertyValue() );
-  char* key = NULL;
-  while( key =  ws.getNextTrasportPropertyKey() )
-  {
-    printf( "Next trasport key = %s\n", key );
-    printf( "Next trasport value = %s\n", ws.getCurrentTrasportPropertyValue() );
-  }
+    strcpy(buffer1, bigstring.c_str());
+    //printf(ws.echoString(buffer1));
+    if (0 == strcmp(ws.echoString("hello world"), "hello world"))
+	printf("successful\n");
+    else
+	printf("failed\n");
 
-  // testing echoStringArray 
-  xsd__string_Array arrstr;
-  arrstr.m_Array = new char *[ARRAYSIZE];
-  arrstr.m_Size = ARRAYSIZE;
-  for (int i = 0; i < ARRAYSIZE; i++)
+    printf("Test trasport property accessors\n");
+    printf("First trasport key = %s\n", ws.getFirstTrasportPropertyKey());
+    printf("First trasport value = %s\n",
+	   ws.getCurrentTrasportPropertyValue());
+    char *key = NULL;
+    int count = 1;
+    while (key = ws.getNextTrasportPropertyKey())
     {
-      sprintf (buffer1, "%dth element of string array", i + 1);
-      arrstr.m_Array[i] = buffer1;
+	printf("Next trasport key = %s\n", key);
+	printf("Next trasport value = %s\n",
+	       ws.getCurrentTrasportPropertyValue());
+	count++;
+	//test removal of last trasport property
+	if (count == 3)
+	    ws.deleteCurrentTrasportProperty();
     }
-  printf ("invoking echoStringArray...\n");
-  if (ws.echoStringArray (arrstr).m_Array != NULL)
-    printf ("successful\n");
-  else
-    printf ("failed\n");
 
-  printf ("Stub Base class test end\n");
-  return 0;
+    ws.deleteTrasportProperty("Accept-Language", 2);
+
+    printf("First header block no of children = %d\n",
+	   ws.getFirstSOAPHeaderBlock()->getNoOfChildren());
+    IHeaderBlock *header = NULL;
+    count = 1;
+    while (header = ws.getNextSOAPHeaderBlock())
+    {
+	printf("Next header block no of children = %d\n",
+	       header->getNoOfChildren());
+	count++;
+	//test removal of last SOAP header block
+	if (count == 2)
+	    ws.deleteCurrentSOAPHeaderBlock();
+    }
+
+    //test removing SOAP header block using pointer
+    header = ws.getFirstSOAPHeaderBlock();
+    ws.deleteSOAPHeaderBlock(header);
+    //now the request should have no SOAP headers
+
+    // testing echoStringArray 
+    xsd__string_Array arrstr;
+    arrstr.m_Array = new char *[ARRAYSIZE];
+    arrstr.m_Size = ARRAYSIZE;
+    for (int i = 0; i < ARRAYSIZE; i++)
+    {
+	sprintf(buffer1, "%dth element of string array", i + 1);
+	arrstr.m_Array[i] = buffer1;
+    }
+    printf("invoking echoStringArray...\n");
+    if (ws.echoStringArray(arrstr).m_Array != NULL)
+	printf("successful\n");
+    else
+	printf("failed\n");
+
+    printf("Stub Base class test end\n");
+    return 0;
 }
