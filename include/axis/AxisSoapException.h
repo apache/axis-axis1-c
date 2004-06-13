fa322257efe86b7e1a91c5af00e034d8876208fa
Damitha N.M. Kumarage <damitha@apache.org>
2004-06-13 11:31:18 +0000

    *** empty log message ***

---
+++ include/axis/AxisSoapException.h
@@ -0,0 +1,52 @@
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *
+ *   @author Damitha Kumarage (damitha@opensource.lk, damitha@jkcsworld.com)
+ *
+ */
+ 
+#ifndef __AXISSOAPEXCEPTION_H_OF_AXIS_INCLUDED_
+#define __AXISSOAPEXCEPTION_H_OF_AXIS_INCLUDED_
+
+#include <string>
+#include <exception>
+#include <axis/server/AxisException.h>
+using namespace std;
+
+class AxisSoapException :public AxisException
+{
+
+public:
+    AxisSoapException();
+    AxisSoapException(int iExceptionCode);
+    AxisSoapException(exception* e);
+    AxisSoapException(exception* e, int iExceptionCode);
+    virtual ~AxisSoapException() throw();
+    const char* what() throw();
+    const int getExceptionCode();
+    const string getMessage(exception* e);
+    const string getMessage(int iExceptionCode);
+                                                                                                                             
+private:
+    void processException(exception* e);
+    void processException(exception* e, int iExceptionCode);
+    void processException(int iExceptionCode);
+    string m_sMessage;
+    int m_iExceptionCode;
+};
+
+#endif
+
