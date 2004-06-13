adf4a41eec8b9c33f7d58af9ea4dea01e18a218a
Damitha N.M. Kumarage <damitha@apache.org>
2004-06-13 14:26:48 +0000

    *** empty log message ***

---
+++ tests/client/exceptionTest/AxisDivByZeroException.cpp
@@ -0,0 +1,110 @@
+/*
+ * This is the  implementation file genarated by WSDL2Ws tool.
+ * AxisDivByZeroException.cpp: implemtation for the AxisDivByZeroException.
+ *
+ */
+
+#include "AxisDivByZeroException.h"
+
+#include <axis/server/AxisWrapperAPI.h>
+
+AxisDivByZeroException::AxisDivByZeroException()
+{
+/* This only serves the pupose of indicating that the 
+ * service has thrown an excpetion 
+ */ 
+	m_iExceptionCode = AXISC_SERVICE_THROWN_EXCEPTION; 
+	processException(m_iExceptionCode); 
+}
+
+AxisDivByZeroException::AxisDivByZeroException(DivByZeroStruct*pFault)
+{
+	m_iExceptionCode = AXISC_SERVICE_THROWN_EXCEPTION;
+	processException(pFault);}
+
+AxisDivByZeroException::AxisDivByZeroException(int iExceptionCode)
+{
+
+	m_iExceptionCode = iExceptionCode;
+	processException (iExceptionCode);
+}
+
+AxisDivByZeroException::AxisDivByZeroException(exception* e)
+{
+	processException (e);
+}
+
+AxisDivByZeroException::AxisDivByZeroException(exception* e,int iExceptionCode)
+{
+
+	processException (e, iExceptionCode);
+}
+
+AxisDivByZeroException::~AxisDivByZeroException() throw () 
+{
+	m_sMessage ="";
+}
+
+void AxisDivByZeroException:: processException(exception* e, int iExceptionCode)
+{
+	m_sMessage = getMessage (e) + getMessage (iExceptionCode);
+}
+
+void AxisDivByZeroException::processException (DivByZeroStruct* pFault)
+{
+	/*User can do something like deserializing the struct into a string*/
+        int iSize = strlen(pFault->varString) + 8;
+        char* sMessage = (char*) malloc(iSize);
+        m_sMessage = (char*) malloc(iSize);
+        char carrTempBuff[4];
+        sprintf(carrTempBuff, "%d", pFault->varInt);
+        strcat(sMessage, carrTempBuff);
+        strcat(sMessage, "\n");
+        sprintf(carrTempBuff, "%f", pFault->varFloat);
+        strcat(sMessage, carrTempBuff);
+        strcat(sMessage, "\n");
+        strcat(sMessage, pFault->varString);
+        strcat(sMessage, "\n");
+        m_sMessage = sMessage;
+        free(sMessage);
+}
+
+void AxisDivByZeroException::processException(exception* e)
+{
+	m_sMessage = getMessage (e);
+}
+
+void AxisDivByZeroException::processException(int iExceptionCode)
+{
+	m_sMessage = getMessage (iExceptionCode);
+}
+
+const string AxisDivByZeroException::getMessage (exception* objException)
+{
+	string sMessage = objException->what();
+	return sMessage;
+}
+
+const string AxisDivByZeroException::getMessage (int iExceptionCode)
+{
+	string sMessage;
+	switch(iExceptionCode)
+	{
+		case AXISC_SERVICE_THROWN_EXCEPTION:
+		sMessage = "A service has thrown an exception. see detail";
+		break;
+		default:
+		sMessage = "Unknown Exception has occured";
+	}
+return sMessage;
+}
+
+const char* AxisDivByZeroException::what() throw ()
+{
+	return m_sMessage.c_str ();
+}
+
+const int AxisDivByZeroException::getExceptionCode(){
+	return m_iExceptionCode;
+}
+
