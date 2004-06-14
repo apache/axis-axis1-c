84f3a51995de837f4512c06b0d48787dbb5548ae
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2004-06-14 07:36:02 +0000

    added author tag

--- c/include/axis/server/AxisConfig.h
+++ c/include/axis/server/AxisConfig.h
@@ -12,6 +12,8 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
+ *
+ * @author Sanjaya Singharage (sanjayas@opensource.lk, sanjayas@jkcsworld.com)
  */
  
 #if !defined(__AXIS_AXISCONFIG_H_OF_AXIS_INCLUDED__)
--- c/src/server/apache/mod_axis.cpp
+++ c/src/server/apache/mod_axis.cpp
@@ -13,6 +13,9 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
+ *
+ * @author Sanjaya Singharage (sanjayas@opensource.lk, sanjayas@jkcsworld.com)
+ * @author Susantha Kumara (susantha@opensource.lk, skumara@virtusa.com)
  */
 
 #include "ApacheTransport.h"
