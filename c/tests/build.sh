ee23a81705219ffac6832b0e24e4921f1c7f9685
Damitha N.M. Kumarage <damitha@apache.org>
2004-06-14 06:23:22 +0000

    *** empty log message ***

--- c/tests/build.sh
+++ c/tests/build.sh
@@ -1,5 +1,21 @@
-sh autogen.sh
-sh runconfig
-make
+#!/bin/bash
+echo "run libtoolize."          && 
+libtoolize --force              && 
+echo "run aclocal."             && 
+aclocal                         && 
+echo "run autoconf."            && 
+autoconf                        && 
+echo "run autoheader."          && 
+autoheader                      && 
+echo "run automake."            && 
+automake --add-missing --gnu    &&
+
+echo "./configure --bindir=$AXISCPP_HOME/tests/client/"
+./configure --bindir=$AXISCPP_HOME/tests/client/
+
+echo "make"
+make 2> tests_build_errors
+
+echo "make install"
 make install
-#strip -g sslclient base cbase groupB doclitbase doclitgroupB 
+
