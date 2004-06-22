aabbaa2b4984faa90253358f06446bb1fd229a2d
Hemapani Srinath Perera <hemapani@apache.org>
2004-06-22 09:06:10 +0000

    add test cases to test WAR and JAR files

--- contrib/ews/samples/ejb/bookquote/com/jwsbook/jaxrpc/BookQuote.java
+++ contrib/ews/samples/ejb/bookquote/com/jwsbook/jaxrpc/BookQuote.java
@@ -8,5 +8,5 @@
 package com.jwsbook.jaxrpc;
 
 public interface BookQuote extends java.rmi.Remote {
-    public float getBookPrice(java.lang.String isbn) throws java.rmi.RemoteException, com.jwsbook.jaxrpc.InvalidIsbnException;
+    public float getBookPrice(java.lang.String isbn) throws java.rmi.RemoteException;
 }
