87176af86fb3959c894cc20877e95a81684b4f38
Hemapani Srinath Perera <hemapani@apache.org>
2004-07-07 09:28:11 +0000

    fix the sample test cases

--- contrib/ews/samples/jaxrpc/book/ejb/ejb/com/jwsbook/jaxrpc/BookQuoteEJB.java
+++
@@ -1,7 +0,0 @@
-package com.jwsbook.jaxrpc;
-
-import javax.ejb.EJBObject;
-
-public interface BookQuoteEJB extends EJBObject {
-	public float getBookPrice(java.lang.String isbn) throws java.rmi.RemoteException;
-}
