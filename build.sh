2ced25373ca0008f0eba6445bed1befd095cb2ba
Damitha N.M. Kumarage <damitha@apache.org>
2004-07-27 03:31:17 +0000

    PR: Obtained from: Submitted by: Reviewed by:
    Changes for the linux build process to make it simpler

--- build.sh
+++ build.sh
@@ -6,15 +6,8 @@ echo "----------------------------------"
 
 echo "run configure"
 ./configure --prefix=/usr/local/axiscpp_deploy \
-    --libdir=/usr/local/axiscpp_deploy/lib \
-    --bindir=/usr/local/axiscpp_deploy/bin \
     --with-apache2=/usr/local/apache2 \
-    --with-apache=no \
-    --with-expat=/usr/local/expat1957 \
-    --with-xercesc=no \
-    --enable-samples=yes \
-    --enable-testcases=yes \
-    --sysconfdir=/usr/local/axiscpp_deploy/conf
+    --with-expat=/usr/local/expat1957
 echo "make"
 make 2> build_errors
 echo "make install"
--- deploy/deploy_apache.sh
+++ deploy/deploy_apache.sh
@@ -10,6 +10,9 @@ else
     mkdir ../log
 fi
 chmod 777 ../log
+cp -f ../etc/server.wsdd_linux ../etc/server.wsdd
+cp -f ../etc/client.wsdd_linux ../etc/client.wsdd
+cp -f ../etc/axiscpp.conf_linux ../etc/axiscpp.conf
 /usr/local/apache/bin/apachectl stop
 sleep 2
 /usr/local/apache/bin/apachectl start
