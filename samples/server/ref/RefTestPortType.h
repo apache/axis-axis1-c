3d121653f61869dfb2613d1b3817d48bcb81a5ba
Damitha N.M. Kumarage <damitha@apache.org>
2004-08-17 10:05:14 +0000

    *** empty log message ***

---
+++ samples/server/ref/RefTestPortType.h
@@ -0,0 +1,27 @@
+/*
+ * This is the Service Class genarated by the tool WSDL2Ws
+ * RefTestPortType.h: interface for the RefTestPortTypeclass.
+ *
+ */
+#if !defined(__REFTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_)
+#define __REFTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_
+
+#include <axis/server/AxisUserAPI.h>
+#include "AxisServiceException.h" 
+
+#include "intType.h"
+
+class RefTestPortType 
+{
+	public:
+		RefTestPortType();
+	public:
+		virtual ~RefTestPortType();
+	public: 
+		void onFault();
+		void init();
+		void fini();
+		intType* echoInt(intType* Value0);
+};
+
+#endif /* !defined(__REFTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_)*/
