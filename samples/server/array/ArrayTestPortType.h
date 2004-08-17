be3ad52b378a43602fc1f643eea9f3bd98ed3bfc
Damitha N.M. Kumarage <damitha@apache.org>
2004-08-17 06:55:51 +0000

    Added files for the array sample

---
+++ samples/server/array/ArrayTestPortType.h
@@ -0,0 +1,27 @@
+/*
+ * This is the Service Class genarated by the tool WSDL2Ws
+ * ArrayTestPortType.h: interface for the ArrayTestPortTypeclass.
+ *
+ */
+#if !defined(__ARRAYTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_)
+#define __ARRAYTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_
+
+#include <axis/server/AxisUserAPI.h>
+#include "AxisServiceException.h" 
+
+#include "intArrayType.h"
+
+class ArrayTestPortType 
+{
+	public:
+		ArrayTestPortType();
+	public:
+		virtual ~ArrayTestPortType();
+	public: 
+		void onFault();
+		void init();
+		void fini();
+		intArrayType* echoIntArray(intArrayType* Value0);
+};
+
+#endif /* !defined(__ARRAYTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_)*/
