7138d2e9cb853a560ea2f3b334a2debca8bcf498
Damitha N.M. Kumarage <damitha@apache.org>
2004-08-27 07:55:01 +0000

    PR: Obtained from: Submitted by: Reviewed by: Install process is now like complete for the moment. Updated the INSTALL to reflect this.

--- deploy/bin/deploy_apache.sh
+++ deploy/bin/deploy_apache.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+#
+#   Copyright 2003-2004 The Apache Software Foundation.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#
+#   @author Damitha Kumarage (damitha@opensource.lk, damitha@jkcsworld.com)
+#
+
 rm /usr/local/apache/libexec/libaxiscpp_mod.so
 rm ../lib/libaxis_xmlparser.so
 cp -f ../lib/libaxiscpp_mod.so /usr/local/apache/libexec/
 cp -f ../lib/libaxis_expat.so ../lib/libaxis_xmlparser.so
-if test -d ../log; then
-    echo "Checking log folder..."
-else
-    mkdir ../log
-fi
-chmod 777 ../log
 cp -f ../etc/server.wsdd_linux ../etc/server.wsdd
 cp -f ../etc/client.wsdd_linux ../etc/client.wsdd
 cp -f ../etc/axiscpp.conf_linux ../etc/axiscpp.conf
