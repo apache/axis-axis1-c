12fdd5ea1f5e97932278a31ff1b98d84e168fdc3
Damitha N.M. Kumarage <damitha@apache.org>
2004-08-30 05:42:27 +0000

    Removed hard coded url to take from command line

--- tests/client/xsdAnyTest1/ExtensibilityQueryMain.cpp
+++ tests/client/xsdAnyTest1/ExtensibilityQueryMain.cpp
@@ -1,18 +1,26 @@
 #include "gen_src/ExtensibilityQueryPortType.h"
 
-int main(void)
+int main(int argc, char* argv[])
 {
+    char endpoint[256];
+    const char* server="localhost";
+    const char* port="80";
     AnyType* pAny = new AnyType();
     pAny->_size = 2;
     pAny->_array = new char*[2];
+
+    server = argv[1];
+    port = argv[2];
 
     pAny->_array[0] = strdup("<queryExpression><queryByServiceDataNames xsi:type=\"ns1:QNamesType\" xmlns:ns1=\"http://www.gridforum.org/namespaces/2003/03/OGSI\"><name>serviceDataName</name></queryByServiceDataNames></queryExpression>");
     pAny->_array[1] = strdup("<getQuoteResponse xmlns=\"http://www.getquote.org/test\"><result><name>Widgets Inc.</name><symbol>WID</symbol><public>true</public></result></getQuoteResponse>");
 
     string str;
     try{
-
-        ExtensibilityQueryPortType* pStub = new ExtensibilityQueryPortType("http://localhost:9090/axis/testXSDANY",APTHTTP);
+	sprintf(endpoint, "http://%s:%s/axis/testXSDANY", server, port);
+        printf("endpoint:%s\n", endpoint);
+        //ExtensibilityQueryPortType* pStub = new ExtensibilityQueryPortType("http://localhost:5555/axis/testXSDANY",APTHTTP);
+        ExtensibilityQueryPortType* pStub = new ExtensibilityQueryPortType(endpoint);
         AnyType* pAnyReturn = pStub->query(pAny);
 		if (!pAnyReturn)
 		{
