07d8e4523e3baff0df84b95ea6d90e1f9f605e50
John Hawkins <hawkeye@apache.org>
2004-09-08 13:38:36 +0000

    Accidently overwrote original version with a version to call a java web service. restored original PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- tests/auto_build/testcases/client/cpp/CalculatorClient.cpp
+++ tests/auto_build/testcases/client/cpp/CalculatorClient.cpp
@@ -1,154 +1,66 @@
 #include "Calculator.h"
 #include <axis/AxisGenException.h>
-#include <iostream>
-#include <fstream>
+#include <ctype.h>
 
-#define WSDL_DEFAULT_ENDPOINT "http://localhost:9080/Calculator/services/Calculator"
-
-// Prototype
-bool parse_args_for_endpoint(int *argc, char *argv[], char **endpoint);
-void shift_args(int i, int *argc, char *argv[]);
-void setLogOptions(const char *output_filename);
-
-// If we re-direct cout it will be to this ofstream
-ofstream output_file;
+void PrintUsage();
+bool IsNumber(const char* p);
 
 int main(int argc, char* argv[])
 {
-    Calculator *ws;
-
-    char *endpoint = WSDL_DEFAULT_ENDPOINT;
-    bool endpoint_set = false;
-    int returnValue = 1; // Assume Failure
-
-    endpoint_set = parse_args_for_endpoint(&argc, argv, &endpoint);
-
-    try {
-        if(endpoint_set) {
-            ws = new Calculator(endpoint, APTHTTP);
-            free(endpoint);
-            endpoint_set = false;
-        } else
-            ws = new Calculator();
-
-        const char* op = "add";
-        int i1=2, i2=2;
+	char endpoint[256];
+	const char* url="http://192.168.101.4:80/axis/Calculator";
+	const char* op = 0;
+	int i1=0, i2=0;
         int iResult;
-        char* pcDetail = 0;
-    
-        if (strcmp(op, "add") == 0)
+        char* pcDetail;
+        pcDetail = 0;
+
+	url = argv[1];
+
+	try
         {
-            iResult = ws->add(i1, i2);
-            cout << iResult << endl;
+		printf("Sending Requests to Server %s .......\n\n", url);
+		sprintf(endpoint, "%s", url);
+		Calculator ws(endpoint);
+
+		op = "add";
+		i1 = 2;
+		i2 = 3;
+
+		if (strcmp(op, "add") == 0)
+		{
+        	        iResult = ws.add(i1, i2);
+                	ws.getFaultDetail(&pcDetail);
+	                printf("%d\n", iResult);
+        	        /*printf("pcDetail:%s\n", pcDetail);*/
+		}
         }
-        else     if (strcmp(op, "sub") == 0)
+        catch(AxisException& e)
         {
-            iResult = ws->sub(i1, i2);
-            cout << iResult << endl;
+            printf("Exception : %s\n", e.what());
         }
-        else     if (strcmp(op, "mul") == 0)
+        catch(exception& e)
         {
-            iResult = ws->mul(i1, i2);
-            cout << iResult << endl;
+            printf("Unknown exception has occured\n" );
         }
-        else     if (strcmp(op, "div") == 0)
+	catch(...)
         {
-            iResult = ws->div(i1, i2);
-            cout << iResult << endl;
+            printf("Unknown exception has occured\n" );
         }
-        else 
-        {
-            cout << "Invalid operation " << op << endl;
-        }
-        returnValue = 0; // Success
-    } catch(AxisException &e) {
-        cerr << e.what() << endl;
-        if(endpoint_set)
-          free(endpoint);
-    } catch(...) {
-        cerr << "Unknown Exception occurred." << endl;
-        if(endpoint_set)
-          free(endpoint);
-    }
-    return returnValue;
+	return 0;
 }
 
-/* Spin through args list and check for -e -p and -s options.
-   Option values are expected to follow the option letter as the next
-   argument.
- 
-   These options and values are removed from the arg list.
-   If both -e and -s and or -p, then -e takes priority
-*/
-bool parse_args_for_endpoint(int *argc, char *argv[], char **endpoint) {
-
-    // We need at least 2 extra arg after program name
-    if(*argc < 3)
-        return false;
-
-    char *server = "localhost";
-    int  port = 80;
-    bool ep_set = false;
-    bool server_set = false;
-    bool port_set = false;
-
-    for(int i=1; i<*argc; i++) {
-        if(*argv[i] == '-') {
-            switch(*(argv[i]+1)) {
-            case 'e':
-                *endpoint = strdup(argv[i+1]);
-                ep_set = true;
-                shift_args(i, argc, argv);
-                i--;
-                break;
-            case 's':
-                server = strdup(argv[i+1]);
-                server_set = true;
-                shift_args(i, argc, argv);
-                i--;
-                break;
-            case 'p':
-                port = atoi(argv[i+1]);
-                if(port >80) port_set = true;
-                shift_args(i, argc, argv);
-                i--;
-                break;
-            case 'o':
-                setLogOptions(argv[i+1]);
-                shift_args(i, argc, argv);
-                i--;
-                break;
-            default:
-                break;
-            }
-        }
-    }
-
-    // use the supplied server and/or port to build the endpoint
-    if(ep_set == false && (server_set || port_set)) {
-        // Set p to the location of the first '/' after the http:// (7 chars)
-        // e.g. from http://localhost:80/axis/base gets /axis/base
-        char *ep_context = strpbrk(&(*endpoint)[7], "/");
-
-        // http://:/ is 9 characters + terminating NULL character so add 10.
-        // Allow space for port number upto 999999 6 chars
-        *endpoint = (char *)calloc(1, 10 + strlen(ep_context) + strlen(server) + 6);
-        sprintf(*endpoint, "http://%s:%d/%s", server, port, ep_context+1);
-        if(server_set) free(server);
-        ep_set = true;
-    }
-
-    return ep_set;
+void PrintUsage()
+{
+	printf("Usage :\n Calculator <server> <port> <operation> <parameter> <parameter>\n\n");
+	exit(1);
 }
 
-void shift_args(int i, int *argc, char *argv[]) {
-    for(int j=i, k=i+2; j<*(argc)-2; j++, k++)
-                    argv[j]=argv[k];
-    *argc-=2;
-}
-void setLogOptions(const char *output_filename) {
-    output_file.open(output_filename, ios::out);
-    if(output_file.is_open()){
-        cout.rdbuf( output_file.rdbuf() );
-    }
+bool IsNumber(const char* p)
+{
+	for (int x=0; x < strlen(p); x++)
+	{
+		if (!isdigit(p[x])) return false;
+	}
+	return true;
 }
