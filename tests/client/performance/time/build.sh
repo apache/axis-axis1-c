7e3767d5739f42641549391ac9bde388eae68090
Damitha N.M. Kumarage <damitha@apache.org>
2004-10-12 11:05:16 +0000

    Sorry this my mistake. I removed those unneccsaray files samisa.

--- tests/client/performance/time/build.sh
+++ tests/client/performance/time/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 java -classpath $AXISCPP_HOME/lib/axis/wsdl2ws.jar:$CLASSPATH org.apache.axis.wsdl.wsdl2ws.WSDL2Ws -sclient -lc++ -ogen_src $AXISCPP_DEPLOY/wsdls/base.wsdl
-g++ -I$AXISCPP_HOME/include -L$AXISCPP_DEPLOY/lib -laxiscpp_client -ldl -olargeArrays Client.cpp gen_src/*.cpp
+g++ -I$AXISCPP_HOME/include -L$AXISCPP_DEPLOY/lib -laxiscpp_client -ldl -otimeTest Client.cpp gen_src/*.cpp
 
--- tests/client/performance/time/sample_gen_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#java GetInput <maximum line length> <array size>
-java GenInput 128 1024 > stringdata.txt
