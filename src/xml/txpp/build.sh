a0081f17b7d56d23029a510fca70a2ce7e949d51
Damitha N.M. Kumarage <damitha@apache.org>
2004-10-21 03:52:20 +0000

    *** empty log message ***

--- src/xml/txpp/build.sh
+++ src/xml/txpp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sh autogen.sh
 echo "run configure"
-./configure --prefix=/home/damitha/projects/inst_txpp
+./configure --prefix=/home/damitha/projects/cvshead/src/xml/txpp/install
 echo "make"
 make 2> build_errors
 make install
