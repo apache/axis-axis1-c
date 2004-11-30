eff472933cc265ad76789adbf8fed371adff48a9
Adrian Dick <dicka@apache.org>
2004-11-30 15:51:41 +0000

    Update ISecureChannel to include public method setTransportProperty(AXIS_TRANSPORT_INFORMATION_TYPE type, const char* value).
    Submitted by: Adrian Dick
    Reviewed by: Fred Preston

--- src/transport/axis2/ISecureChannel.hpp
+++ src/transport/axis2/ISecureChannel.hpp
@@ -1,6 +1,7 @@
 #if !defined(_AXIS_ISECURECHANNEL_HPP)
 #define _AXIS_ISECURECHANNEL_HPP
 #include <string>
+#include <axis/GDefine.hpp>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ class ISecureChannel
 public:
 	virtual void			setSecureProperties( const char *)=0;
 	virtual const char *	getSecureProperties()=0;
+	virtual int setTransportProperty(AXIS_TRANSPORT_INFORMATION_TYPE type, const char* value)=0;
 };
 
 #endif
