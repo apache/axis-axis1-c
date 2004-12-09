402550c6b670a7700e2154ca2f1407f2112c8ded
Sanjaya Gayan Prasanna Singharage <sanjaya@apache.org>
2004-12-09 10:45:06 +0000

    adding a missing java file needed to compile that Axis java web service for soap header based session sample

---
+++ samples/server/session/headers/counters/Counter_Port.java
@@ -0,0 +1,12 @@
+/**
+ * Counter_Port.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC1 Sep 29, 2004 (08:29:40 EDT) WSDL2Java emitter.
+ */
+
+package counters;
+
+public interface Counter_Port extends java.rmi.Remote {
+    public int count(int in0) throws java.rmi.RemoteException;
+}
