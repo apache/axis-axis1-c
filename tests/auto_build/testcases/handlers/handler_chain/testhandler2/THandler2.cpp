4b7bcf3b3b0073a92eceb4b2c622a0823e32ea2a
Andrew Perry <perryan@apache.org>
2004-12-11 15:26:15 +0000

    New Apache Ant based test framework has been added, including documentation. PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

---
+++ tests/auto_build/testcases/handlers/handler_chain/testhandler2/THandler2.cpp
@@ -0,0 +1,103 @@
+/*
+ *   Copyright 2003-2004 The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *
+ * @author Roshan Weerasuriya (roshan@opensource.lk, roshanw@jkcs.slt.lk)
+ *
+ */
+
+#include "THandler2.h"
+#include <axis/GDefine.hpp>
+#include <axis/IHandlerSoapSerializer.hpp>
+#include <axis/IHandlerSoapDeSerializer.hpp>
+#include <axis/BasicNode.hpp>
+#include <iostream>
+
+THandler2::THandler2()
+{
+    m_pOption = NULL;
+    m_sEmpty = "";
+}
+
+THandler2::~THandler2()
+{
+
+}
+
+const string& THandler2::getOption(const string& sArg)
+{
+  map<string, string>::const_iterator it = m_pOption->find(sArg);
+  if (it != m_pOption->end())
+  {
+      return (*it).second;
+  }
+  return m_sEmpty;	
+}
+
+void THandler2::setOptionList(const map<string, string>* OptionList)
+{
+   m_pOption = OptionList;
+}
+
+int THandler2::invoke(void *pvIMsg)
+{
+	IMessageData *pIMsg = (IMessageData*) pvIMsg;
+
+	if(pIMsg->isPastPivot()) {
+		/*this is a response*/
+
+		
+		
+	} else {
+		/*this is a request*/
+		
+		IHandlerSoapSerializer* pISZ;
+		pIMsg->getSoapSerializer(&pISZ);
+
+		IHeaderBlock* pIHeaderBlock= pISZ->createHeaderBlock();
+
+		pIHeaderBlock->setLocalName("echoMeString2");
+		pIHeaderBlock->setUri("http://soapinterop.org/echoheader/");
+
+		const AxisChar* pachHeaderVal = "Header Value2";
+
+		BasicNode* pBasicNode = pIHeaderBlock->createChild(CHARACTER_NODE);
+		pBasicNode->setValue(pachHeaderVal);
+		
+		pIHeaderBlock->addChild(pBasicNode);
+		
+	}
+
+	return AXIS_SUCCESS;
+}
+
+void THandler2::onFault(void *pvIMsg)
+{
+
+}
+
+int THandler2::init()
+{
+	//do any initialization, resetting of values
+
+	return AXIS_SUCCESS;
+}
+
+int THandler2::fini()
+{
+	//do any finalizatoin
+
+	return AXIS_SUCCESS;
+}
