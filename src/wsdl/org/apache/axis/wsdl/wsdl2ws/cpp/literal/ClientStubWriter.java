d964ee168b47461eea28baf6e5faa5581d18b80d
Adrian Dick <dicka@apache.org>
2005-01-14 11:34:31 +0000

    Complex elements without a namespace were incorrectly being given the namespace of their parent.
    Submitted by: Adrian Dick

--- src/wsdl/org/apache/axis/wsdl/wsdl2ws/cpp/literal/ClientStubWriter.java
+++ src/wsdl/org/apache/axis/wsdl/wsdl2ws/cpp/literal/ClientStubWriter.java
@@ -511,29 +511,35 @@ public class ClientStubWriter
                         ((ParameterInfo) paramsB.get(i))
                             .getElementName()
                             .getNamespaceURI();
-                    // If namespace is empty then getNamespacePrefix will create a new prefix
-                    // with no namespace which causes the server to send a fault message.
-                    // If no namespace for current tag, then use WSDL default namespace
-                    if (namespace.length() == 0)
-                    {
-                        namespace = minfo.getInputMessage().getNamespaceURI();
-                    }
-                    int stringLength = 8 + 1 + parameterName.length() + 1;
 
-                    writer.write(
-                        "\tchar cPrefixAndParamName"
-                            + i
-                            + "["
-                            + stringLength
-                            + "];\n");
-                    writer.write(
-                        "\tsprintf( cPrefixAndParamName"
-                            + i
-                            + ", \"%s:"
-                            + parameterName
-                            + "\", getNamespacePrefix(\""
-                            + namespace
-                            + "\"));\n");
+					if (namespace.length() == 0)
+					{
+						writer.write(
+							"\tchar cPrefixAndParamName"
+								+ i
+								+ "["
+								+ "] = \""
+								+ parameterName
+								+ "\";\n");
+					}
+					else
+					{
+						int stringLength = 8 + 1 + parameterName.length() + 1;
+						writer.write(
+							"\tchar cPrefixAndParamName"
+							+ i
+							+ "["
+							+ stringLength
+							+ "];\n");
+						writer.write(
+							"\tsprintf( cPrefixAndParamName"
+								+ i
+								+ ", \"%s:"
+								+ parameterName
+								+ "\", getNamespacePrefix(\""
+								+ namespace
+								+ "\"));\n");
+					}
 
                     if (typeisarray)
                     {
