7859912a75fd478cf78c44b0d68c1ad7e5b91e70
Nithyakala Thangarajah <nithya@apache.org>
2005-01-21 12:42:36 +0000

    added files to support ws-attachments

---
+++ src/server/apache2/AttachmentHelper.h
@@ -0,0 +1,52 @@
+// AttachmentHelper.h: interface for the AttachmentHelper class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_ATTACHMENTHELPER_H__F133843A_6A8F_4443_9DCC_850C7DEEFB7E__INCLUDED_)
+#define AFX_ATTACHMENTHELPER_H__F133843A_6A8F_4443_9DCC_850C7DEEFB7E__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#pragma warning( disable : 4786 )
+
+#include <string>
+#include <map>
+#include <axis/ISoapAttachment.hpp>
+#include "../../soap/SoapAttachment.hpp"
+
+using namespace std;
+AXIS_CPP_NAMESPACE_USE
+
+/*
+struct ltstr
+{
+         //bool operator()(const string* s1, const string* s2) const
+		 bool operator()(const char* s1, const char* s2) const
+         {
+          //return strcmp(s1->c_str() , s2->c_str()) < 0;
+		  return strcmp(s1 , s2) < 0;
+         }
+};
+*/
+
+class AttachmentHelper  
+{
+private:
+	map<const string, ISoapAttachment*> mymap;
+	map<const string, ISoapAttachment*>::iterator iter;
+	char* pMime;
+public:
+	char* getIncomingSOAPMimeHeaders();
+	ISoapAttachment* getAttachment (const char* pcAttchId);
+	void extract_info(string header,const char* label,SoapAttachment* pSoapAttachment);
+	void extract_SOAPMimeHeaders(char* pBuffer);
+	void extract_Soap (char* pBuffer);
+	void extract_Attachment (char* pBuffer);
+	AttachmentHelper();
+	virtual ~AttachmentHelper();
+
+};
+
+#endif // !defined(AFX_ATTACHMENTHELPER_H__F133843A_6A8F_4443_9DCC_850C7DEEFB7E__INCLUDED_)
