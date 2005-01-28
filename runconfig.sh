7c74d411c4b8be055c1440fdc95f5dbab6c0078d
Damitha N.M. Kumarage <damitha@apache.org>
2005-01-28 05:48:42 +0000

    PR: Obtained from: Submitted by: Reviewed by:
    Added some comments

--- runconfig.sh
+++ runconfig.sh
@@ -17,6 +17,21 @@
 #
 #   @author Damitha Kumarage (damitha@opensource.lk, damitha@jkcsworld.com)
 #
+# Following is the configure options with a brief description
+# --prefix=path to your installation folder. default is /usr/ocal/axiscpp_deploy
+# --enable-tspp    Whether tspp parser library should be built. by default this is not built
+# --enable-axis3    build axis3 client transport library. default=no
+# --enable-testcases    build test cases. default=no
+# --enable-samples    build samples. default=yes
+# --enable-axis3    build axis3 client transport library. default=no
+# --with-apache2=PATH    path to apache2 installation. if not given apache2 module library is not built.
+# --with-apache=PATH    path to apache1 installation. if not given apache1 module library is not built.
+# --with-xercesc=PATH    path to xercesc installation. if not given xercesc parser library is not built.
+# --with-cppunit=PATH      build cppunit tests. this option is valid only if --enable-testcases option is yes
+# --with-axis2-ssl=PATH      build axis2 secure  transport channel library.
+# --with-axis3-ssl=PATH      build axis3 secure transport channel library. this can be enabled only if --enable-axis3
+#                            option is yes.
+ 
 
 echo "run configure with the options"
 echo     "--prefix=/usr/local/axiscpp_deploy"
