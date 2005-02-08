e86d66eedeca0fef631e151c2e6fbf402a3d0be9
Andrew Perry <perryan@apache.org>
2005-02-08 09:59:27 +0000

    Updated the test to make more sense. Construct stub with a bogus endpoint before setting the correct endpoint delete the stub when finished check the request on-the-wire as the endpoint gets adjusted to the test endpoint before comparison

--- tests/auto_build/testcases/client/cpp/TestSetEndpointClient.cpp
+++ tests/auto_build/testcases/client/cpp/TestSetEndpointClient.cpp
@@ -3,39 +3,40 @@
  */
 
 #include "Calculator.hpp"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
 	char endpoint[256];
+	const char* bogus_url="http://no-one.home/rubbish";
 	const char* url="http://localhost:80/axis/Calculator";
 	int iResult;
 
-	url = argv[1];
+	if(argc>1)
+		url = argv[1];
 
 	try
 	{
-		sprintf(endpoint, "%s", url);
-		Calculator ws;
-		printf("EndPointURI is = %s \n", url);
-		ws.setEndPoint(url);
+		Calculator* ws = new Calculator(bogus_url);
+		ws->setEndPoint(url);
 
-		iResult = ws.add(2,3);
-		printf("%d\n", iResult);
+		iResult = ws->add(2,3);
+		cout << iResult << endl;;
+		delete ws;
 	}
 	catch(AxisException& e)
 	{
-	    printf("in AxisException block \n");
-	    printf("Exception : %s\n", e.what());
+	    cout << e.what() << endl;
 	}
 	catch(exception& e)
 	{
-	    printf("Unknown exception has occured\n" );
+	    cout << "Unknown exception has occured" << endl;
 	}
 	catch(...)
         {
-	    printf("Unpecified exception has occured \n" );
+	    cout << "Unpecified exception has occured" << endl;
 	}
-
+	cout<< "---------------------- TEST COMPLETE -----------------------------"<< endl;
 	return 0;
 }
 
