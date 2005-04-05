3ac72c1c4aac1c1dfbd5a8f72d70d76255996316
Don Samisa Abeysinghe <samisa@apache.org>
2005-04-05 04:54:24 +0000

    Fixed some array related problems

--- tests/auto_build/testcases/client/cpp/InteropTestRound1Client.cpp
+++ tests/auto_build/testcases/client/cpp/InteropTestRound1Client.cpp
@@ -85,11 +85,14 @@ int main(int argc, char* argv[])
 		printf("failed\n");
 	// testing echoIntegerArray 
 	xsd__int_Array arrint;
-	*(arrint.m_Array) = new int[ARRAYSIZE];
-        arrint.m_Size = ARRAYSIZE;
+	arrint.m_Array = new int*[ARRAYSIZE];
+    arrint.m_Size = ARRAYSIZE;
+	int iToSend[ARRAYSIZE];
+
 	for (x=0;x<ARRAYSIZE;x++)
 	{
-		 *(arrint.m_Array[x]) = x;
+		iToSend[x] = x;
+		 arrint.m_Array[x] = &iToSend[x];
 	}
 	printf("invoking echoIntegerArray...\n");
 	if (ws.echoIntegerArray(arrint).m_Array != NULL)
@@ -105,11 +108,13 @@ int main(int argc, char* argv[])
 		printf("failed\n");
 	// testing echoFloat 
 	xsd__float_Array arrfloat;
-	 *(arrfloat.m_Array) = new float[ARRAYSIZE];
+	 arrfloat.m_Array = new float*[ARRAYSIZE];
 	arrfloat.m_Size = ARRAYSIZE;
+	float fToSend[ARRAYSIZE];
 	for (x=0;x<ARRAYSIZE;x++)
 	{
-		 *(arrfloat.m_Array[x]) = 1.1111*x;
+		fToSend[x] = 1.1111*x;
+		arrfloat.m_Array[x] = &fToSend[x];
 	}
 	printf("invoking echoFloatArray...\n");
 	if (ws.echoFloatArray(arrfloat).m_Array != NULL)
