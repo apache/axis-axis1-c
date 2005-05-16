8b7eda3a9c64057ba25c66ecfcb2befc73939401
Dushshantha Chandradasa <dushshantha@apache.org>
2005-05-16 05:18:43 +0000

    Added 3 tests to test 'all' WSDL construct

---
+++ tests/auto_build/testcases/client/cpp/BasicAllTestClient.cpp
@@ -0,0 +1,118 @@
+#include "AllComplexType.hpp"
+#include "AllTestSoap.hpp"
+#include <iostream>
+#include <axis/AxisException.hpp>
+#include <ctype.h>
+
+#define WSDL_DEFAULT_ENDPOINT "http://localhost:80/axis/BasicAll"
+
+void PrintUsage();
+
+static void
+usage (char *programName, char *defaultURL)
+{
+    cout << "\nUsage:\n"
+	<< programName << " [-? | service_url] " << endl
+	<< "    -?             Show this help.\n"
+	<< "    service_url    URL of the service.\n"
+	<< "    Default service URL is assumed to be " << defaultURL << endl;
+}
+
+int
+main (int argc, char *argv[])
+{
+    char endpoint[256];
+    sprintf (endpoint, "%s", WSDL_DEFAULT_ENDPOINT);
+
+    int returnValue = 1;	// Assume Failure
+
+       if (argc > 1)
+    {
+		// Watch for special case help request
+		if (!strncmp (argv[1], "-", 1))	// Check for - only so that it works for
+			//-?, -h or --help; -anything
+		{
+			usage (argv[0], endpoint);
+			return 2;
+		}
+		sprintf (endpoint, argv[1]);
+	}
+
+	bool bSuccess = false;
+    int iRetryIterationCount = 3;
+
+	do
+    {
+		try
+		{
+			AllTestSoap ws (endpoint, APTHTTP1_1);
+
+			AllComplexType* inParam = new AllComplexType();
+			
+			inParam->IntValue = 5;
+			inParam->StringValue = "HELLO";
+
+			printf("\nSending.................");
+			printf("\nIntValue = %d",inParam->IntValue);
+			printf("\nStringValue= %s",inParam->StringValue);
+			
+		
+			ws.setTransportProperty("SOAPAction" , "BasicAll#echoAll");
+			AllComplexType* outParam = ws.echoAll(inParam);
+
+			if (outParam != NULL)
+			{
+				printf("\n\nReceived................");
+				printf("\nIntValue = %d",outParam->IntValue);
+				printf("\nStringValue= %s",outParam->StringValue);
+				printf("\n\nSuccessfull\n");
+			}
+			else
+				printf("\nFault\n");
+
+			bSuccess = true;
+			delete inParam;
+			delete outParam;
+		}
+		catch (AxisException & e)
+		{
+			bool bSilent = false;
+
+			if (e.getExceptionCode () ==
+			CLIENT_TRANSPORT_OPEN_CONNECTION_FAILED)
+			{
+			if (iRetryIterationCount > 0)
+			{
+				bSilent = true;
+			}
+			}
+			else
+			{
+			iRetryIterationCount = 0;
+			}
+
+			if (!bSilent)
+			{
+			printf ("%s\n", e.what ());
+			}
+		}
+		catch (exception & e)
+		{
+			printf ("%s\n", e.what ());
+		}	
+		catch (...)
+		{
+			cerr << "Unknown Exception occured." << endl;
+		}
+
+		iRetryIterationCount--;
+
+	}while (iRetryIterationCount > 0 && !bSuccess);
+
+    cout <<
+	"---------------------- TEST COMPLETE -----------------------------"
+	<< endl;
+
+    return returnValue;
+
+}
---
+++ tests/auto_build/testcases/client/cpp/LimitedAllTestlClient.cpp
@@ -0,0 +1,117 @@
+#include "AllComplexType.hpp"
+#include "AllTestSoap.hpp"
+#include <iostream>
+#include <axis/AxisException.hpp>
+#include <ctype.h>
+
+#define WSDL_DEFAULT_ENDPOINT "http://localhost:80/axis/LimitedAll"
+
+void PrintUsage();
+
+static void
+usage (char *programName, char *defaultURL)
+{
+    cout << "\nUsage:\n"
+	<< programName << " [-? | service_url] " << endl
+	<< "    -?             Show this help.\n"
+	<< "    service_url    URL of the service.\n"
+	<< "    Default service URL is assumed to be " << defaultURL << endl;
+}
+
+int
+main (int argc, char *argv[])
+{
+    char endpoint[256];
+    sprintf (endpoint, "%s", WSDL_DEFAULT_ENDPOINT);
+
+    int returnValue = 1;	// Assume Failure
+
+       if (argc > 1)
+    {
+		// Watch for special case help request
+		if (!strncmp (argv[1], "-", 1))	// Check for - only so that it works for
+			//-?, -h or --help; -anything
+		{
+			usage (argv[0], endpoint);
+			return 2;
+		}
+		sprintf (endpoint, argv[1]);
+	}
+
+	bool bSuccess = false;
+    int iRetryIterationCount = 3;
+
+	do
+    {
+		try
+		{
+			AllTestSoap ws (endpoint, APTHTTP1_1);
+
+			AllComplexType* inParam = new AllComplexType();
+			
+			inParam->Value0 = 5;
+			inParam->Value2 = "HELLO";
+
+			printf("\nSending.................");
+			printf("\nValue0 = %d",inParam->Value0);
+			printf("\nValue2 = %s",inParam->Value2);
+			
+			ws.setTransportProperty("SOAPAction" , "LimitedAll#echoAll");
+			AllComplexType* outParam = ws.echoAll(inParam);
+
+			if (outParam != NULL)
+			{
+				printf("\n\nReceived................");
+				printf("\nValue0 = %d",outParam->Value0);
+				printf("\nValue2 = %s",outParam->Value2);
+				printf("\n\nSuccessfull\n");
+			}
+			else
+				printf("\nFault\n");
+
+			bSuccess = true;
+			delete inParam;
+			delete outParam;
+		}
+		catch (AxisException & e)
+		{
+			bool bSilent = false;
+
+			if (e.getExceptionCode () ==
+			CLIENT_TRANSPORT_OPEN_CONNECTION_FAILED)
+			{
+			if (iRetryIterationCount > 0)
+			{
+				bSilent = true;
+			}
+			}
+			else
+			{
+			iRetryIterationCount = 0;
+			}
+
+			if (!bSilent)
+			{
+			printf ("%s\n", e.what ());
+			}
+		}
+		catch (exception & e)
+		{
+			printf ("%s\n", e.what ());
+		}	
+		catch (...)
+		{
+			cerr << "Unknown Exception occured." << endl;
+		}
+
+		iRetryIterationCount--;
+
+	}while (iRetryIterationCount > 0 && !bSuccess);
+
+    cout <<
+	"---------------------- TEST COMPLETE -----------------------------"
+	<< endl;
+
+    return returnValue;
+
+}
