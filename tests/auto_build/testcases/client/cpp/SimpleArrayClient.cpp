6e750f94895988d2a697df00fbfc3e1583ffdd16
Dushshantha Chandradasa <dushshantha@apache.org>
2005-06-03 04:30:15 +0000

    Fixed an error in initializing array in  the client code

--- tests/auto_build/testcases/client/cpp/SimpleArrayClient.cpp
+++ tests/auto_build/testcases/client/cpp/SimpleArrayClient.cpp
@@ -1,18 +1,3 @@
-// Copyright 2003-2004 The Apache Software Foundation.
-// (c) Copyright IBM Corp. 2004, 2005 All Rights Reserved
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//        http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // ArrayClient.cpp : Defines the entry point for the console application.i
 //
 #include <string>
@@ -42,13 +27,15 @@ int main(int argc, char* argv[])
 	printf("Sending Requests to Server %s ........\n\n", url);
 	sprintf(endpoint, "%s", url);
 	ArrayTestPortType ws(endpoint);
+	
 	//testing echoIntArray
          intArrayType arrin;
          arrin.intItem.m_Array = new int*[ARRAYSIZE];
          arrin.intItem.m_Size = ARRAYSIZE;
          for (x=0;x<ARRAYSIZE;x++)
          {
-           arrin.intItem.m_Array[x] =&x;
+			 arrin.intItem.m_Array[x] = new int;
+			 *(arrin.intItem.m_Array[x]) = x;
          }
          printf("invoking echoIntArray...\n");
          if (ws.echoIntArray(&arrin)->intItem.m_Array != NULL)
