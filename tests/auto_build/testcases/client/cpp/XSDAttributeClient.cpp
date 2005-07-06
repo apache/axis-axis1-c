63413cc2c715e66e256af9084afa6aca07bd8122
Adrian Dick <dicka@apache.org>
2005-07-06 15:04:34 +0000

    Minor change to test case following recent changes to unsignedLong to use 64bit integers.
    PR: AXISCPP-653
    Submitted by: Adrian Dick

--- tests/auto_build/testcases/client/cpp/XSDAttributeClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSDAttributeClient.cpp
@@ -19,6 +19,25 @@
 #include <ctype.h>
 #include <iostream>
 
+#ifdef WIN32
+  // Bug in MS Visual C++ 6.0. Fixed in Visual C++ .Net version.
+  // Cannot print an __int64 number with cout without this overloading
+  std::ostream& operator<<(std::ostream& os, __int64 i )
+  {
+    char buf[20];
+    sprintf(buf,"%I64d", i );
+    os << buf;
+    return os;
+  }
+
+  std::ostream& operator<<(std::ostream& os, unsigned __int64 i )
+  {
+    char buf[20];
+    sprintf(buf,"%I64u", i );
+    os << buf;
+    return os;
+  }
+#endif
 
 int main(int argc, char* argv[])
 {
