a2af81259f3236178fd9d4b00333d2d7076306e9
John Hawkins <hawkeye@apache.org>
2005-08-15 12:39:34 +0000

    Adding in new unittest for XSD_anyURIfault

---
+++ tests/auto_build/testcases/client/cpp/XSD_anyURIfaultClient.cpp
@@ -0,0 +1,254 @@
+// Copyright 2003-2004 The Apache Software Foundation.
+// (c) Copyright IBM Corp. 2004, 2005 All Rights Reserved
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "XSD_anyURI.hpp"
+#include <axis/AxisException.hpp>
+#include <ctype.h>
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    char endpoint[256];
+    const char* url="http://localhost:80/axis/XSD_anyURI";
+
+    if(argc>1)
+        url = argv[1];
+
+    try
+    {
+        sprintf(endpoint, "%s", url);
+        XSD_anyURI* ws = new XSD_anyURI(endpoint);
+      
+        char emptyanyURI[1] = "";
+        xsd__anyURI emptyInput = new char[1];
+        strcpy (emptyInput, emptyanyURI);
+		char simpleanyURI[25] = "http://www.xyz.com";
+        xsd__anyURI input = new char[25];
+        strcpy (input, simpleanyURI);
+
+        // Test non-nillable element
+        xsd__anyURI result = ws->asNonNillableElement(input);
+        if (result)
+        {
+            if (*result)
+            {
+                cout << "non-nillable element=" << result << endl;
+            }
+            else
+            {
+                cout << "non-nillable element=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "non-nillable element=<nil>" << endl;
+        }
+        delete [] input;
+
+        // Test empty non-nillable element
+        result = ws->asNonNillableElement(emptyInput);
+        if (result)
+        {
+            if (*result)
+            {
+                cout << "empty non-nillable element=" << result << endl;
+            }
+            else
+            {
+                cout << "empty non-nillable element=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "empty non-nillable element=<nil>" << endl;
+        }
+        delete [] emptyInput;
+
+
+        // Test required attribute
+        input = new char[25];
+        strcpy (input, simpleanyURI);
+        RequiredAttributeElement requiredAttributeInput;
+        requiredAttributeInput.setrequiredAttribute(input);
+        RequiredAttributeElement* requiredAttributeResult = ws->asRequiredAttribute(&requiredAttributeInput);
+        if (requiredAttributeResult->getrequiredAttribute())
+        {
+            if (*(requiredAttributeResult->getrequiredAttribute()))
+            {
+                cout << "required attribute=" << requiredAttributeResult->getrequiredAttribute() << endl;
+            }
+            else
+            {
+                cout << "required attribute=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "required attribute=<nil>" << endl;
+        }
+        delete requiredAttributeResult;
+
+ 
+
+/* Optional Attributes currently unsupported by WSDL2Ws
+ * Exact coding of this section may change depending on chosen implementation
+        // Test optional attribute, with a value
+        input = new char[25];
+        strcpy (input, simpleanyURI);
+        OptionalAttributeElement optionalAttributeInput;
+        optionalAttributeInput.setoptionalAttribute(input);
+        OptionalAttributeElement* optionalAttributeResult = ws->asOptionalAttribute(&optionalAttributeInput);
+        if (optionalAttributeResult->getoptionalAttribute())
+        {
+            if (*(optionalAttributeResult->getoptionalAttribute()))
+            {
+                cout << "optional attribute, with data=" << optionalAttributeResult->getoptionalAttribute() << endl;
+            }
+            else
+            {
+                cout << "optional attribute, with data=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "optional attribute, with data=<not present>" << endl;
+        }
+        delete [] input;
+        delete optionalAttributeResult;
+
+        // Test empty optional attribute
+        emptyInput = new char[1];
+        strcpy (emptyInput, emptyanyURI);
+        optionalAttributeInput.setoptionalAttribute(emptyInput);
+        optionalAttributeResult = ws->asOptionalAttribute(&optionalAttributeInput);
+        if (optionalAttributeResult->getoptionalAttribute())
+        {
+            if (*(optionalAttributeResult->getoptionalAttribute()))
+            {
+                cout << "empty optional attribute=" << optionalAttributeResult->getoptionalAttribute() << endl;
+            }
+            else
+            {
+                cout << "empty optional attribute=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "empty optional attribute=<not present>" << endl;
+        }
+        delete [] emptyInput;
+        delete optionalAttributeResult;
+
+        // Test optional attribute, not present
+        // optionalAttributeInput.setattribute();
+        optionalAttributeResult = ws->asOptionalAttribute(&optionalAttributeInput);
+        if (optionalAttributeResult->getoptionalAttribute())
+        {
+            if (*(optionalAttributeResult->getoptionalAttribute()))
+            {
+                cout << "optional attribute, not present=" << optionalAttributeResult->getoptionalAttribute() << endl;
+            }
+            else
+            {
+                cout << "optional attribute, not present=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "optional attribute, not present=<not present>" << endl;
+        }
+        delete optionalAttributeResult;
+*/
+
+        // Test array
+        xsd__anyURI_Array arrayInput;
+        arrayInput.m_Array = new xsd__anyURI[2];
+        arrayInput.m_Size = 2;
+        for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
+        {
+            input = new char[25];
+            strcpy (input, simpleanyURI);
+            arrayInput.m_Array[inputIndex] = input;
+        }
+        xsd__anyURI_Array arrayResult = ws->asArray(arrayInput);
+        cout << "array of " << arrayResult.m_Size << " elements" << endl;
+        for (int index = 0; index < arrayResult.m_Size ; index++)
+        {
+            if (arrayResult.m_Array[index])
+            {
+                if (*(arrayResult.m_Array[index]))
+                {
+                    cout << "  element[" << index << "]=" << arrayResult.m_Array[index] << endl;
+                }
+                else
+                {
+                    cout << "  element[" << index << "]=<empty>" << endl;
+                }
+                delete arrayResult.m_Array[index];
+            }
+            else
+            {
+                cout << "  element[" << index << "]=<nil>" << endl;
+            }
+        }
+        delete [] arrayInput.m_Array;
+        delete [] arrayResult.m_Array;
+
+        // Test complex type
+        input = new char[25];
+        strcpy (input, simpleanyURI);
+        SimpleComplexType complexTypeInput;
+        complexTypeInput.setcomplexTypeElement(input);
+        SimpleComplexType* complexTypeResult = ws->asComplexType(&complexTypeInput);
+        if (complexTypeResult->getcomplexTypeElement())
+        {
+            if (*(complexTypeResult->getcomplexTypeElement()))
+            {
+                cout << "within complex type=" << complexTypeResult->getcomplexTypeElement() << endl;
+            }
+            else
+            {
+                cout << "within complex type=<empty>" << endl;
+            }
+        }
+        else
+        {
+            cout << "within complex type=<nil>" << endl;
+        }
+        delete complexTypeResult;
+
+        // Tests now complete
+
+        delete ws;
+    }
+    catch(AxisException& e)
+    {
+        cout << "Exception : " << e.what() << endl;
+    }
+    catch(exception& e)
+    {
+        cout << "Unknown exception has occured: " << e.what() << endl;
+    }
+    catch(...)
+    {
+        cout << "Unknown exception has occured" << endl;
+    }
+
+    cout<< "---------------------- TEST COMPLETE -----------------------------"<< endl;
+   
+    return 0;
+}
