6a9f784700acb13573fac6446df13558f1738664
James Jose <jamejose@apache.org>
2005-11-08 06:33:18 +0000

    Updated the testcase with new memory model for arrays

--- tests/auto_build/testcases/client/cpp/AttMultiSameAttachment.cpp
+++ tests/auto_build/testcases/client/cpp/AttMultiSameAttachment.cpp
@@ -65,10 +65,12 @@ int main(int argc, char* argv[])
 				att2->addBody(&b64b1);				
 				//Calling webservice with multiple attachments. Here same object is passed as argument
 				xsd__string_Array ret=ws.multiAttachment(att1,att1,att2,att1);
-				cout<<ret.m_Array[0]<<endl;
-				cout<<ret.m_Array[1]<<endl;
-				cout<<ret.m_Array[2]<<endl;
-				cout<<ret.m_Array[3]<<endl;			    			
+				int outputsize=0;
+				const xsd__string * array=ret.get(outputsize);
+				cout << array[0]<<endl;
+				cout << array[1]<<endl;
+				cout << array[2]<<endl;
+				cout << array[3]<<endl;		    			
 			    bSuccess = true;
 			}	
 	catch(AxisException& e)
