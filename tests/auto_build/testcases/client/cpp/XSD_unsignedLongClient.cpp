60f0b9f166960c4c3746785a3c527df907a7ab68
James Jose <jamejose@apache.org>
2005-11-08 14:26:26 +0000

    Updated the testcase with new memory model of Arrays

--- tests/auto_build/testcases/client/cpp/XSD_unsignedLongClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_unsignedLongClient.cpp
@@ -109,24 +109,30 @@ int main(int argc, char* argv[])
 
         // Test array
         xsd__unsignedLong_Array arrayInput;
-        arrayInput.m_Array = new xsd__unsignedLong*[2];
-        xsd__unsignedLong * array = new xsd__unsignedLong[2];
-        arrayInput.m_Size = 2;
-        for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
+		int arraySize =2;
+		xsd__unsignedLong ** array = new xsd__unsignedLong*[arraySize]();
+        
+        for (int inputIndex=0 ; inputIndex < arraySize ; inputIndex++)
         {
-            array[inputIndex] = 123456789;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] = new xsd__unsignedLong(123456789);
+           
         }
+		arrayInput.set(array,arraySize);
         xsd__unsignedLong_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		int outputSize =0;
+		const xsd__unsignedLong ** output = arrayResult.get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            cout << "  element[" << index << "]=" << *((xsd__unsignedLong*)(arrayResult.m_Array[index])) << endl;
-            delete arrayResult.m_Array[index];
+            cout << "  element[" << index << "]=" << *((xsd__unsignedLong*)(output[index])) << endl;
+           
+        }
+        // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
         }
         delete [] array;
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
 
         // Test complex type
         SimpleComplexType complexTypeInput;
