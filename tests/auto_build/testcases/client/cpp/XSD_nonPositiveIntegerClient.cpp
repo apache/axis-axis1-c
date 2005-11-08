6cd5c177c7fb29873831a4f3e00bc0b4d5d54420
James Jose <jamejose@apache.org>
2005-11-08 13:01:26 +0000

    Updated the testcase with new memory model of Arrays

--- tests/auto_build/testcases/client/cpp/XSD_nonPositiveIntegerClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_nonPositiveIntegerClient.cpp
@@ -115,24 +115,32 @@ int main(int argc, char* argv[])
 
         // Test array
         xsd__nonPositiveInteger_Array arrayInput;
-        arrayInput.m_Array = new xsd__nonPositiveInteger*[2];
-        xsd__nonPositiveInteger * array = new xsd__nonPositiveInteger[2];
-        arrayInput.m_Size = 2;
-        for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
+		int arraySize=2;
+		xsd__nonPositiveInteger ** array = new xsd__nonPositiveInteger*[arraySize]();
+        
+        for (int inputIndex=0 ; inputIndex < arraySize ; inputIndex++)
         {
-            array[inputIndex] = -123456789;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] = new xsd__nonPositiveInteger(-123456789);
+            
         }
+		arrayInput.set(array,arraySize);
         xsd__nonPositiveInteger_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		int outputSize=0;
+		const xsd__nonPositiveInteger ** output = arrayResult.get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            cout << "  element[" << index << "]=" << *((xsd__nonPositiveInteger*)(arrayResult.m_Array[index])) << endl;
-            delete arrayResult.m_Array[index];
+            cout << "  element[" << index << "]=" << *((xsd__nonPositiveInteger*)(output[index])) << endl;
+            
+        }
+        // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
         }
         delete [] array;
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
+
+
 
         // Test complex type
         SimpleComplexType complexTypeInput;
