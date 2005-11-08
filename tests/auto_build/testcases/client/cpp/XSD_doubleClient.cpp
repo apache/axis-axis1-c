e5bf8f8a71045e9c0e12e3182837619791a016b8
James Jose <jamejose@apache.org>
2005-11-08 10:28:06 +0000

    Updated the testcase with new memory model of Arrays

--- tests/auto_build/testcases/client/cpp/XSD_doubleClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_doubleClient.cpp
@@ -111,25 +111,28 @@ int main(int argc, char* argv[])
 
      // Test array
         xsd__double_Array arrayInput;
-        arrayInput.m_Array = new xsd__double*[2];
-        xsd__double * array = new xsd__double[2];
-        arrayInput.m_Size = 2;
+		int arraySize=2;
+		xsd__double **array =new xsd__double*[arraySize];        
         for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
         {
-            array[inputIndex] = (xsd__double)70.7175888888;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] =new xsd__double(70.7175888888);          
         }
+		arrayInput.set(array,arraySize);
         xsd__double_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
-     {
-         printf("  element[%i]=%.5f\n", index,  *((arrayResult.m_Array[index])));
+		int outputSize=0;
+		const xsd__double ** output=arrayResult.get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
+         {
+         printf("  element[%i]=%.5f\n", index,  *(output[index]));
             fflush(stdout);
-         delete arrayResult.m_Array[index];
+        }
+	  // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
         }
         delete [] array;
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
 
       // Test complex type
       SimpleComplexType complexTypeInput;
