b90261daefb985fa1d3a715bb3ada78f26363aae
James Jose <jamejose@apache.org>
2005-11-08 10:59:27 +0000

    Updated the testcase with new memory model of Arrays

--- tests/auto_build/testcases/client/cpp/XSD_intClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_intClient.cpp
@@ -112,24 +112,30 @@ int main(int argc, char* argv[])
 
         // Test array
         xsd__int_Array arrayInput;
-        arrayInput.m_Array = new xsd__int*[2];
-        xsd__int * array = new xsd__int[2];
-        arrayInput.m_Size = 2;
+		int arraySize=2;
+		xsd__int ** array = new xsd__int*[arraySize]();
+        
         for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
         {
-            array[inputIndex] = 123456789;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] = new xsd__int(123456789);
+           
         }
+		arrayInput.set(array,arraySize);
         xsd__int_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		int outputSize=0;
+		const xsd__int ** output = arrayResult.get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            cout << "  element[" << index << "]=" << *((xsd__int*)(arrayResult.m_Array[index])) << endl;
-            delete arrayResult.m_Array[index];
+            cout << "  element[" << index << "]=" << *((xsd__int*)(output[index])) << endl;
+           
+        }
+        // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
         }
         delete [] array;
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
 
         // Test complex type
         SimpleComplexType complexTypeInput;
