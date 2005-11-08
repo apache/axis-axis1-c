b3bdc59683c4b8c356bcc455885ee6ee135615f8
James Jose <jamejose@apache.org>
2005-11-08 13:15:12 +0000

    Updated the testcase with new memory model of Arrays

--- tests/auto_build/testcases/client/cpp/XSD_normalizedStringClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_normalizedStringClient.cpp
@@ -299,37 +299,42 @@ int main(int argc, char* argv[])
 
         // Test array
         xsd__normalizedString_Array arrayInput;
-        arrayInput.m_Array = new xsd__normalizedString[2];
-        arrayInput.m_Size = 2;
-        for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
+		int arraySize=2;
+		xsd__normalizedString * array = new xsd__normalizedString[arraySize]();        
+        for (int inputIndex=0 ; inputIndex < arraySize ; inputIndex++)
         {
             input = new char[25];
             strcpy (input, simplenormalizedString);
-            arrayInput.m_Array[inputIndex] = input;
+            array[inputIndex] = input;
         }
+		arrayInput.set(array,arraySize);
         xsd__normalizedString_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		int outputSize=0;
+		const xsd__normalizedString * output = arrayResult.get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            if (arrayResult.m_Array[index])
+            if (output != NULL)
             {
-                if (*(arrayResult.m_Array[index]))
+                if (output[index]!=NULL)
                 {
-                    cout << "  element[" << index << "]=" << arrayResult.m_Array[index] << endl;
+                    cout << "  element[" << index << "]=" << output[index] << endl;
                 }
                 else
                 {
                     cout << "  element[" << index << "]=<empty>" << endl;
                 }
-                delete arrayResult.m_Array[index];
+                
             }
             else
             {
                 cout << "  element[" << index << "]=<nil>" << endl;
             }
         }
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
+        // Clear up input array  
+        
+        delete [] array;
+
 
         // Test complex type
         input = new char[25];
