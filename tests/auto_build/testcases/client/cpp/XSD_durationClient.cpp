68be6d668d143c072fbacbf458178c77566e310a
James Jose <jamejose@apache.org>
2005-11-08 10:33:23 +0000

    Updated the testcase with new memory model for Arrays

--- tests/auto_build/testcases/client/cpp/XSD_durationClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_durationClient.cpp
@@ -104,24 +104,31 @@ int main(int argc, char* argv[])
 */
 		// Test array
         xsd__duration_Array arrayInput;
-        arrayInput.m_Array = new xsd__duration*[2];
-        xsd__duration * array = new xsd__duration[2];
-        arrayInput.m_Size = 2;
+		int arraySize=2;
+		xsd__duration ** array = new xsd__duration*[arraySize];        
         for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
         {
-            array[inputIndex] = 123456789;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] = new xsd__duration(123456789);
+            
         }
+		arrayInput.set(array,arraySize);
 		xsd__duration_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-		for (int index = 0; index < arrayResult.m_Size ; index++)
+		int outputSize=0;
+		const xsd__duration **output = arrayResult.get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+		for (int index = 0; index < outputSize; index++)
 		{
-			cout << "  element[" << index << "]=" << *((xsd__duration*)(arrayResult.m_Array[index])) << endl;
-			delete arrayResult.m_Array[index];
+			cout << "  element[" << index << "]=" << *((xsd__duration*)output[index]) << endl;
+			
 		}
+      
+		 // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
+        }
         delete [] array;
-        delete [] arrayInput.m_Array;
-		delete [] arrayResult.m_Array;
+
 
 		// Test complex type
 		SimpleComplexType complexTypeInput;
