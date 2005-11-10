91dcd30c357a43a68865729a9949a5e6ab18a5b1
James Jose <jamejose@apache.org>
2005-11-10 10:21:08 +0000

    Updated the testcase with new Array APIs

--- tests/auto_build/testcases/client/cpp/XSD_gYearMonthClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_gYearMonthClient.cpp
@@ -116,25 +116,32 @@ int main(int argc, char* argv[])
 */
         // Test array
         xsd__gYearMonth_Array arrayInput;
-        arrayInput.m_Array = new xsd__gYearMonth*[2];
-        xsd__gYearMonth * array = new xsd__gYearMonth[2];
-        arrayInput.m_Size = 2;
-        for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
+		int arraySize =2;
+		xsd__gYearMonth ** array = new xsd__gYearMonth*[arraySize];
+        
+        for (int inputIndex=0 ; inputIndex < arraySize ; inputIndex++)
         {
-            array[inputIndex] = time;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] =new xsd__gYearMonth(time);
+            
         }
-        xsd__gYearMonth_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		arrayInput.set(array,arraySize);
+        xsd__gYearMonth_Array* arrayResult = ws->asArray(&arrayInput);
+		int outputSize = 0;
+		const xsd__gYearMonth **output = arrayResult->get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            strftime(returnString, 50, "%b %Y", arrayResult.m_Array[index]);
+            strftime(returnString, 50, "%b %Y", output[index]);
             cout << "  element[" << index << "]=" << returnString << endl;
-            delete arrayResult.m_Array[index];
+           
+        }
+       // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
         }
         delete [] array;
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
+
 
         // Test complex type
         SimpleComplexType complexTypeInput;
