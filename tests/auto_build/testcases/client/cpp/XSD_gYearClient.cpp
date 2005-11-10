378565092d1dc107794e2fa4ffff9783c6eee4cc
James Jose <jamejose@apache.org>
2005-11-10 10:15:50 +0000

    Updated the testcase with new Array APIs

--- tests/auto_build/testcases/client/cpp/XSD_gYearClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_gYearClient.cpp
@@ -116,25 +116,32 @@ int main(int argc, char* argv[])
 */
         // Test array
         xsd__gYear_Array arrayInput;
-        arrayInput.m_Array = new xsd__gYear*[2];
-        xsd__gYear * array = new xsd__gYear[2];
-        arrayInput.m_Size = 2;
+		int arraySize = 2;
+		xsd__gYear ** array = new xsd__gYear*[arraySize];
+        
         for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
         {
-            array[inputIndex] = time;
-            arrayInput.m_Array[inputIndex] = &array[inputIndex];
+            array[inputIndex] = new xsd__gYear(time);
+            
         }
-        xsd__gYear_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		arrayInput.set(array,arraySize);
+        xsd__gYear_Array* arrayResult = ws->asArray(&arrayInput);
+		int outputSize = 0;
+		const xsd__gYear **output = arrayResult->get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            strftime(returnString, 50, "%Y", arrayResult.m_Array[index]);
+            strftime(returnString, 50, "%Y", output[index]);
             cout << "  element[" << index << "]=" << returnString << endl;
-            delete arrayResult.m_Array[index];
+            
+        }
+        // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
         }
         delete [] array;
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
+
 
         // Test complex type
         SimpleComplexType complexTypeInput;
