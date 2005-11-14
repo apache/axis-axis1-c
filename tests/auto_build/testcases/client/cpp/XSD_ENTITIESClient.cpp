5dd4df301b8b4be724f890ce2bb72bf481538d0b
James Jose <jamejose@apache.org>
2005-11-14 14:59:27 +0000

    Updated the testcase with new Array APIs

--- tests/auto_build/testcases/client/cpp/XSD_ENTITIESClient.cpp
+++ tests/auto_build/testcases/client/cpp/XSD_ENTITIESClient.cpp
@@ -299,37 +299,45 @@ int main(int argc, char* argv[])
 
         // Test array
         xsd__ENTITIES_Array arrayInput;
-        arrayInput.m_Array = new xsd__ENTITIES[2];
-        arrayInput.m_Size = 2;
-        for (int inputIndex=0 ; inputIndex < 2 ; inputIndex++)
+		int arraySize = 2;
+		xsd__ENTITIES * array = new xsd__ENTITIES[arraySize];
+        for (int inputIndex=0 ; inputIndex < arraySize ; inputIndex++)
         {
-            input = new char[25];
-            strcpy (input, simpleENTITIES);
-            arrayInput.m_Array[inputIndex] = input;
+            array[inputIndex] = new char[25];
+            strcpy (array[inputIndex], simpleENTITIES);
+            
         }
-        xsd__ENTITIES_Array arrayResult = ws->asArray(arrayInput);
-        cout << "array of " << arrayResult.m_Size << " elements" << endl;
-        for (int index = 0; index < arrayResult.m_Size ; index++)
+		arrayInput.set(array,arraySize);
+        xsd__ENTITIES_Array* arrayResult = ws->asArray(&arrayInput);
+		int outputSize = 0;
+		const xsd__ENTITIES * output = arrayResult->get(outputSize);
+        cout << "array of " << outputSize << " elements" << endl;
+        for (int index = 0; index < outputSize ; index++)
         {
-            if (arrayResult.m_Array[index])
+            if (output[index])
             {
-                if (*(arrayResult.m_Array[index]))
+                if (*(output[index]))
                 {
-                    cout << "  element[" << index << "]=" << arrayResult.m_Array[index] << endl;
+                    cout << "  element[" << index << "]=" << output[index] << endl;
                 }
                 else
                 {
                     cout << "  element[" << index << "]=<empty>" << endl;
                 }
-                delete arrayResult.m_Array[index];
+                
             }
             else
             {
                 cout << "  element[" << index << "]=<nil>" << endl;
             }
         }
-        delete [] arrayInput.m_Array;
-        delete [] arrayResult.m_Array;
+        
+		 // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < arraySize ; deleteIndex++ )
+        {
+            delete array[deleteIndex];
+        }
+        delete [] array;
 
         // Test complex type
         input = new char[25];
