c9cdb0c1c7005f51881f2d8bcb949440bc31a0a7
James Jose <jamejose@apache.org>
2005-11-15 07:08:29 +0000

    Updated the testcase with new Array APIs

--- tests/auto_build/testcases/client/cpp/UnboundedChoiceClient.cpp
+++ tests/auto_build/testcases/client/cpp/UnboundedChoiceClient.cpp
@@ -16,26 +16,31 @@ int main(int argc, char *argv[])
 		
 		Value0=new	ChoiceComplexType();
 		Value0->StringValue=NULL;
-		
+
 		cout<< "Calling the Service..."<<endl;
-		
-		Value0->IntValue->m_Array=new xsd__int*[5];
-		xsd__int * intArray = new xsd__int[5];
-		Value0->IntValue->m_Size = 5;
+		xsd__int_Array * arrayIn = new xsd__int_Array();
+		xsd__int ** array = new xsd__int*[5];		
 		for (int x=0; x<5; x++)
 		{
-            intArray[x] = x+1;
-			Value0->IntValue->m_Array[x] = &intArray[x];
+            array[x] = new xsd__int(x+1);			
 		}
-
-
+		arrayIn->set(array,5);
+		Value0->setIntValue(&arrayIn);
 		url = argv[1];
 		ChoiceTestSoap ws(url);
 
 		Value1=ws.echoChoice(Value0);
+		int outputSize=0;
 		
 		for(int i=0;i<5;i++)
-			cout<<"Value1->IntValue->m_Array["<<i<<"] = "<<*(Value1->IntValue->m_Array[i])<<endl;
+			cout<<"Value1->IntValue->m_Array["<<i<<"] = "<<*((*(Value1->IntValue))->get(outputSize)[i])<<endl;
+		  // Clear up input array        
+        for (int deleteIndex = 0 ; deleteIndex < 5 ; deleteIndex++ )
+        {
+            delete [] array[deleteIndex];
+        }
+
+		delete arrayIn;
 	
 	}catch (AxisException & e)
     {
