9a024f630e8e56c0dea3b1c346870133bbf56481
Dushshantha Chandradasa <dushshantha@apache.org>
2005-12-08 05:50:33 +0000

    Changed the service implementation.

--- tests/auto_build/testcases/server/cpp/InteropTestPortType.cpp
+++ tests/auto_build/testcases/server/cpp/InteropTestPortType.cpp
@@ -38,25 +38,25 @@ void InteropTestPortType::onFault()
 xsd__string InteropTestPortType::echoString(xsd__string Value0)  
 {	return Value0;
 }
-xsd__string_Array InteropTestPortType::echoStringArray(xsd__string_Array Value0)  
+xsd__string_Array * InteropTestPortType::echoStringArray(xsd__string_Array * Value0)  
 {	return Value0;
 }
 int InteropTestPortType::echoInteger(int Value0)  
 {	return Value0;
 }
-xsd__int_Array InteropTestPortType::echoIntegerArray(xsd__int_Array Value0)  
+xsd__int_Array * InteropTestPortType::echoIntegerArray(xsd__int_Array * Value0)  
 {	return Value0;
 }
 float InteropTestPortType::echoFloat(float Value0)  
 {	return Value0;
 }
-xsd__float_Array InteropTestPortType::echoFloatArray(xsd__float_Array Value0)  
+xsd__float_Array * InteropTestPortType::echoFloatArray(xsd__float_Array * Value0)  
 {	return Value0;
 }
 SOAPStruct* InteropTestPortType::echoStruct(SOAPStruct* Value0)  
 {	return Value0;
 }
-SOAPStruct_Array InteropTestPortType::echoStructArray(SOAPStruct_Array Value0)  
+SOAPStruct_Array * InteropTestPortType::echoStructArray(SOAPStruct_Array * Value0)  
 {	return Value0;
 }
 void InteropTestPortType::echoVoid()  
