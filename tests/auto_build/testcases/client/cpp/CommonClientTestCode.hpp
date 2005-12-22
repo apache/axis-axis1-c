532efe344791f3372cdebeeedb8f99d292871c4a
John Hawkins <hawkeye@apache.org>
2005-12-22 09:43:16 +0000

    Added a new test for AXISCPP-753

--- tests/auto_build/testcases/client/cpp/CommonClientTestCode.hpp
+++ tests/auto_build/testcases/client/cpp/CommonClientTestCode.hpp
@@ -1,6 +1,10 @@
 #ifndef __COMMONHEADERFILE
 #define __COMMONHEADERFILE
 
+// Prototype
+bool parse_args(int *argc, char *argv[], char **endpoint);
+void shift_args_up(int i, int *argc, char *argv[]);
+
 #ifdef WIN32
 	#if defined(_MSC_VER) && (_MSC_VER < 1300)
 	// Bug in MS Visual C++ 6.0. Fixed in Visual C++ .Net version.
@@ -158,5 +162,72 @@ char * asciiToStringOfLength( char * pString, int iLength)
 }
 
 #endif
+/* Spin through args list and check for -e -p and -s options.
+   Option values are expected to follow the option letter as the next
+   argument.
+ 
+   These options and values are removed from the arg list.
+   If both -e and -s and or -p, then -e takes priority
+*/
+bool parse_args(int *argc, char *argv[], char **endpoint) {
+
+    // We need at least 2 extra arg after program name
+    if(*argc < 3)
+        return false;
+
+    char *server = "localhost";
+    int  port = 80;
+    bool ep_set = false;
+    bool server_set = false;
+    bool port_set = false;
+
+    for(int i=1; i<*argc; i++) {
+        if(*argv[i] == '-') {
+            switch(*(argv[i]+1)) {
+            case 'e':
+                *endpoint = strdup(argv[i+1]);
+                ep_set = true;
+                shift_args_up(i, argc, argv);
+                i--;
+                break;
+            case 's':
+                server = strdup(argv[i+1]);
+                server_set = true;
+                shift_args_up(i, argc, argv);
+                i--;
+                break;
+            case 'p':
+                port = atoi(argv[i+1]);
+                if(port >80) port_set = true;
+                shift_args_up(i, argc, argv);
+                i--;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    // use the supplied server and/or port to build the endpoint
+    if(ep_set == false && (server_set || port_set)) {
+        // Set p to the location of the first '/' after the http:// (7 chars)
+        // e.g. from http://localhost:80/axis/base gets /axis/base
+        char *ep_context = strpbrk(&(*endpoint)[7], "/");
+
+        // http://:/ is 9 characters + terminating NULL character so add 10.
+        // Allow space for port number upto 999999 6 chars
+        *endpoint = (char *)calloc(1, 10 + strlen(ep_context) + strlen(server) + 6);
+        sprintf(*endpoint, "http://%s:%d/%s", server, port, ep_context+1);
+        if(server_set) free(server);
+        ep_set = true;
+    }
+
+    return ep_set;
+}
+void shift_args_up(int i, int *argc, char *argv[]) {
+    for(int j=i, k=i+2; j<*(argc)-2; j++, k++)
+        argv[j]=argv[k];
+    *argc-=2;
+}
 
 #endif
---
+++ tests/auto_build/testcases/client/cpp/HiddenMessageParts.cpp
@@ -0,0 +1,68 @@
+#include "PruebaDatosCabecera.hpp"
+#include "PruebasBugPortType.hpp"
+#include "CommonClientTestCode.hpp"
+#include <iostream>
+#include <axis/AxisException.hpp>
+#include <ctype.h>
+
+#define WSDL_DEFAULT_ENDPOINT "http://localhost:80/axis/BasicAll"
+
+void PrintUsage();
+
+int
+main (int argc, char *argv[])
+{
+    char *endpoint = WSDL_DEFAULT_ENDPOINT;
+    bool endpoint_set = parse_args(&argc, argv, &endpoint); 
+    
+    int returnValue = 1;	// Assume Failure
+
+	bool bSuccess = false;
+    PruebasBugPortType* ws;
+		try
+		{
+            if(endpoint_set) 
+            {
+              ws = new PruebasBugPortType(endpoint, APTHTTP1_1);
+              free(endpoint);
+              endpoint_set = false;
+            } 
+            else
+            {
+              ws = new PruebasBugPortType();
+            }
+          
+            PruebaDatosCabecera* data = new PruebaDatosCabecera();
+            data->setIdenOrgNac("23");
+            data->setNumSec("12");
+            
+            printf("Sending.................\n");
+            xsd__int returnVal = ws->AbySend(data);
+            cout << "result = "<<returnVal<<endl;
+			
+			bSuccess = true;
+			delete data;
+		}
+		catch (AxisException & e)
+		{
+			bool bSilent = false;
+
+			printf ("%s\n", e.what ());
+		}
+		catch (exception & e)
+		{
+			printf ("%s\n", e.what ());
+		}	
+		catch (...)
+		{
+			cerr << "Unknown Exception occured." << endl;
+		}
+
+
+    cout <<
+	"---------------------- TEST COMPLETE -----------------------------"
+	<< endl;
+
+    return returnValue;
+
+}
