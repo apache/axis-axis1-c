9c82f8011b5faeec8d0a73d4ee2214ffb0bdc464
John Hawkins <hawkeye@apache.org>
2006-01-11 10:24:04 +0000

    Altered Cookie test to test new functionality

--- tests/auto_build/testcases/client/cpp/CookiesClient.cpp
+++ tests/auto_build/testcases/client/cpp/CookiesClient.cpp
@@ -15,6 +15,7 @@
 
 #include "Calculator.hpp"
 #include <axis/AxisException.hpp>
+#include <axis/GDefine.hpp>
 #include <ctype.h>
 #include <iostream>
 #include <signal.h>
@@ -22,6 +23,7 @@
 void sig_handler(int);
 void PrintUsage();
 bool IsNumber(const char* p);
+void callCalculatorAndPrintProperties(Calculator& calculator, int, int);
 
 int main(int argc, char* argv[])
 {
@@ -40,11 +42,6 @@ int main(int argc, char* argv[])
 
 	url = argv[1];
 
-		bool bSuccess = false;
-		int	iRetryIterationCount = 3;
-
-		do
-		{
 	try
 	{
 		sprintf(endpoint, "%s", url);
@@ -55,41 +52,68 @@ int main(int argc, char* argv[])
 		i2 = 3;
 
         ws.setMaintainSession(true);
-        iResult = ws.add(i1, i2);
-         cout << iResult << endl;
-        iResult = ws.add(i1, i2);
-        cout << iResult << endl;
-        bSuccess = true;
-          
+        cout << "Calling calculator for the first time and expecting Set-Cookie back"<<endl;
+        callCalculatorAndPrintProperties(ws, i1, i2);
+        
+        // get the cookies
+        cout <<"Storing the cookies so I can give them to the next web service"<<endl;
+        int currentCookieKey=0;
+        string cookieKeys[3];
+        const char* key = ws.getFirstTransportPropertyKey();
+        string keyString(key);
+        if(key)
+        {
+            if(keyString.compare("Set-Cookie")==0)
+            {
+             string valueString(ws.getCurrentTransportPropertyValue());
+             cookieKeys[currentCookieKey++] = valueString;
+            }
+        }
+        // then the rest
+        while(key = ws.getNextTransportPropertyKey())
+        {
+            string nextKeyString(key);
+            if(nextKeyString.compare("Set-Cookie")==0)
+            {
+             string valueString(ws.getCurrentTransportPropertyValue());
+            cookieKeys[currentCookieKey++] = valueString;
+            }
+        }
+        
+        cout << "Calling calculator for the second time and expecting to send the cookies back"<<endl;
+        callCalculatorAndPrintProperties(ws, i1, i2);
+        
         // Now create a new instance of the service and the cookies should be lost
+        cout <<"Use a new instance of calculator and ensure that the old cookies are lost"<<endl;
         Calculator calculator(endpoint);
         calculator.setMaintainSession(true);
-        iResult = calculator.add(i1, i2);
-        cout << iResult << endl;
-        
-        
-          
+        callCalculatorAndPrintProperties(calculator, i1, i2);
+        // OK, Now add the previously saved cookie on to this new service.
+        cout << "Add the cookies from the previous web service to the new web service"<<endl;
+        currentCookieKey=0;
+        while(currentCookieKey< 3)
+        {
+            calculator.setTransportProperty("Cookie", cookieKeys[currentCookieKey++].c_str());
+        }
+        cout <<"Completed adding cookies"<<endl;
+        // Call the service again twice to ensure that the key has been set as a cookie
+        cout <<"Call the calculator service twice and ensure that the cookies remain"<<endl;
+        callCalculatorAndPrintProperties(calculator, i1, i2);
+        callCalculatorAndPrintProperties(calculator, i1, i2);
+        // Delete one of the cookies
+        cout <<"Deleting the LtpaToken2 cookie"<<endl;
+        calculator.deleteTransportProperty("LtpaToken2");
+        cout << "Calling the service and not expecting the LtpaToken2 in the cookie header"<<endl;
+        callCalculatorAndPrintProperties(calculator, i1, i2);
+        // OK, now remove all the cookie remaining 
+        cout <<"Removing all the cookies by using the 'deleteTransportProperty(Cookie)' method"<<endl;
+        calculator.deleteTransportProperty("Cookie");
+        cout <<"Calling the service and expecting no cookies to be there"<<endl;
+        callCalculatorAndPrintProperties(calculator, i1, i2);
 	}
 	catch(AxisException& e)
 	{
-			bool bSilent = false;
-
-			if( e.getExceptionCode() == CLIENT_TRANSPORT_OPEN_CONNECTION_FAILED)
-			{
-				if( iRetryIterationCount > 0)
-				{
-					bSilent = true;
-				}
-			}
-			else
-			{
-				iRetryIterationCount = 0;
-			}
-
-            if( !bSilent)
-			{
-				cout << "Exception : " << e.what() << endl;
-			}
+			cout << "Exception : " << e.what() << endl;
 	}
 	catch(exception& e)
 	{
@@ -99,11 +123,48 @@ int main(int argc, char* argv[])
 	{
 	    cout << "Unknown exception has occured" << endl;
 	}
-		iRetryIterationCount--;
-		} while( iRetryIterationCount > 0 && !bSuccess);
   cout<< "---------------------- TEST COMPLETE -----------------------------"<< endl;
 	
 	return 0;
+}
+
+void callCalculatorAndPrintProperties(Calculator& calculator, int i1, int i2)
+{
+ 
+        int iResult = calculator.add(i1, i2);
+        // get the HTTP properties that were sent out
+        cout <<"----------------------------------------------------------"<<endl;
+        cout << "Properties that were sent out"<<endl;
+        const char* key = calculator.getFirstTransportPropertyKey(false);
+        if(key)
+        {
+            cout << key;
+            cout << "="<<calculator.getCurrentTransportPropertyValue(false)<<endl;
+        }
+        // then the rest
+        while(key = calculator.getNextTransportPropertyKey(false))
+        {
+            cout << key;
+            cout << "="<<calculator.getCurrentTransportPropertyValue(false)<<endl;
+        }
+        cout <<"----------------------------------------------------------"<<endl;
+        cout << "Properties that were received"<<endl;
+        key = calculator.getFirstTransportPropertyKey();
+        if(key)
+        {
+            cout << key;
+            cout << "="<<calculator.getCurrentTransportPropertyValue()<<endl;
+        }
+        // then the rest
+        while(key = calculator.getNextTransportPropertyKey())
+        {
+            cout << key;
+            cout << "="<<calculator.getCurrentTransportPropertyValue()<<endl;
+        }
+        cout <<"----------------------------------------------------------"<<endl;
+        
+        
+ 
 }
 
 void PrintUsage()
