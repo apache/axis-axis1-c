01936a98fdc319f5b392abb811dec7538aa54dde
Fred Preston <prestonf@apache.org>
2006-01-18 15:49:07 +0000

    Tidy files to make them easier to read and remove warnings.

--- tests/auto_build/testcases/client/cpp/FaultMappingClient.cpp
+++ tests/auto_build/testcases/client/cpp/FaultMappingClient.cpp
@@ -22,139 +22,176 @@
 void sig_handler(int);
 using namespace std;
 
-int main(int argc, char* argv[])
+typedef struct
 {
-	char endpoint[256];
-	const char* server="localhost";
-	const char* url="http://localhost:80/axis/MathOps";
-	const char* port="80";
-	const char* op = 0;
-	const char* p1 = 0;
-	const char* p2 = 0;
-	int i1=0, i2=0;
-	int iResult;
-	char* pcDetail;
+	int	iValue1;
+	int	iValue2;
+} MathOperationData;
 
-	signal(SIGILL, sig_handler);
-	signal(SIGABRT, sig_handler);
-	signal(SIGSEGV, sig_handler);
-	//signal(SIGQUIT, sig_handler);
-	//signal(SIGBUS, sig_handler);
-	signal(SIGFPE, sig_handler);
+int main( int argc, char * argv[])
+{
+	char *				pszURL = "http://localhost:80/axis/MathOps";
+	const char *		pszOperation = NULL;
+	MathOperationData	sMathOperationData[] = {{  10,  5},
+												{  10,  0},
+												{1000,  5},
+												{  10, -5},
+												{   0,  0}};
 
-	url = argv[1];
+	signal( SIGILL,  sig_handler);
+	signal( SIGABRT, sig_handler);
+	signal( SIGSEGV, sig_handler);
+//  signal( SIGQUIT, sig_handler);
+//  signal( SIGBUS,  sig_handler);
+	signal( SIGFPE,  sig_handler);
 
-	sprintf(endpoint, "%s", url);
+	pszURL = argv[1];
 
-	op = "div";
+	pszOperation = "div";
 
-	if (strcmp(op, "div") == 0)
+	if( strcmp( pszOperation, "div") == 0)
 	{
-	    for(int i = 0; i < 4; i++)
+	    int	iMathDataCount = 0;
+
+		while( sMathOperationData[iMathDataCount].iValue1 != 0 &&
+			   sMathOperationData[iMathDataCount].iValue1 != sMathOperationData[iMathDataCount].iValue2)
 	    {
-            cout << i << endl;
+			bool bSuccess = false;
+			int	iRetryIterationCount = 3;
 
-			switch(i)
-			{
-				case 0: i1 = 10; i2 = 5; break;
-				case 1: i1 = 10; i2 = 0; break;
-				case 2: i1 = 1000; i2 = 5; break;
-				case 3: i1 = 10; i2 = -5; break;
-			}
-		bool bSuccess = false;
-		int	iRetryIterationCount = 3;
+            cout << iMathDataCount << endl;
 
-		do
-		{
-			try
+			do
 			{
-				MathOps ws(endpoint);
-				if( iRetryIterationCount == 3)
-					cout << "Trying to " << op << " " << i1 << " by " << i2 << endl;
-				iResult = ws.div(i1, i2);		
-				cout << "Result is " << iResult << endl;
-				bSuccess = true;
-			}
-			catch(DivByZeroStruct& dbzs)
-			{
-				cout << "DivByZeroStruct Fault: \"" 
-					<< dbzs.varString 
-					<< "\", " 
-					<< dbzs.varInt 
-					<< ", " 
-					<< dbzs.varFloat 
-					<< endl; 
-				bSuccess = true;
-			}
-			catch(SpecialDetailStruct& sds)
-			{
-				cout << "SpecialDetailStruct Fault: \"" 
-					<< sds.varString 
-					<< "\"" 
-					<< endl;
-				bSuccess = true;
-			}
-			catch(OutOfBoundStruct& oobs)
-			{
-				cout << "OutOfBoundStruct Fault: \"" 
-					<< oobs.varString 
-					<< "\", " 
-					<< oobs.varInt 
-					<< ", \"" 
-					<< oobs.specialDetail->varString 
-					<< "\"" 
-					<< endl;
-				bSuccess = true;
-			}
-			catch(SoapFaultException& sfe)
-			{
-				cout << "SoapFaultException: " << sfe.what() << endl;
-			}
-			catch(AxisException& e)
-			{
-			bool bSilent = false;
-
-			if( e.getExceptionCode() == CLIENT_TRANSPORT_OPEN_CONNECTION_FAILED)
-			{
-				if( iRetryIterationCount > 0)
+				try
 				{
-					bSilent = true;
-				}
-			}
-			else
-			{
-				iRetryIterationCount = 0;
-			}
+					MathOps	ws( pszURL);
 
-            if( !bSilent)
-			{
-				cout << "Exception : " << e.what() << endl;
-			}
-			}
-			catch(exception& e)
-			{
-                cout << "Unknown Exception: " << endl;
-			}
-			catch(...)
-			{
-                cout << "Unspecified Exception: " << endl;
-			}
-		iRetryIterationCount--;
-		} while( iRetryIterationCount > 0 && !bSuccess);
+					if( iRetryIterationCount == 3)
+					{
+						cout << "Trying to "
+							 << pszOperation
+							 << " "
+							 << sMathOperationData[iMathDataCount].iValue1
+							 << " by "
+							 << sMathOperationData[iMathDataCount].iValue2
+							 << endl;
+					}
+
+// HTTP/1.1 500 Internal Server Error
+// :
+// <soapenv:Header/>
+// <faultcode xmlns:ns2144481696="http://soapinterop.org/types"
+//                         xmlns="">ns2144481696:DivByZeroStruct</faultcode>
+// <faultstring xmlns="">org.soapinterop.DivByZeroStruct</faultstring>
+// <detail xmlns="">
+// <faultstruct1 href="#id0"/>
+// </detail>
+// </soapenv:Fault>
+// <multiRef id="id0"
+//           soapenc:root="0"
+//           soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
+//           xsi:type="ns2144481696:DivByZeroStruct"
+//           xmlns:ns2144481696="http://soapinterop.org/types"
+//           xmlns="">
+// <varString xsi:type="xsd:string">Division by zero exception</varString>
+
+					int	iResult = ws.div( sMathOperationData[iMathDataCount].iValue1,
+										  sMathOperationData[iMathDataCount].iValue2);
+				
+					cout << "Result is " << iResult << endl;
+				
+					bSuccess = true;
+				}
+				catch( DivByZeroStruct& dbzs)
+				{
+					cout << "DivByZeroStruct Fault: \""
+						 << dbzs.varString
+						 << "\", "
+						 << dbzs.varInt
+						 << ", "
+						 << dbzs.varFloat
+						 << endl;
+
+					bSuccess = true;
+				}
+				catch( SpecialDetailStruct& sds)
+				{
+					cout << "SpecialDetailStruct Fault: \""
+						 << sds.varString
+						 << "\""
+						 << endl;
+
+					bSuccess = true;
+				}
+				catch( OutOfBoundStruct& oobs)
+				{
+					cout << "OutOfBoundStruct Fault: \""
+						 << oobs.varString
+						 << "\", "
+						 << oobs.varInt
+						 << ", \""
+						 << oobs.specialDetail->varString
+						 << "\""
+						 << endl;
+
+					bSuccess = true;
+				}
+				catch( SoapFaultException& sfe)
+				{
+					cout << "SoapFaultException: " << sfe.what() << endl;
+				}
+				catch( AxisException& e)
+				{
+					bool bSilent = false;
+
+					if( e.getExceptionCode() == CLIENT_TRANSPORT_OPEN_CONNECTION_FAILED)
+					{
+						if( iRetryIterationCount > 1)
+						{
+							bSilent = true;
+						}
+					}
+					else
+					{
+						iRetryIterationCount = 0;
+					}
+
+		            if( !bSilent)
+					{
+						cout << "Exception : " << e.what() << endl;
+					}
+				}
+				catch( exception& e)
+				{
+		            cout << "Unknown Exception: " << e.what() << endl;
+				}
+				catch(...)
+				{
+		            cout << "Unspecified Exception: " << endl;
+				}
+
+				iRetryIterationCount--;
+			} while( iRetryIterationCount > 0 && !bSuccess);
+
+			iMathDataCount++;
 	    }
 	}
 	else 
 	{
-		cout << "Invalid operation " << op << endl;
+		cout << "Invalid operation " << pszOperation << endl;
 	}
-	cout << "---------------------- TEST COMPLETE -----------------------------"<< endl;
+
+	cout << "---------------------- TEST COMPLETE -----------------------------" << endl;
 	
 	return 0;
 }
 
-void sig_handler(int sig) {
-	signal(sig, sig_handler);
+void sig_handler( int sig)
+{
+	signal( sig, sig_handler);
+
     cout << "SIGNAL RECEIVED " << sig << endl;
+
 	exit(1);
 }
-
