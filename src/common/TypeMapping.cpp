0ed1996e2bd4598c7f91eeb2dc08f738b8ab69a0
Adrian Dick <dicka@apache.org>
2006-02-23 17:24:30 +0000

    AXISCPP-939
    Add missing XSD built-in simple types to Map in TypeMapping.

--- src/common/TypeMapping.cpp
+++ src/common/TypeMapping.cpp
@@ -50,33 +50,51 @@ void TypeMapping::initialize ()
 {
     if (!m_bInit)
     {
-        m_sTypeMap[std::string ("int")] = XSD_INT;
-        m_sTypeMap[std::string ("boolean")] = XSD_BOOLEAN;
-        m_sTypeMap[std::string ("unsignedInt")] = XSD_UNSIGNEDINT;
-        m_sTypeMap[std::string ("short")] = XSD_SHORT;
-        m_sTypeMap[std::string ("unsignedShort")] = XSD_UNSIGNEDSHORT;
-        m_sTypeMap[std::string ("byte")] = XSD_BYTE;
-        m_sTypeMap[std::string ("unsignedByte")] = XSD_UNSIGNEDBYTE;
-        m_sTypeMap[std::string ("long")] = XSD_LONG;
-        m_sTypeMap[std::string ("integer")] = XSD_INTEGER;
-        m_sTypeMap[std::string ("unsignedLong")] = XSD_UNSIGNEDLONG;
-        m_sTypeMap[std::string ("float")] = XSD_FLOAT;
-        m_sTypeMap[std::string ("double")] = XSD_DOUBLE;
-        m_sTypeMap[std::string ("decimal")] = XSD_DECIMAL;
-        m_sTypeMap[std::string ("string")] = XSD_STRING;
-        m_sTypeMap[std::string ("base64Binary")] = XSD_BASE64BINARY;
-        m_sTypeMap[std::string ("hexBinary")] = XSD_HEXBINARY;
-        m_sTypeMap[std::string ("anyURI")] = XSD_ANYURI;
-        m_sTypeMap[std::string ("QName")] = XSD_QNAME;
         m_sTypeMap[std::string ("duration")] = XSD_DURATION;
         m_sTypeMap[std::string ("dateTime")] = XSD_DATETIME;
+        m_sTypeMap[std::string ("time")] = XSD_TIME;         
         m_sTypeMap[std::string ("date")] = XSD_DATE;
-        m_sTypeMap[std::string ("time")] = XSD_TIME;
         m_sTypeMap[std::string ("gYearMonth")] = XSD_GYEARMONTH;
         m_sTypeMap[std::string ("gYear")] = XSD_GYEAR;
         m_sTypeMap[std::string ("gMonthDay")] = XSD_GMONTHDAY;
         m_sTypeMap[std::string ("gDay")] = XSD_GDAY;
         m_sTypeMap[std::string ("gMonth")] = XSD_GMONTH;
+        m_sTypeMap[std::string ("string")] = XSD_STRING;
+        m_sTypeMap[std::string ("normalizedString")] = XSD_NORMALIZEDSTRING;
+        m_sTypeMap[std::string ("token")] = XSD_TOKEN;
+        m_sTypeMap[std::string ("language")] = XSD_LANGUAGE;
+        m_sTypeMap[std::string ("Name")] = XSD_NAME;
+        m_sTypeMap[std::string ("NCName")] = XSD_NCNAME;
+        m_sTypeMap[std::string ("ID")] = XSD_ID;
+        m_sTypeMap[std::string ("IDREF")] = XSD_IDREF;
+        m_sTypeMap[std::string ("IDREFS")] = XSD_IDREFS;
+        m_sTypeMap[std::string ("ENTITY")] = XSD_ENTITY;
+        m_sTypeMap[std::string ("ENTITIES")] = XSD_ENTITIES;
+        m_sTypeMap[std::string ("NMTOKEN")] = XSD_NMTOKEN;
+        m_sTypeMap[std::string ("NMTOKENS")] = XSD_NMTOKENS;
+        m_sTypeMap[std::string ("boolean")] = XSD_BOOLEAN;
+        m_sTypeMap[std::string ("base64Binary")] = XSD_BASE64BINARY;
+        m_sTypeMap[std::string ("hexBinary")] = XSD_HEXBINARY;
+        m_sTypeMap[std::string ("float")] = XSD_FLOAT;
+        m_sTypeMap[std::string ("decimal")] = XSD_DECIMAL;
+        m_sTypeMap[std::string ("nonPositiveInteger")] = XSD_NONPOSITIVEINTEGER;
+        m_sTypeMap[std::string ("negativeInteger")] = XSD_NEGATIVEINTEGER;
+        m_sTypeMap[std::string ("integer")] = XSD_INTEGER;
+        m_sTypeMap[std::string ("long")] = XSD_LONG;
+        m_sTypeMap[std::string ("int")] = XSD_INT;
+        m_sTypeMap[std::string ("short")] = XSD_SHORT;
+        m_sTypeMap[std::string ("byte")] = XSD_BYTE;
+        m_sTypeMap[std::string ("nonNegativeInteger")] = XSD_NONNEGATIVEINTEGER;
+        m_sTypeMap[std::string ("unsignedLong")] = XSD_UNSIGNEDLONG;
+        m_sTypeMap[std::string ("unsignedInt")] = XSD_UNSIGNEDINT;
+        m_sTypeMap[std::string ("unsignedShort")] = XSD_UNSIGNEDSHORT;
+        m_sTypeMap[std::string ("unsignedByte")] = XSD_UNSIGNEDBYTE;
+        m_sTypeMap[std::string ("positiveInteger")] = XSD_POSITIVEINTEGER;
+        m_sTypeMap[std::string ("double")] = XSD_DOUBLE;
+        m_sTypeMap[std::string ("anyURI")] = XSD_ANYURI;
+        m_sTypeMap[std::string ("QName")] = XSD_QNAME;
+        m_sTypeMap[std::string ("NOTATION")] = XSD_NOTATION;
+
         m_bInit = true;
     }
 }
