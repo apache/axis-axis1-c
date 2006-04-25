fdba5a7204bc93a87ca7412635f3fe22c4e522f8
Nadir K. Amra <nadiramra@apache.org>
2006-04-25 03:36:59 +0000

    C support fixes/enhancements. Really no need for AxisUserAPIArrays.h

--- include/axis/AxisUserAPI.h
+++ include/axis/AxisUserAPI.h
@@ -427,6 +427,83 @@ typedef struct
     int _size;
 } AxiscAnyType;
 
+/**
+ * Axis Array implementation.
+ */
+ 
+#define AXISC_DEFINED_ARRAY(type) \
+    typedef struct {\
+        type ** m_Array;\
+        int m_Size;\
+        AXISC_XSDTYPE m_Type; \
+    } type##_Array;
+
+#define AXISC_DEFINED_POINTER_ARRAY(type) \
+    typedef struct {\
+        type * m_Array;\
+        int m_Size;\
+        AXISC_XSDTYPE m_Type; \
+    } type##_Array;
+
+#define AXISC_DEFINED_ARRAY2(type) \
+    typedef struct {\
+        AXISCHANDLE m_Array;\
+        int m_Size;\
+        AXISC_XSDTYPE m_Type; \
+    } type##_Array;
+
+typedef struct {
+    void** m_Array;
+    int m_Size;
+    AXISC_XSDTYPE m_Type; \
+} Axisc_Array;
+
+AXISC_DEFINED_ARRAY(xsdc__duration) 
+AXISC_DEFINED_ARRAY(xsdc__dateTime)
+AXISC_DEFINED_ARRAY(xsdc__time)
+AXISC_DEFINED_ARRAY(xsdc__date)
+AXISC_DEFINED_ARRAY(xsdc__gYearMonth)
+AXISC_DEFINED_ARRAY(xsdc__gYear)
+AXISC_DEFINED_ARRAY(xsdc__gMonthDay)
+AXISC_DEFINED_ARRAY(xsdc__gDay)
+AXISC_DEFINED_ARRAY(xsdc__gMonth)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__string)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__normalizedString)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__token)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__language)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__Name)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__NCName)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__ID)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__IDREF)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__IDREFS)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__ENTITY)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__ENTITIES)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__NMTOKEN)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__NMTOKENS)
+AXISC_DEFINED_ARRAY(xsdc__boolean)
+AXISC_DEFINED_ARRAY(xsdc__base64Binary)
+AXISC_DEFINED_ARRAY(xsdc__hexBinary)
+AXISC_DEFINED_ARRAY(xsdc__float)
+AXISC_DEFINED_ARRAY(xsdc__decimal)
+AXISC_DEFINED_ARRAY(xsdc__integer)
+AXISC_DEFINED_ARRAY(xsdc__nonPositiveInteger)
+AXISC_DEFINED_ARRAY(xsdc__negativeInteger)
+AXISC_DEFINED_ARRAY(xsdc__long)
+AXISC_DEFINED_ARRAY(xsdc__int)
+AXISC_DEFINED_ARRAY(xsdc__short)
+AXISC_DEFINED_ARRAY(xsdc__byte)
+AXISC_DEFINED_ARRAY(xsdc__nonNegativeInteger)
+AXISC_DEFINED_ARRAY(xsdc__unsignedLong)
+AXISC_DEFINED_ARRAY(xsdc__unsignedInt)
+AXISC_DEFINED_ARRAY(xsdc__unsignedByte)
+AXISC_DEFINED_ARRAY(xsdc__unsignedShort)
+AXISC_DEFINED_ARRAY(xsdc__positiveInteger)
+AXISC_DEFINED_ARRAY(xsdc__double)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__anyURI)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__QName)
+AXISC_DEFINED_POINTER_ARRAY(xsdc__NOTATION)
+
+
 #ifdef __cplusplus
   }
 #endif
