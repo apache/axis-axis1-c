0a7958db1fa696e42876bb52bd95a65283c53049
Fred Preston <prestonf@apache.org>
2006-04-28 13:14:53 +0000

    Changes to Client sample applications for the new memory model and to tidy up any warnings.

--- samples/server/doclitfault/MathOps.cpp
+++ samples/server/doclitfault/MathOps.cpp
@@ -45,7 +45,7 @@ xsd__int MathOps::div(xsd__int Value0, xsd__int Value1)
          {
              pObjFault->varString = "Division by zero exception";
              pObjFault->varInt = 1;
-             pObjFault->varFloat = 10.52;
+             pObjFault->varFloat = (xsd__float) 10.52;
              throw pObjFault;
          }
     }
