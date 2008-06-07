87a9f801b286c3edf951d2c239e3741c7cfddde2
Nadir K. Amra <nadiramra@apache.org>
2008-06-07 02:47:14 +0000

    reformat....

--- src/wsdl/org/apache/axis/wsdl/wsdl2ws/SymbolTableParsingUtils.java
+++ src/wsdl/org/apache/axis/wsdl/wsdl2ws/SymbolTableParsingUtils.java
@@ -31,84 +31,93 @@ import org.apache.axis.wsdl.wsdl2ws.info.MethodInfo;
 /**
  * @author hemapani
  */
-public class SymbolTableParsingUtils {
-	public static String getTargetEndPointURI(Iterator ports){
-	  //we are checking only the first port. 
-	  //if the targetEndPointURI not specifed we continue having it null
-	  //The WrapperInfo will set a default value for the targetEndPointURI
-			
-	  if (ports.hasNext()) {
-		  Port port = (Port) ports.next();
-		  List adresslist = port.getExtensibilityElements();
-		  if (adresslist != null
-			  && adresslist.size() != 0
-			  && (adresslist.get(0)
-				  instanceof javax.wsdl.extensions.soap.SOAPAddress))
-			  return 
-				  ((javax.wsdl.extensions.soap.SOAPAddress) adresslist.get(0))
-					  .getLocationURI();
-	  }
-	  return null;
-	}
-	
-	public static String getTransportType(Binding binding){
-		List soapbinding = binding.getExtensibilityElements();
-		if (soapbinding != null
-			&& soapbinding.size() > 0
-			&& (soapbinding.get(0) instanceof javax.wsdl.extensions.soap.SOAPBinding))
-			return 	((javax.wsdl.extensions.soap.SOAPBinding) soapbinding.get(0))
-					.getTransportURI();
-		return null;			
-	}
-	
-	public static String getSoapAction(BindingOperation bindingOperation){
-		List extenstions = bindingOperation.getExtensibilityElements();
-		for(int i=0;i<extenstions.size();i++){
-			if(extenstions.get(i) instanceof SOAPOperation)
-				return ((SOAPOperation)extenstions.get(i)).getSoapActionURI();
-		}
-		return null;
-	}
-	public static List getInputInfo(BindingInput input,MethodInfo methodinfo){
-	  if (input == null)
-			return null;
-	  List soapbodies = input.getExtensibilityElements();
+public class SymbolTableParsingUtils
+{
+    public static String getTargetEndPointURI(Iterator ports)
+    {
+        // we are checking only the first port.
+        // if the targetEndPointURI not specifed we continue having it null
+        // The WrapperInfo will set a default value for the targetEndPointURI
 
-	  if (soapbodies != null) {
-		  for (int j = 0; j < soapbodies.size(); j++) {
-			  if (soapbodies.get(j) instanceof javax.wsdl.extensions.soap.SOAPBody) 
-              {
-					javax.wsdl.extensions.soap.SOAPBody body =
-						 ((javax.wsdl.extensions.soap.SOAPBody) soapbodies
-						  .get(j));
-					methodinfo.setInputEncoding(body.getEncodingStyles());
-					methodinfo.setInputUse(body.getUse());
+        if (ports.hasNext())
+        {
+            Port port = (Port) ports.next();
+            List adresslist = port.getExtensibilityElements();
+            if (adresslist != null
+                    && adresslist.size() != 0
+                    && (adresslist.get(0) instanceof javax.wsdl.extensions.soap.SOAPAddress))
+                return ((javax.wsdl.extensions.soap.SOAPAddress) adresslist.get(0))
+                        .getLocationURI();
+        }
+        return null;
+    }
+
+    public static String getTransportType(Binding binding)
+    {
+        List soapbinding = binding.getExtensibilityElements();
+        if (soapbinding != null
+                && soapbinding.size() > 0
+                && (soapbinding.get(0) instanceof javax.wsdl.extensions.soap.SOAPBinding))
+            return ((javax.wsdl.extensions.soap.SOAPBinding) soapbinding.get(0))
+                    .getTransportURI();
+        return null;
+    }
+
+    public static String getSoapAction(BindingOperation bindingOperation)
+    {
+        List extenstions = bindingOperation.getExtensibilityElements();
+        for (int i = 0; i < extenstions.size(); i++)
+        {
+            if (extenstions.get(i) instanceof SOAPOperation)
+                return ((SOAPOperation) extenstions.get(i)).getSoapActionURI();
+        }
+        return null;
+    }
+
+    public static List getInputInfo(BindingInput input, MethodInfo methodinfo)
+    {
+        if (input == null)
+            return null;
+        List soapbodies = input.getExtensibilityElements();
+
+        if (soapbodies != null)
+        {
+            for (int j = 0; j < soapbodies.size(); j++)
+            {
+                if (soapbodies.get(j) instanceof javax.wsdl.extensions.soap.SOAPBody)
+                {
+                    javax.wsdl.extensions.soap.SOAPBody body = ((javax.wsdl.extensions.soap.SOAPBody) soapbodies
+                            .get(j));
+                    methodinfo.setInputEncoding(body.getEncodingStyles());
+                    methodinfo.setInputUse(body.getUse());
                     methodinfo.setInputNamespaceURI(body.getNamespaceURI());
-			  }
-		  }
-	  }
-	  return null;
-	}
-	public static List getOutputInfo(BindingOutput input,MethodInfo methodinfo){
-	  if (input == null)
-			return null;
-	  List soapbodies = input.getExtensibilityElements();
+                }
+            }
+        }
+        return null;
+    }
 
-	  if (soapbodies != null) {
-		  for (int j = 0; j < soapbodies.size(); j++) 
-          {
-			  if (soapbodies.get(j) instanceof javax.wsdl.extensions.soap.SOAPBody) 
-              {
-					javax.wsdl.extensions.soap.SOAPBody body =
-						 ((javax.wsdl.extensions.soap.SOAPBody) soapbodies
-						  .get(j));
-					methodinfo.setInputEncoding(body.getEncodingStyles());
-					methodinfo.setInputUse(body.getUse());
+    public static List getOutputInfo(BindingOutput input, MethodInfo methodinfo)
+    {
+        if (input == null)
+            return null;
+        List soapbodies = input.getExtensibilityElements();
+
+        if (soapbodies != null)
+        {
+            for (int j = 0; j < soapbodies.size(); j++)
+            {
+                if (soapbodies.get(j) instanceof javax.wsdl.extensions.soap.SOAPBody)
+                {
+                    javax.wsdl.extensions.soap.SOAPBody body = ((javax.wsdl.extensions.soap.SOAPBody) soapbodies
+                            .get(j));
+                    methodinfo.setInputEncoding(body.getEncodingStyles());
+                    methodinfo.setInputUse(body.getUse());
                     methodinfo.setOutputNamespaceURI(body.getNamespaceURI());
-			  }
-		  }
-	  }
-	  return null;
-	}
+                }
+            }
+        }
+        return null;
+    }
 
 }
